a39b38c997b37a7236cc63cddfe7b9f37b4cd896
Prasanna Meda <pmeda@akamai.com>
2005-03-15 15:39:08 -0800

    [PATCH] use strncpy in get_task_comm
    Set_task_comm uses strlcpy, so get_task_comm must use strncpy.
    
    Signed-Off-by: Prasanna Meda <pmeda@akamai.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Chris Wright <chrisw@osdl.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- fs/exec.c
+++ fs/exec.c
@@ -814,7 +814,7 @@ void get_task_comm(char *buf, struct task_struct *tsk)
 {
 	/* buf must be at least sizeof(tsk->comm) in size */
 	task_lock(tsk);
-	memcpy(buf, tsk->comm, sizeof(tsk->comm));
+	strncpy(buf, tsk->comm, sizeof(tsk->comm));
 	task_unlock(tsk);
 }
 
