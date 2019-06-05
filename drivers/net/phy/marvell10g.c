9ccd2bf43ca63336a816cdd1cf1c03caf6b41ffd
Russell King <rmk+kernel@armlinux.org.uk>
2019-06-05 11:44:55 +0100

    net: phy: marvell10g: allow PHY to probe without firmware
    Allow the PHY to probe when there is no firmware, but do not allow the
    link to come up by forcing the PHY state to PHY_HALTED in a similar way
    to phy_error().
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
    [baruch: rebase on firmware load code]
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>

--- drivers/net/phy/marvell10g.c
+++ drivers/net/phy/marvell10g.c
@@ -104,6 +104,7 @@ enum {
 
 struct mv3310_priv {
 	u32 firmware_ver;
+	bool firmware_failed;
 	bool rate_match;
 
 	struct device *hwmon_dev;
@@ -457,6 +458,7 @@ out:
 
 static int mv3310_check_firmware(struct phy_device *phydev)
 {
+	struct mv3310_priv *priv = dev_get_drvdata(&phydev->mdio.dev);
 	int ret;
 
 	ret = phy_read_mmd(phydev, MDIO_MMD_PMAPMD, MV_PMA_BOOT);
@@ -466,6 +468,7 @@ static int mv3310_check_firmware(struct phy_device *phydev)
 	if (ret & MV_PMA_BOOT_FATAL) {
 		dev_warn(&phydev->mdio.dev,
 			 "PHY failed to boot firmware, status=%04x\n", ret);
+		priv->firmware_failed = true;
 		return -ENODEV;
 	}
 
@@ -551,6 +554,19 @@ static int mv3310_resume(struct phy_device *phydev)
 		return ret;
 
 	return mv3310_hwmon_config(phydev, true);
+}
+
+static int mv3310_start(struct phy_device *phydev)
+{
+	struct mv3310_priv *priv = dev_get_drvdata(&phydev->mdio.dev);
+
+	if (priv->firmware_failed) {
+		dev_warn(&phydev->mdio.dev,
+			 "PHY firmware failure: PHY not starting");
+		return -EINVAL;
+	}
+
+	return 0;
 }
 
 /* Some PHYs in the Alaska family such as the 88X3310 and the 88E2010
@@ -892,6 +908,7 @@ static struct phy_driver mv3310_drivers[] = {
 		.probe		= mv3310_probe,
 		.suspend	= mv3310_suspend,
 		.resume		= mv3310_resume,
+		.start		= mv3310_start,
 		.config_aneg	= mv3310_config_aneg,
 		.aneg_done	= mv3310_aneg_done,
 		.read_status	= mv3310_read_status,
@@ -906,6 +923,7 @@ static struct phy_driver mv3310_drivers[] = {
 		.probe		= mv3310_probe,
 		.suspend	= mv3310_suspend,
 		.resume		= mv3310_resume,
+		.start		= mv3310_start,
 		.config_init	= mv3310_config_init,
 		.config_aneg	= mv3310_config_aneg,
 		.aneg_done	= mv3310_aneg_done,
