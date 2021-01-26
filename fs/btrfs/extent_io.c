82099b9b9f20f26368d351206fd3f1dba7e8d583
Qu Wenruo <wqu@suse.com>
2021-01-26 16:33:45 +0800

    btrfs: merge PAGE_CLEAR_DIRTY and PAGE_SET_WRITEBACK to PAGE_START_WRITEBACK
    [ Upstream commit 6869b0a8be775e920be54ee9b69a743ca20d8332 ]
    
    PAGE_CLEAR_DIRTY and PAGE_SET_WRITEBACK are two defines used in
    __process_pages_contig(), to let the function know to clear page dirty
    bit and then set page writeback.
    
    However page writeback and dirty bits are conflicting (at least for
    sector size == PAGE_SIZE case), this means these two have to be always
    updated together.
    
    This means we can merge PAGE_CLEAR_DIRTY and PAGE_SET_WRITEBACK to
    PAGE_START_WRITEBACK.
    
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Stable-dep-of: 82323b1a7088 ("btrfs: fix double-decrement of bytes_may_use in submit_one_async_extent()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -1983,10 +1983,10 @@ static int __process_pages_contig(struct address_space *mapping,
 				pages_locked++;
 				continue;
 			}
-			if (page_ops & PAGE_CLEAR_DIRTY)
+			if (page_ops & PAGE_START_WRITEBACK) {
 				clear_page_dirty_for_io(pages[i]);
-			if (page_ops & PAGE_SET_WRITEBACK)
 				set_page_writeback(pages[i]);
+			}
 			if (page_ops & PAGE_SET_ERROR)
 				SetPageError(pages[i]);
 			if (page_ops & PAGE_END_WRITEBACK)
