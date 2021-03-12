77d276d987f9552111198670f0499558b4ebeb55
Mathieu Poirier <mathieu.poirier@linaro.org>
2021-03-12 09:24:39 -0700

    remoteproc: Add new RPROC_ATTACHED state
    [ Upstream commit 4196d18903f94090f0a223d65de25e3bf50a3d13 ]
    
    Add a new RPROC_ATTACHED state to take into account scenarios
    where the remoteproc core needs to attach to a remote processor
    that is booted by another entity.
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>
    Link: https://lore.kernel.org/r/20210312162453.1234145-4-mathieu.poirier@linaro.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Stable-dep-of: 74ee3b2f5767 ("remoteproc: Prevent crash handling to race with rproc_del()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/remoteproc/remoteproc_sysfs.c
+++ drivers/remoteproc/remoteproc_sysfs.c
@@ -201,6 +201,7 @@ static const char * const rproc_state_string[] = {
 	[RPROC_RUNNING]		= "running",
 	[RPROC_CRASHED]		= "crashed",
 	[RPROC_DELETED]		= "deleted",
+	[RPROC_ATTACHED]	= "attached",
 	[RPROC_DETACHED]	= "detached",
 	[RPROC_LAST]		= "invalid",
 };
