77a5ac3f8a03b8a9638eb5f04c1bd6d88bde57b1
Vincent Whitchurch <vincent.whitchurch@axis.com>
2021-04-23 13:45:24 +0200

    regulator: core: Fix off_on_delay handling
    [ Upstream commit a8ce7bd89689997537dd22dcbced46cf23dc19da ]
    
    The jiffies-based off_on_delay implementation has a couple of problems
    that cause it to sometimes not actually delay for the required time:
    
     (1) If, for example, the off_on_delay time is equivalent to one jiffy,
         and the ->last_off_jiffy is set just before a new jiffy starts,
         then _regulator_do_enable() does not wait at all since it checks
         using time_before().
    
     (2) When jiffies overflows, the value of "remaining" becomes higher
         than "max_delay" and the code simply proceeds without waiting.
    
    Fix these problems by changing it to use ktime_t instead.
    
    [Note that since jiffies doesn't start at zero but at INITIAL_JIFFIES
     ("-5 minutes"), (2) above also led to the code not delaying if
     the first regulator_enable() is called when the ->last_off_jiffy is not
     initialised, such as for regulators with ->constraints->boot_on set.
     It's not clear to me if this was intended or not, but I've preserved
     this behaviour explicitly with the check for a non-zero ->last_off.]
    
    Signed-off-by: Vincent Whitchurch <vincent.whitchurch@axis.com>
    Link: https://lore.kernel.org/r/20210423114524.26414-1-vincent.whitchurch@axis.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Stable-dep-of: 86a8eeb0e913 ("regulator: core: move supply check earlier in set_machine_constraints()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/regulator/driver.h
+++ include/linux/regulator/driver.h
@@ -473,7 +473,7 @@ struct regulator_dev {
 	unsigned int is_switch:1;
 
 	/* time when this regulator was disabled last time */
-	unsigned long last_off_jiffy;
+	ktime_t last_off;
 };
 
 struct regulator_dev *
