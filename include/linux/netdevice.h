a6ebcafc2f5ff7f0d1ce0c6dc38ac09a16a56ec0
Nikolay Aleksandrov <razor@blackwall.org>
2022-04-13 13:51:56 +0300

    net: add ndo_fdb_del_bulk
    [ Upstream commit 1306d5362a591493a2d07f685ed2cc480dcda320 ]
    
    Add a new netdev op called ndo_fdb_del_bulk, it will be later used for
    driver-specific bulk delete implementation dispatched from rtnetlink. The
    first user will be the bridge, we need it to signal to rtnetlink from
    the driver that we support bulk delete operation (NLM_F_BULK).
    
    Signed-off-by: Nikolay Aleksandrov <razor@blackwall.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Stable-dep-of: bf29555f5bdc ("rtnetlink: Allow deleting FDB entries in user namespace")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1200,6 +1200,10 @@ struct netdev_net_notifier {
  *		      struct net_device *dev,
  *		      const unsigned char *addr, u16 vid)
  *	Deletes the FDB entry from dev coresponding to addr.
+ * int (*ndo_fdb_del_bulk)(struct ndmsg *ndm, struct nlattr *tb[],
+ *			   struct net_device *dev,
+ *			   u16 vid,
+ *			   struct netlink_ext_ack *extack);
  * int (*ndo_fdb_dump)(struct sk_buff *skb, struct netlink_callback *cb,
  *		       struct net_device *dev, struct net_device *filter_dev,
  *		       int *idx)
@@ -1452,6 +1456,11 @@ struct net_device_ops {
 					       struct net_device *dev,
 					       const unsigned char *addr,
 					       u16 vid);
+	int			(*ndo_fdb_del_bulk)(struct ndmsg *ndm,
+						    struct nlattr *tb[],
+						    struct net_device *dev,
+						    u16 vid,
+						    struct netlink_ext_ack *extack);
 	int			(*ndo_fdb_dump)(struct sk_buff *skb,
 						struct netlink_callback *cb,
 						struct net_device *dev,
