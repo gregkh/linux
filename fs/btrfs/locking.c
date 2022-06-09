d5a30a6117eaee25a1aadf09ab3ae15223581b51
Zygo Blaxell <ce3g8jdj@umail.furryterror.org>
2022-06-08 22:39:36 -0400

    btrfs: don't set lock_owner when locking extent buffer for reading
    [ Upstream commit 97e86631bccddfbbe0c13f9a9605cdef11d31296 ]
    
    In 196d59ab9ccc "btrfs: switch extent buffer tree lock to rw_semaphore"
    the functions for tree read locking were rewritten, and in the process
    the read lock functions started setting eb->lock_owner = current->pid.
    Previously lock_owner was only set in tree write lock functions.
    
    Read locks are shared, so they don't have exclusive ownership of the
    underlying object, so setting lock_owner to any single value for a
    read lock makes no sense.  It's mostly harmless because write locks
    and read locks are mutually exclusive, and none of the existing code
    in btrfs (btrfs_init_new_buffer and print_eb_refs_lock) cares what
    nonsense is written in lock_owner when no writer is holding the lock.
    
    KCSAN does care, and will complain about the data race incessantly.
    Remove the assignments in the read lock functions because they're
    useless noise.
    
    Fixes: 196d59ab9ccc ("btrfs: switch extent buffer tree lock to rw_semaphore")
    CC: stable@vger.kernel.org # 5.15+
    Reviewed-by: Nikolay Borisov <nborisov@suse.com>
    Reviewed-by: Filipe Manana <fdmanana@suse.com>
    Signed-off-by: Zygo Blaxell <ce3g8jdj@umail.furryterror.org>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/btrfs/locking.c
+++ fs/btrfs/locking.c
@@ -47,7 +47,6 @@ void __btrfs_tree_read_lock(struct extent_buffer *eb, enum btrfs_lock_nesting ne
 		start_ns = ktime_get_ns();
 
 	down_read_nested(&eb->lock, nest);
-	eb->lock_owner = current->pid;
 	trace_btrfs_tree_read_lock(eb, start_ns);
 }
 
@@ -64,7 +63,6 @@ void btrfs_tree_read_lock(struct extent_buffer *eb)
 int btrfs_try_tree_read_lock(struct extent_buffer *eb)
 {
 	if (down_read_trylock(&eb->lock)) {
-		eb->lock_owner = current->pid;
 		trace_btrfs_try_tree_read_lock(eb);
 		return 1;
 	}
@@ -92,7 +90,6 @@ int btrfs_try_tree_write_lock(struct extent_buffer *eb)
 void btrfs_tree_read_unlock(struct extent_buffer *eb)
 {
 	trace_btrfs_tree_read_unlock(eb);
-	eb->lock_owner = 0;
 	up_read(&eb->lock);
 }
 
