a74e04deb5c89e97595672f44645e3341e5c7880
Ben Dooks <ben.dooks@sifive.com>
2022-07-14 11:51:01 +0100

    bpf: Add endian modifiers to fix endian warnings
    [ Upstream commit 96a233e600df351bcb06e3c20efe048855552926 ]
    
    A couple of the syscalls which load values (bpf_skb_load_helper_16() and
    bpf_skb_load_helper_32()) are using u16/u32 types which are triggering
    warnings as they are then converted from big-endian to CPU-endian. Fix
    these by making the types __be instead.
    
    Fixes the following sparse warnings:
    
      net/core/filter.c:246:32: warning: cast to restricted __be16
      net/core/filter.c:246:32: warning: cast to restricted __be16
      net/core/filter.c:246:32: warning: cast to restricted __be16
      net/core/filter.c:246:32: warning: cast to restricted __be16
      net/core/filter.c:273:32: warning: cast to restricted __be32
      net/core/filter.c:273:32: warning: cast to restricted __be32
      net/core/filter.c:273:32: warning: cast to restricted __be32
      net/core/filter.c:273:32: warning: cast to restricted __be32
      net/core/filter.c:273:32: warning: cast to restricted __be32
      net/core/filter.c:273:32: warning: cast to restricted __be32
    
    Signed-off-by: Ben Dooks <ben.dooks@sifive.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/20220714105101.297304-1-ben.dooks@sifive.com
    Stable-dep-of: d4bac0288a2b ("bpf: support SKF_NET_OFF and SKF_LL_OFF on skb frags")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/core/filter.c
+++ net/core/filter.c
@@ -205,7 +205,7 @@ BPF_CALL_2(bpf_skb_load_helper_8_no_cache, const struct sk_buff *, skb,
 BPF_CALL_4(bpf_skb_load_helper_16, const struct sk_buff *, skb, const void *,
 	   data, int, headlen, int, offset)
 {
-	u16 tmp, *ptr;
+	__be16 tmp, *ptr;
 	const int len = sizeof(tmp);
 
 	if (offset >= 0) {
@@ -232,7 +232,7 @@ BPF_CALL_2(bpf_skb_load_helper_16_no_cache, const struct sk_buff *, skb,
 BPF_CALL_4(bpf_skb_load_helper_32, const struct sk_buff *, skb, const void *,
 	   data, int, headlen, int, offset)
 {
-	u32 tmp, *ptr;
+	__be32 tmp, *ptr;
 	const int len = sizeof(tmp);
 
 	if (likely(offset >= 0)) {
