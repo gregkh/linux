f44124f407a394753d5d470546e4dc387cd75746
Vincent Mailhol <mailhol.vincent@wanadoo.fr>
2022-07-27 19:16:37 +0900

    can: etas_es58x: advertise timestamping capabilities and add ioctl support
    [ Upstream commit 1d46efa0008a6d73dad40e78a2b3fa6d3cfb74e4 ]
    
    Currently, userland has no method to query which timestamping features
    are supported by the etas_es58x driver (aside maybe of getting RX
    messages and observe whether or not hardware timestamps stay at zero).
    
    The canonical way for a network driver to advertise what kind of
    timestamping is supports is to implement
    ethtool_ops::get_ts_info(). Here, we use the CAN specific
    can_ethtool_op_get_ts_info_hwts() function to achieve this.
    
    In addition, the driver currently does not support the hardware
    timestamps ioctls. According to [1], SIOCSHWTSTAMP is "must" and
    SIOCGHWTSTAMP is "should". This patch fills up that gap by
    implementing net_device_ops::ndo_eth_ioctl() using the CAN specific
    function can_eth_ioctl_hwts().
    
    [1] kernel doc Timestamping, section 3.1: "Hardware Timestamping
    Implementation: Device Drivers"
    Link: https://docs.kernel.org/networking/timestamping.html#hardware-timestamping-implementation-device-drivers
    
    Signed-off-by: Vincent Mailhol <mailhol.vincent@wanadoo.fr>
    Link: https://lore.kernel.org/all/20220727101641.198847-11-mailhol.vincent@wanadoo.fr
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Stable-dep-of: 38c0abad45b1 ("can: etas_es58x: populate ndo_change_mtu() to prevent buffer overflow")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/can/usb/etas_es58x/es58x_core.c
+++ drivers/net/can/usb/etas_es58x/es58x_core.c
@@ -10,6 +10,7 @@
  * Copyright (c) 2020, 2021 Vincent Mailhol <mailhol.vincent@wanadoo.fr>
  */
 
+#include <linux/ethtool.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/usb.h>
@@ -1981,7 +1982,12 @@ static netdev_tx_t es58x_start_xmit(struct sk_buff *skb,
 static const struct net_device_ops es58x_netdev_ops = {
 	.ndo_open = es58x_open,
 	.ndo_stop = es58x_stop,
-	.ndo_start_xmit = es58x_start_xmit
+	.ndo_start_xmit = es58x_start_xmit,
+	.ndo_eth_ioctl = can_eth_ioctl_hwts,
+};
+
+static const struct ethtool_ops es58x_ethtool_ops = {
+	.get_ts_info = can_ethtool_op_get_ts_info_hwts,
 };
 
 /**
@@ -2088,6 +2094,7 @@ static int es58x_init_netdev(struct es58x_device *es58x_dev, int channel_idx)
 	es58x_init_priv(es58x_dev, es58x_priv(netdev), channel_idx);
 
 	netdev->netdev_ops = &es58x_netdev_ops;
+	netdev->ethtool_ops = &es58x_ethtool_ops;
 	netdev->flags |= IFF_ECHO;	/* We support local echo */
 
 	ret = register_candev(netdev);
