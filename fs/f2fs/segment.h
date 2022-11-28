c6ad7333e530dc17bd9207193a99007162bb93a9
Christoph Hellwig <hch@lst.de>
2022-11-28 10:43:44 +0100

    f2fs: remove struct segment_allocation default_salloc_ops
    [ Upstream commit 1c8a8ec0a0e9a1176022a35c4daf04fe1594d270 ]
    
    There is only  single instance of these ops, so remove the indirection
    and call allocate_segment_by_default directly.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Stable-dep-of: 43563069e1c1 ("f2fs: check curseg->inited before write_sum_page in change_curseg")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/f2fs/segment.h
+++ fs/f2fs/segment.h
@@ -227,10 +227,6 @@ struct sec_entry {
 	unsigned int valid_blocks;	/* # of valid blocks in a section */
 };
 
-struct segment_allocation {
-	void (*allocate_segment)(struct f2fs_sb_info *, int, bool);
-};
-
 #define MAX_SKIP_GC_COUNT			16
 
 struct inmem_pages {
@@ -240,8 +236,6 @@ struct inmem_pages {
 };
 
 struct sit_info {
-	const struct segment_allocation *s_ops;
-
 	block_t sit_base_addr;		/* start block address of SIT area */
 	block_t sit_blocks;		/* # of blocks used by SIT area */
 	block_t written_valid_blocks;	/* # of valid blocks in main area */
