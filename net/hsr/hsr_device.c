660a7c97023e8bddd2c3e0a8494af12ff1d3540c
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2022-11-29 17:48:14 +0100

    hsr: Use a single struct for self_node.
    [ Upstream commit 20d3c1e9b861b85e1a774e1876d6adeeb0251fc3 ]
    
    self_node_db is a list_head with one entry of struct hsr_node. The
    purpose is to hold the two MAC addresses of the node itself.
    It is convenient to recycle the structure. However having a list_head
    and fetching always the first entry is not really optimal.
    
    Created a new data strucure contaning the two MAC addresses named
    hsr_self_node. Access that structure like an RCU protected pointer so
    it can be replaced on the fly without blocking the reader.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Reviewed-by: Kurt Kanzenbach <kurt@linutronix.de>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: 7f16289b91eb ("net: hsr: free learned nodes on device setup failure")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/hsr/hsr_device.c
+++ net/hsr/hsr_device.c
@@ -491,7 +491,6 @@ int hsr_dev_finalize(struct net_device *hsr_dev, struct net_device *slave[2],
 	hsr = netdev_priv(hsr_dev);
 	INIT_LIST_HEAD(&hsr->ports);
 	INIT_LIST_HEAD(&hsr->node_db);
-	INIT_LIST_HEAD(&hsr->self_node_db);
 	spin_lock_init(&hsr->list_lock);
 
 	ether_addr_copy(hsr_dev->dev_addr, slave[0]->dev_addr);
