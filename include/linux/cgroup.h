76f78ff6e06b6b24e2d05724fcfa730f2574d38e
Tejun Heo <tj@kernel.org>
2023-04-13 06:40:04 -1000

    cgroup: Implement cgroup_show_cftypes()
    Implement cgroup_show_cftypes() which shows and hides all cgroup files
    associated with the specified set of cgroup file types. CFTYPE_HIDDEN flag
    is added so that files can be created hidden from the get-go.
    
    cgroup_show_cftypes() can be used whether the cftypes are added or not. It
    also combines with cgroup_show_file() so that a given file is visible iff
    both its cftype and cfile are visible.
    
    This will be used by a new sched_class to selectively show and hide CPU
    controller interface files depending on whether they're supported.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: David Vernet <dvernet@meta.com>
    Acked-by: Josh Don <joshdon@google.com>
    Acked-by: Hao Luo <haoluo@google.com>
    Acked-by: Barret Rhoden <brho@google.com>

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -115,6 +115,7 @@ int cgroup_transfer_tasks(struct cgroup *to, struct cgroup *from);
 int cgroup_add_dfl_cftypes(struct cgroup_subsys *ss, struct cftype *cfts);
 int cgroup_add_legacy_cftypes(struct cgroup_subsys *ss, struct cftype *cfts);
 int cgroup_rm_cftypes(struct cftype *cfts);
+void cgroup_show_cftype(struct cftype *cft, bool show);
 void cgroup_file_notify(struct cgroup_file *cfile);
 void cgroup_file_show(struct cgroup_file *cfile, bool show);
 
