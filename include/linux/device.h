86455cf8fc5275269d7ac212087486208e0c7d59
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2023-07-21 16:13:09 +0300

    driver core: Move dev_err_probe() to where it belogs
    [ Upstream commit 9e0cace7a6254070159ebd86497eadc29ea307ca ]
    
    dev_err_probe() belongs to the printing API, hence
    move the definition from device.h to dev_printk.h.
    
    There is no change to the callers at all, since:
    1) implementation is located in the same core.c;
    2) dev_printk.h is guaranteed to be included by device.h.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Andi Shyti <andi.shyti@kernel.org>
    Link: https://lore.kernel.org/r/20230721131309.16821-1-andriy.shevchenko@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Stable-dep-of: 797cc011ae02 ("backlight: sky81452-backlight: Check return value of devm_gpiod_get_optional() in sky81452_bl_parse_dt()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/device.h
+++ include/linux/device.h
@@ -1030,8 +1030,6 @@ void device_links_supplier_sync_state_pause(void);
 void device_links_supplier_sync_state_resume(void);
 void device_link_wait_removal(void);
 
-__printf(3, 4) int dev_err_probe(const struct device *dev, int err, const char *fmt, ...);
-
 /* Create alias, so I can be autoloaded. */
 #define MODULE_ALIAS_CHARDEV(major,minor) \
 	MODULE_ALIAS("char-major-" __stringify(major) "-" __stringify(minor))
