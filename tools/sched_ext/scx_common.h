7a1c90f939bee72a215c5729a14d3642b1ef4656
Tejun Heo <tj@kernel.org>
2023-11-27 15:13:58 -1000

    scx: Add s/uSIZE typedefs in scx_common.h
    The availability of s/uSIZE types are hit and miss. Let's always define them
    in terms of stdint types. This makes life easier for the scx user repo.

--- tools/sched_ext/scx_common.h
+++ tools/sched_ext/scx_common.h
@@ -7,15 +7,25 @@
 #ifndef __SCHED_EXT_COMMON_H
 #define __SCHED_EXT_COMMON_H
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "user_exit_info.h"
-
 #ifdef __KERNEL__
 #error "Should not be included by BPF programs"
 #endif
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "user_exit_info.h"
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
 
 #define SCX_BUG(__fmt, ...)							\
 	do {									\
