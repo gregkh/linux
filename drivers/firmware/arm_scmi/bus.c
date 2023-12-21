41714150f3d148fbcd7a947869c85531b53c50d4
Cristian Marussi <cristian.marussi@arm.com>
2023-12-21 15:11:29 +0000

    firmware: arm_scmi: Add multiple protocols registration support
    [ Upstream commit 2858f6e5f06440d6b9e9c3f57bc68915344830a9 ]
    
    Add the capability for a SCMI driver to register to the core SCMI stack
    with multiple SCMI protocols. In such a case the SCMI driver probe
    function will end up being called once for each registered protocol
    which have been also found as implemented on the platform.
    
    This is especially useful in testing scenarios.
    
    Signed-off-by: Cristian Marussi <cristian.marussi@arm.com>
    Link: https://lore.kernel.org/r/20231221151129.325749-1-cristian.marussi@arm.com
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>
    Stable-dep-of: 9f7cd6a62aa7 ("firmware: arm_scmi: Unrequest devices if driver registration fails")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/firmware/arm_scmi/bus.c
+++ drivers/firmware/arm_scmi/bus.c
@@ -134,6 +134,17 @@ out:
 	return ret;
 }
 
+static int scmi_protocol_table_register(const struct scmi_device_id *id_table)
+{
+	int ret = 0;
+	const struct scmi_device_id *entry;
+
+	for (entry = id_table; entry->name && ret == 0; entry++)
+		ret = scmi_protocol_device_request(entry);
+
+	return ret;
+}
+
 /**
  * scmi_protocol_device_unrequest  - Helper to unrequest a device
  *
@@ -177,6 +188,15 @@ static void scmi_protocol_device_unrequest(const struct scmi_device_id *id_table
 					     (void *)victim->id_table);
 		kfree(victim);
 	}
+}
+
+static void
+scmi_protocol_table_unregister(const struct scmi_device_id *id_table)
+{
+	const struct scmi_device_id *entry;
+
+	for (entry = id_table; entry->name; entry++)
+		scmi_protocol_device_unrequest(entry);
 }
 
 static const struct scmi_device_id *
@@ -275,7 +295,7 @@ int scmi_driver_register(struct scmi_driver *driver, struct module *owner,
 	if (!driver->probe)
 		return -EINVAL;
 
-	retval = scmi_protocol_device_request(driver->id_table);
+	retval = scmi_protocol_table_register(driver->id_table);
 	if (retval)
 		return retval;
 
@@ -295,7 +315,7 @@ EXPORT_SYMBOL_GPL(scmi_driver_register);
 void scmi_driver_unregister(struct scmi_driver *driver)
 {
 	driver_unregister(&driver->driver);
-	scmi_protocol_device_unrequest(driver->id_table);
+	scmi_protocol_table_unregister(driver->id_table);
 }
 EXPORT_SYMBOL_GPL(scmi_driver_unregister);
 
