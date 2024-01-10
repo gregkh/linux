d5061f96f1daf21fbe89723e369c7537860c4159
David Vernet <void@manifault.com>
2024-01-10 12:25:10 -0600

    scx: Convert remaining testcases to use new framework
    Now that the framework has been merged, let's update the remaining
    testcases to use it.
    
    Signed-off-by: David Vernet <void@manifault.com>

--- tools/testing/selftests/scx/select_cpu_dfl.c
+++ tools/testing/selftests/scx/select_cpu_dfl.c
@@ -4,32 +4,35 @@
  * Copyright (c) 2023 David Vernet <dvernet@meta.com>
  * Copyright (c) 2023 Tejun Heo <tj@kernel.org>
  */
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
-#include <libgen.h>
 #include <bpf/bpf.h>
 #include <scx/common.h>
 #include <sys/wait.h>
+#include <unistd.h>
 #include "select_cpu_dfl.bpf.skel.h"
 #include "scx_test.h"
 
 #define NUM_CHILDREN 1028
 
-int main(int argc, char **argv)
+static enum scx_test_status setup(void **ctx)
 {
 	struct select_cpu_dfl *skel;
+
+	skel = select_cpu_dfl__open_and_load();
+	SCX_FAIL_IF(!skel, "Failed to open and load skel");
+	*ctx = skel;
+
+	return SCX_TEST_PASS;
+}
+
+static enum scx_test_status run(void *ctx)
+{
+	struct select_cpu_dfl *skel = ctx;
 	struct bpf_link *link;
 	pid_t pids[NUM_CHILDREN];
 	int i, status;
 
-	libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
-
-	skel = select_cpu_dfl__open_and_load();
-	SCX_BUG_ON(!skel, "Failed to open and load skel");
-
 	link = bpf_map__attach_struct_ops(skel->maps.select_cpu_dfl_ops);
-	SCX_BUG_ON(!link, "Failed to attach struct_ops");
+	SCX_FAIL_IF(!link, "Failed to attach scheduler");
 
 	for (i = 0; i < NUM_CHILDREN; i++) {
 		pids[i] = fork();
@@ -45,8 +48,25 @@ int main(int argc, char **argv)
 	}
 
 	SCX_ASSERT(!skel->bss->saw_local);
-	bpf_link__destroy(link);
-	select_cpu_dfl__destroy(skel);
 
-	return 0;
+	bpf_link__destroy(link);
+
+	return SCX_TEST_PASS;
 }
+
+static void cleanup(void *ctx)
+{
+	struct select_cpu_dfl *skel = ctx;
+
+	select_cpu_dfl__destroy(skel);
+}
+
+struct scx_test select_cpu_dfl = {
+	.name = "select_cpu_dfl",
+	.description = "Verify the default ops.select_cpu() dispatches tasks "
+		       "when idles cores are found, and skips ops.enqueue()",
+	.setup = setup,
+	.run = run,
+	.cleanup = cleanup,
+};
+REGISTER_SCX_TEST(&select_cpu_dfl)
--- tools/testing/selftests/scx/select_cpu_dispatch.c
+++ tools/testing/selftests/scx/select_cpu_dispatch.c
@@ -4,32 +4,35 @@
  * Copyright (c) 2023 David Vernet <dvernet@meta.com>
  * Copyright (c) 2023 Tejun Heo <tj@kernel.org>
  */
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
-#include <libgen.h>
 #include <bpf/bpf.h>
 #include <scx/common.h>
 #include <sys/wait.h>
+#include <unistd.h>
 #include "select_cpu_dispatch.bpf.skel.h"
 #include "scx_test.h"
 
 #define NUM_CHILDREN 1028
 
-int main(int argc, char **argv)
+static enum scx_test_status setup(void **ctx)
 {
 	struct select_cpu_dispatch *skel;
+
+	skel = select_cpu_dispatch__open_and_load();
+	SCX_FAIL_IF(!skel, "Failed to open and load skel");
+	*ctx = skel;
+
+	return SCX_TEST_PASS;
+}
+
+static enum scx_test_status run(void *ctx)
+{
+	struct select_cpu_dispatch *skel = ctx;
 	struct bpf_link *link;
 	pid_t pids[NUM_CHILDREN];
 	int i, status;
 
-	libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
-
-	skel = select_cpu_dispatch__open_and_load();
-	SCX_BUG_ON(!skel, "Failed to open and load skel");
-
 	link = bpf_map__attach_struct_ops(skel->maps.select_cpu_dispatch_ops);
-	SCX_BUG_ON(!link, "Failed to attach struct_ops");
+	SCX_FAIL_IF(!link, "Failed to attach scheduler");
 
 	for (i = 0; i < NUM_CHILDREN; i++) {
 		pids[i] = fork();
@@ -44,9 +47,24 @@ int main(int argc, char **argv)
 		SCX_EQ(status, 0);
 	}
 
-
 	bpf_link__destroy(link);
-	select_cpu_dispatch__destroy(skel);
 
-	return 0;
+	return SCX_TEST_PASS;
 }
+
+static void cleanup(void *ctx)
+{
+	struct select_cpu_dispatch *skel = ctx;
+
+	select_cpu_dispatch__destroy(skel);
+}
+
+struct scx_test select_cpu_dispatch = {
+	.name = "select_cpu_dispatch",
+	.description = "Test direct dispatching to built-in DSQs from "
+		       "ops.select_cpu()",
+	.setup = setup,
+	.run = run,
+	.cleanup = cleanup,
+};
+REGISTER_SCX_TEST(&select_cpu_dispatch)
