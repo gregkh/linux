d5ba53140ee780f7c8a744f2fce4dfe2fa40c0f8
Valentin Schneider <vschneid@redhat.com>
2024-04-29 16:43:36 +0200

    rcu: Rename rcu_momentary_dyntick_idle() into rcu_momentary_eqs()
    [ Upstream commit 32a9f26e5e266f0116cc8536e5e4544523ddd334 ]
    
    The context_tracking.state RCU_DYNTICKS subvariable has been renamed to
    RCU_WATCHING, replace "dyntick_idle" into "eqs" to drop the dyntick
    reference.
    
    Signed-off-by: Valentin Schneider <vschneid@redhat.com>
    Reviewed-by: Frederic Weisbecker <frederic@kernel.org>
    Signed-off-by: Neeraj Upadhyay <neeraj.upadhyay@kernel.org>
    Stable-dep-of: 27d73e81195b ("rcu: Mark accesses to ->rcu_urgent_qs and ->rcu_need_heavy_qs")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/trace/trace_osnoise.c
+++ kernel/trace/trace_osnoise.c
@@ -1554,7 +1554,7 @@ static int run_osnoise(void)
 		 * This will eventually cause unwarranted noise as PREEMPT_RCU
 		 * will force preemption as the means of ending the current
 		 * grace period. We avoid this problem by calling
-		 * rcu_momentary_dyntick_idle(), which performs a zero duration
+		 * rcu_momentary_eqs(), which performs a zero duration
 		 * EQS allowing PREEMPT_RCU to end the current grace period.
 		 * This call shouldn't be wrapped inside an RCU critical
 		 * section.
@@ -1566,7 +1566,7 @@ static int run_osnoise(void)
 			if (!disable_irq)
 				local_irq_disable();
 
-			rcu_momentary_dyntick_idle();
+			rcu_momentary_eqs();
 
 			if (!disable_irq)
 				local_irq_enable();
