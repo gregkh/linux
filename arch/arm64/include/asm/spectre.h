351a505eb4785168b3cb66eec652c6cbec46de10
James Morse <james.morse@arm.com>
2024-08-19 14:15:53 +0100

    arm64: proton-pack: Expose whether the platform is mitigated by firmware
    commit e7956c92f396a44eeeb6eaf7a5b5e1ad24db6748 upstream.
    
    is_spectre_bhb_fw_affected() allows the caller to determine if the CPU
    is known to need a firmware mitigation. CPUs are either on the list
    of CPUs we know about, or firmware has been queried and reported that
    the platform is affected - and mitigated by firmware.
    
    This helper is not useful to determine if the platform is mitigated
    by firmware. A CPU could be on the know list, but the firmware may
    not be implemented. Its affected but not mitigated.
    
    spectre_bhb_enable_mitigation() handles this distinction by checking
    the firmware state before enabling the mitigation.
    
    Add a helper to expose this state. This will be used by the BPF JIT
    to determine if calling firmware for a mitigation is necessary and
    supported.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/include/asm/spectre.h
+++ arch/arm64/include/asm/spectre.h
@@ -96,6 +96,7 @@ enum mitigation_state arm64_get_meltdown_state(void);
 
 enum mitigation_state arm64_get_spectre_bhb_state(void);
 bool is_spectre_bhb_affected(const struct arm64_cpu_capabilities *entry, int scope);
+bool is_spectre_bhb_fw_mitigated(void);
 void spectre_bhb_enable_mitigation(const struct arm64_cpu_capabilities *__unused);
 #endif	/* __ASSEMBLY__ */
 #endif	/* __ASM_SPECTRE_H */
