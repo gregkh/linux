5f856da6fadacec4b43f7f6b889bc48f02b6a202
Yu Zhao <yuzhao@google.com>
2024-10-19 01:29:38 +0000

    mm: multi-gen LRU: remove MM_LEAF_OLD and MM_NONLEAF_TOTAL stats
    [ Upstream commit ddd6d8e975b171ea3f63a011a75820883ff0d479 ]
    
    Patch series "mm: multi-gen LRU: Have secondary MMUs participate in
    MM_WALK".
    
    Today, the MM_WALK capability causes MGLRU to clear the young bit from
    PMDs and PTEs during the page table walk before eviction, but MGLRU does
    not call the clear_young() MMU notifier in this case.  By not calling this
    notifier, the MM walk takes less time/CPU, but it causes pages that are
    accessed mostly through KVM / secondary MMUs to appear younger than they
    should be.
    
    We do call the clear_young() notifier today, but only when attempting to
    evict the page, so we end up clearing young/accessed information less
    frequently for secondary MMUs than for mm PTEs, and therefore they appear
    younger and are less likely to be evicted.  Therefore, memory that is
    *not* being accessed mostly by KVM will be evicted *more* frequently,
    worsening performance.
    
    ChromeOS observed a tab-open latency regression when enabling MGLRU with a
    setup that involved running a VM:
    
    		Tab-open latency histogram (ms)
    Version		p50	mean	p95	p99	max
    base		1315	1198	2347	3454	10319
    mglru		2559	1311	7399	12060	43758
    fix		1119	926	2470	4211	6947
    
    This series replaces the final non-selftest patchs from this series[1],
    which introduced a similar change (and a new MMU notifier) with KVM
    optimizations.  I'll send a separate series (to Sean and Paolo) for the
    KVM optimizations.
    
    This series also makes proactive reclaim with MGLRU possible for KVM
    memory.  I have verified that this functions correctly with the selftest
    from [1], but given that that test is a KVM selftest, I'll send it with
    the rest of the KVM optimizations later.  Andrew, let me know if you'd
    like to take the test now anyway.
    
    [1]: https://lore.kernel.org/linux-mm/20240926013506.860253-18-jthoughton@google.com/
    
    This patch (of 2):
    
    The removed stats, MM_LEAF_OLD and MM_NONLEAF_TOTAL, are not very helpful
    and become more complicated to properly compute when adding
    test/clear_young() notifiers in MGLRU's mm walk.
    
    Link: https://lkml.kernel.org/r/20241019012940.3656292-1-jthoughton@google.com
    Link: https://lkml.kernel.org/r/20241019012940.3656292-2-jthoughton@google.com
    Fixes: bd74fdaea146 ("mm: multi-gen LRU: support page table walks")
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Signed-off-by: James Houghton <jthoughton@google.com>
    Cc: Axel Rasmussen <axelrasmussen@google.com>
    Cc: David Matlack <dmatlack@google.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: David Stevens <stevensd@google.com>
    Cc: Oliver Upton <oliver.upton@linux.dev>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Sean Christopherson <seanjc@google.com>
    Cc: Wei Xu <weixugc@google.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3376,7 +3376,6 @@ restart:
 			continue;
 
 		if (!pte_young(ptent)) {
-			walk->mm_stats[MM_LEAF_OLD]++;
 			continue;
 		}
 
@@ -3529,7 +3528,6 @@ restart:
 			walk->mm_stats[MM_LEAF_TOTAL]++;
 
 			if (!pmd_young(val)) {
-				walk->mm_stats[MM_LEAF_OLD]++;
 				continue;
 			}
 
@@ -3540,8 +3538,6 @@ restart:
 			walk_pmd_range_locked(pud, addr, vma, args, bitmap, &first);
 			continue;
 		}
-
-		walk->mm_stats[MM_NONLEAF_TOTAL]++;
 
 		if (!walk->force_scan && should_clear_pmd_young()) {
 			if (!pmd_young(val))
@@ -5231,11 +5227,11 @@ static void lru_gen_seq_show_full(struct seq_file *m, struct lruvec *lruvec,
 	for (tier = 0; tier < MAX_NR_TIERS; tier++) {
 		seq_printf(m, "            %10d", tier);
 		for (type = 0; type < ANON_AND_FILE; type++) {
-			const char *s = "   ";
+			const char *s = "xxx";
 			unsigned long n[3] = {};
 
 			if (seq == max_seq) {
-				s = "RT ";
+				s = "RTx";
 				n[0] = READ_ONCE(lrugen->avg_refaulted[type][tier]);
 				n[1] = READ_ONCE(lrugen->avg_total[type][tier]);
 			} else if (seq == min_seq[type] || NR_HIST_GENS > 1) {
@@ -5257,14 +5253,14 @@ static void lru_gen_seq_show_full(struct seq_file *m, struct lruvec *lruvec,
 
 	seq_puts(m, "                      ");
 	for (i = 0; i < NR_MM_STATS; i++) {
-		const char *s = "      ";
+		const char *s = "xxxx";
 		unsigned long n = 0;
 
 		if (seq == max_seq && NR_HIST_GENS == 1) {
-			s = "LOYNFA";
+			s = "TYFA";
 			n = READ_ONCE(mm_state->stats[hist][i]);
 		} else if (seq != max_seq && NR_HIST_GENS > 1) {
-			s = "loynfa";
+			s = "tyfa";
 			n = READ_ONCE(mm_state->stats[hist][i]);
 		}
 
