f9c6aec2a6dd0d5651d421592463c74aeaa54a8c
Borislav Petkov (AMD) <bp@alien8.de>
2024-11-11 17:22:08 +0100

    x86/bugs: Add SRSO_USER_KERNEL_NO support
    commit 877818802c3e970f67ccb53012facc78bef5f97a upstream.
    
    If the machine has:
    
      CPUID Fn8000_0021_EAX[30] (SRSO_USER_KERNEL_NO) -- If this bit is 1,
      it indicates the CPU is not subject to the SRSO vulnerability across
      user/kernel boundaries.
    
    have it fall back to IBPB on VMEXIT only, in the case it is going to run
    VMs:
    
      Speculative Return Stack Overflow: Mitigation: IBPB on VMEXIT only
    
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Nikolay Borisov <nik.borisov@suse.com>
    Link: https://lore.kernel.org/r/20241202120416.6054-2-bp@kernel.org
    [ Harshit: Conflicts resolved as this commit: 7c62c442b6eb ("x86/vmscape:
      Enumerate VMSCAPE bug") has been applied already to 6.12.y ]
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -2810,6 +2810,9 @@ static void __init srso_select_mitigation(void)
 		break;
 
 	case SRSO_CMD_SAFE_RET:
+		if (boot_cpu_has(X86_FEATURE_SRSO_USER_KERNEL_NO))
+			goto ibpb_on_vmexit;
+
 		if (IS_ENABLED(CONFIG_MITIGATION_SRSO)) {
 			/*
 			 * Enable the return thunk for generated code
@@ -2861,6 +2864,7 @@ static void __init srso_select_mitigation(void)
 		}
 		break;
 
+ibpb_on_vmexit:
 	case SRSO_CMD_IBPB_ON_VMEXIT:
 		if (IS_ENABLED(CONFIG_MITIGATION_IBPB_ENTRY)) {
 			if (has_microcode) {
