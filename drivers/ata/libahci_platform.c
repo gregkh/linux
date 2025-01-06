e1bdb71759bb8232c4022fb8e22c129888dfcb2a
Damien Le Moal <dlemoal@kernel.org>
2025-01-06 14:14:47 +0900

    ahci: Introduce ahci_ignore_port() helper
    [ Upstream commit c9b5be909e6595547ed5d45aef39fd65948aa342 ]
    
    libahci and AHCI drivers may ignore some ports if the port is invalid
    (its ID does not correspond to a valid physical port) or if the user
    explicitly requested the port to be ignored with the mask_port_map
    ahci module parameter. Such port that shall be ignored can be identified
    by checking that the bit corresponding to the port ID is not set in the
    mask_port_map field of struct ahci_host_priv. E.g. code such as:
    "if (!(hpriv->mask_port_map & (1 << portid)))".
    
    Replace all direct use of the mask_port_map field to detect such port
    with the new helper inline function ahci_ignore_port() to make the code
    more readable/easier to understand.
    
    The comment describing the mask_port_map field of struct ahci_host_priv
    is also updated to be more accurate.
    
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>
    Reviewed-by: Niklas Cassel <cassel@kernel.org>
    Stable-dep-of: 4d99a91574c4 ("ata: ahci_ceva: fix error paths in ceva_ahci_platform_enable_resources()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/ata/libahci_platform.c
+++ drivers/ata/libahci_platform.c
@@ -48,7 +48,7 @@ int ahci_platform_enable_phys(struct ahci_host_priv *hpriv)
 	int rc, i;
 
 	for (i = 0; i < hpriv->nports; i++) {
-		if (!(hpriv->mask_port_map & (1 << i)))
+		if (ahci_ignore_port(hpriv, i))
 			continue;
 
 		rc = phy_init(hpriv->phys[i]);
@@ -72,7 +72,7 @@ int ahci_platform_enable_phys(struct ahci_host_priv *hpriv)
 
 disable_phys:
 	while (--i >= 0) {
-		if (!(hpriv->mask_port_map & (1 << i)))
+		if (ahci_ignore_port(hpriv, i))
 			continue;
 
 		phy_power_off(hpriv->phys[i]);
@@ -93,7 +93,7 @@ void ahci_platform_disable_phys(struct ahci_host_priv *hpriv)
 	int i;
 
 	for (i = 0; i < hpriv->nports; i++) {
-		if (!(hpriv->mask_port_map & (1 << i)))
+		if (ahci_ignore_port(hpriv, i))
 			continue;
 
 		phy_power_off(hpriv->phys[i]);
