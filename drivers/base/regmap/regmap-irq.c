ef539316c5e88fa5f4e8f617ee6072c4c71ce5bd
Jiasheng Jiang <jiashengjiangcool@gmail.com>
2025-02-05 00:43:43 +0000

    regmap-irq: Add missing kfree()
    commit 32ffed055dcee17f6705f545b069e44a66067808 upstream.
    
    Add kfree() for "d->main_status_buf" to the error-handling path to prevent
    a memory leak.
    
    Fixes: a2d21848d921 ("regmap: regmap-irq: Add main status register support")
    Cc: stable@vger.kernel.org  # v5.1+
    Signed-off-by: Jiasheng Jiang <jiashengjiangcool@gmail.com>
    Link: https://patch.msgid.link/20250205004343.14413-1-jiashengjiangcool@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/regmap/regmap-irq.c
+++ drivers/base/regmap/regmap-irq.c
@@ -808,6 +808,7 @@ err_alloc:
 	kfree(d->wake_buf);
 	kfree(d->mask_buf_def);
 	kfree(d->mask_buf);
+	kfree(d->main_status_buf);
 	kfree(d->status_buf);
 	kfree(d->status_reg_buf);
 	kfree(d);
@@ -854,6 +855,7 @@ void regmap_del_irq_chip(int irq, struct regmap_irq_chip_data *d)
 	kfree(d->wake_buf);
 	kfree(d->mask_buf_def);
 	kfree(d->mask_buf);
+	kfree(d->main_status_buf);
 	kfree(d->status_reg_buf);
 	kfree(d->status_buf);
 	kfree(d);
