256738f63d42c7db1b208ae8c6120d954bddb5c8
Ryan Seto <ryanseto@amd.com>
2025-02-28 14:24:57 -0500

    drm/amd/display: Prevent VStartup Overflow
    [ Upstream commit 29c1c20496a7a9bafe2bc2f833d69aa52e0f2c2d ]
    
    [Why]
    For some VR headsets with large blanks, it's possible
    to overflow the OTG_VSTARTUP_PARAM:VSTARTUP_START
    register. This can lead to incorrect DML calculations
    and underflow downstream.
    
    [How]
    Min the calcualted max_vstartup_lines with the max
    value of the register.
    
    Reviewed-by: Dillon Varone <dillon.varone@amd.com>
    Signed-off-by: Ryan Seto <ryanseto@amd.com>
    Signed-off-by: Tom Chung <chiahsuan.chung@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/display/dc/dml2/dml21/src/dml2_core/dml2_core_dcn4_calcs.c
+++ drivers/gpu/drm/amd/display/dc/dml2/dml21/src/dml2_core/dml2_core_dcn4_calcs.c
@@ -12,6 +12,7 @@
 #define DML2_MAX_FMT_420_BUFFER_WIDTH 4096
 #define DML_MAX_NUM_OF_SLICES_PER_DSC 4
 #define ALLOW_SDPIF_RATE_LIMIT_PRE_CSTATE
+#define DML_MAX_VSTARTUP_START 1023
 
 const char *dml2_core_internal_bw_type_str(enum dml2_core_internal_bw_type bw_type)
 {
@@ -3648,6 +3649,7 @@ static unsigned int CalculateMaxVStartup(
 	dml2_printf("DML::%s: vblank_avail = %u\n", __func__, vblank_avail);
 	dml2_printf("DML::%s: max_vstartup_lines = %u\n", __func__, max_vstartup_lines);
 #endif
+	max_vstartup_lines = (unsigned int)math_min2(max_vstartup_lines, DML_MAX_VSTARTUP_START);
 	return max_vstartup_lines;
 }
 
