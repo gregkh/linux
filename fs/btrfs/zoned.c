eec34d7d14c7e19695588f759ad21d39d1396f0f
Johannes Thumshirn <johannes.thumshirn@wdc.com>
2025-03-17 12:24:59 +0100

    btrfs: zoned: fix zone finishing with missing devices
    commit 35fec1089ebb5617f85884d3fa6a699ce6337a75 upstream.
    
    If do_zone_finish() is called with a filesystem that has missing devices
    (e.g. a RAID file system mounted in degraded mode) it is accessing the
    btrfs_device::zone_info pointer, which will not be set if the device
    in question is missing.
    
    Check if the device is present (by checking if it has a valid block device
    pointer associated) and if not, skip zone finishing for it.
    
    Fixes: 4dcbb8ab31c1 ("btrfs: zoned: make zone finishing multi stripe capable")
    CC: stable@vger.kernel.org # 6.1+
    Reviewed-by: Naohiro Aota <naohiro.aota@wdc.com>
    Reviewed-by: Anand Jain <anand.jain@oracle.com>
    Signed-off-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/btrfs/zoned.c
+++ fs/btrfs/zoned.c
@@ -2055,6 +2055,9 @@ static int do_zone_finish(struct btrfs_block_group *block_group, bool fully_writ
 		struct btrfs_device *device = map->stripes[i].dev;
 		const u64 physical = map->stripes[i].physical;
 
+		if (!device->bdev)
+			continue;
+
 		if (device->zone_info->max_active_zones == 0)
 			continue;
 
