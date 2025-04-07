2804e099b291395afb4d8a9bf7138b8a937c1029
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2025-04-07 20:26:32 +0200

    mptcp: only inc MPJoinAckHMacFailure for HMAC failures
    commit 21c02e8272bc95ba0dd44943665c669029b42760 upstream.
    
    Recently, during a debugging session using local MPTCP connections, I
    noticed MPJoinAckHMacFailure was not zero on the server side. The
    counter was in fact incremented when the PM rejected new subflows,
    because the 'subflow' limit was reached.
    
    The fix is easy, simply dissociating the two cases: only the HMAC
    validation check should increase MPTCP_MIB_JOINACKMAC counter.
    
    Fixes: 4cf8b7e48a09 ("subflow: introduce and use mptcp_can_accept_new_subflow()")
    Cc: stable@vger.kernel.org
    Reviewed-by: Geliang Tang <geliang@kernel.org>
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/20250407-net-mptcp-hmac-failure-mib-v1-1-3c9ecd0a3a50@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    [ No conflicts, but subflow_add_reset_reason() is not needed is this
     version: the related feature is not supported in this version. ]
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/mptcp/subflow.c
+++ net/mptcp/subflow.c
@@ -631,11 +631,13 @@ create_child:
 			if (!owner)
 				goto dispose_child;
 
-			if (!subflow_hmac_valid(req, &mp_opt) ||
-			    !mptcp_can_accept_new_subflow(subflow_req->msk)) {
+			if (!subflow_hmac_valid(req, &mp_opt)) {
 				SUBFLOW_REQ_INC_STATS(req, MPTCP_MIB_JOINACKMAC);
 				goto dispose_child;
 			}
+
+			if (!mptcp_can_accept_new_subflow(owner))
+				goto dispose_child;
 
 			/* move the msk reference ownership to the subflow */
 			subflow_req->msk = NULL;
