ddbfb6adb0596e5b7ecbd6c5dfc5d79c22397e25
Christoph Hellwig <hch@lst.de>
2025-04-30 14:26:48 -0700

    xfs: fix error returns from xfs_bmapi_write
    [ Upstream commit 6773da870ab89123d1b513da63ed59e32a29cb77 ]
    
    xfs_bmapi_write can return 0 without actually returning a mapping in
    mval in two different cases:
    
     1) when there is absolutely no space available to do an allocation
     2) when converting delalloc space, and the allocation is so small
        that it only covers parts of the delalloc extent before the
        range requested by the caller
    
    Callers at best can handle one of these cases, but in many cases can't
    cope with either one.  Switch xfs_bmapi_write to always return a
    mapping or return an error code instead.  For case 1) above ENOSPC is
    the obvious choice which is very much what the callers expect anyway.
    For case 2) there is no really good error code, so pick a funky one
    from the SysV streams portfolio.
    
    This fixes the reproducer here:
    
        https://lore.kernel.org/linux-xfs/CAEJPjCvT3Uag-pMTYuigEjWZHn1sGMZ0GCjVVCv29tNHK76Cgg@mail.gmail.com0/
    
    which uses reserved blocks to create file systems that are gravely
    out of space and thus cause at least xfs_file_alloc_space to hang
    and trigger the lack of ENOSPC handling in xfs_dquot_disk_alloc.
    
    Note that this patch does not actually make any caller but
    xfs_alloc_file_space deal intelligently with case 2) above.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reported-by: 刘通 <lyutoon@gmail.com>
    Reviewed-by: "Darrick J. Wong" <djwong@kernel.org>
    Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>
    Signed-off-by: Leah Rumancik <leah.rumancik@gmail.com>
    Acked-by: "Darrick J. Wong" <djwong@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/xfs/xfs_reflink.c
+++ fs/xfs/xfs_reflink.c
@@ -431,13 +431,6 @@ xfs_reflink_fill_cow_hole(
 	if (error)
 		return error;
 
-	/*
-	 * Allocation succeeded but the requested range was not even partially
-	 * satisfied?  Bail out!
-	 */
-	if (nimaps == 0)
-		return -ENOSPC;
-
 convert:
 	return xfs_reflink_convert_unwritten(ip, imap, cmap, convert_now);
 
@@ -500,13 +493,6 @@ xfs_reflink_fill_delalloc(
 		error = xfs_trans_commit(tp);
 		if (error)
 			return error;
-
-		/*
-		 * Allocation succeeded but the requested range was not even
-		 * partially satisfied?  Bail out!
-		 */
-		if (nimaps == 0)
-			return -ENOSPC;
 	} while (cmap->br_startoff + cmap->br_blockcount <= imap->br_startoff);
 
 	return xfs_reflink_convert_unwritten(ip, imap, cmap, convert_now);
