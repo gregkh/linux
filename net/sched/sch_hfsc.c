f1dde3eb17dc1b8bd07aed00004b1e05fc87a3d4
Cong Wang <xiyou.wangcong@gmail.com>
2025-05-18 15:20:37 -0700

    sch_hfsc: Fix qlen accounting bug when using peek in hfsc_enqueue()
    [ Upstream commit 3f981138109f63232a5fb7165938d4c945cc1b9d ]
    
    When enqueuing the first packet to an HFSC class, hfsc_enqueue() calls the
    child qdisc's peek() operation before incrementing sch->q.qlen and
    sch->qstats.backlog. If the child qdisc uses qdisc_peek_dequeued(), this may
    trigger an immediate dequeue and potential packet drop. In such cases,
    qdisc_tree_reduce_backlog() is called, but the HFSC qdisc's qlen and backlog
    have not yet been updated, leading to inconsistent queue accounting. This
    can leave an empty HFSC class in the active list, causing further
    consequences like use-after-free.
    
    This patch fixes the bug by moving the increment of sch->q.qlen and
    sch->qstats.backlog before the call to the child qdisc's peek() operation.
    This ensures that queue length and backlog are always accurate when packet
    drops or dequeues are triggered during the peek.
    
    Fixes: 12d0ad3be9c3 ("net/sched/sch_hfsc.c: handle corner cases where head may change invalidating calculated deadline")
    Reported-by: Mingi Cho <mincho@theori.io>
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/20250518222038.58538-2-xiyou.wangcong@gmail.com
    Reviewed-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/sched/sch_hfsc.c
+++ net/sched/sch_hfsc.c
@@ -1568,6 +1568,9 @@ hfsc_enqueue(struct sk_buff *skb, struct Qdisc *sch, struct sk_buff **to_free)
 		return err;
 	}
 
+	sch->qstats.backlog += len;
+	sch->q.qlen++;
+
 	if (first && !cl->cl_nactive) {
 		if (cl->cl_flags & HFSC_RSC)
 			init_ed(cl, len);
@@ -1582,9 +1585,6 @@ hfsc_enqueue(struct sk_buff *skb, struct Qdisc *sch, struct sk_buff **to_free)
 			cl->qdisc->ops->peek(cl->qdisc);
 
 	}
-
-	sch->qstats.backlog += len;
-	sch->q.qlen++;
 
 	return NET_XMIT_SUCCESS;
 }
