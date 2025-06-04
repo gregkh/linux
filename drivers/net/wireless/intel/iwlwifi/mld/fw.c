74ae616a49d1396e8d6d5741d5c98c0b296e4c81
Ilan Peer <ilan.peer@intel.com>
2025-06-04 06:13:21 +0300

    wifi: iwlwifi: mld: Move regulatory domain initialization
    [ Upstream commit f81aa834bfa91c827f290b62a245e23c5ad2813c ]
    
    The regulatory domain information was initialized every time the
    FW was loaded and the device was restarted. This was unnecessary
    and useless as at this stage the wiphy channels information was
    not setup yet so while the regulatory domain was set to the wiphy,
    the channel information was not updated.
    
    In case that a specific MCC was configured during FW initialization
    then following updates with this MCC are ignored, and thus the
    wiphy channels information is left with information not matching
    the regulatory domain.
    
    This commit moves the regulatory domain initialization to after the
    operational firmware is started, i.e., after the wiphy channels were
    configured and the regulatory information is needed.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Link: https://patch.msgid.link/20250604061200.f138a7382093.I2fd8b3e99be13c2687da483e2cb1311ffb4fbfce@changeid
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/wireless/intel/iwlwifi/mld/fw.c
+++ drivers/net/wireless/intel/iwlwifi/mld/fw.c
@@ -339,10 +339,6 @@ int iwl_mld_load_fw(struct iwl_mld *mld)
 	if (ret)
 		goto err;
 
-	ret = iwl_mld_init_mcc(mld);
-	if (ret)
-		goto err;
-
 	mld->fw_status.running = true;
 
 	return 0;
@@ -532,6 +528,10 @@ int iwl_mld_start_fw(struct iwl_mld *mld)
 	IWL_DEBUG_INFO(mld, "uCode started.\n");
 
 	ret = iwl_mld_config_fw(mld);
+	if (ret)
+		goto error;
+
+	ret = iwl_mld_init_mcc(mld);
 	if (ret)
 		goto error;
 
