bf33f52887ec40b6b57807f20a31abc671b442c5
Sean Christopherson <seanjc@google.com>
2025-06-10 16:20:04 -0700

    KVM: x86: Convert vcpu_run()'s immediate exit param into a generic bitmap
    commit 2478b1b220c49d25cb1c3f061ec4f9b351d9a131 upstream.
    
    Convert kvm_x86_ops.vcpu_run()'s "force_immediate_exit" boolean parameter
    into an a generic bitmap so that similar "take action" information can be
    passed to vendor code without creating a pile of boolean parameters.
    
    This will allow dropping kvm_x86_ops.set_dr6() in favor of a new flag, and
    will also allow for adding similar functionality for re-loading debugctl
    in the active VMCS.
    
    Opportunistically massage the TDX WARN and comment to prepare for adding
    more run_flags, all of which are expected to be mutually exclusive with
    TDX, i.e. should be WARNed on.
    
    No functional change intended.
    
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/r/20250610232010.162191-3-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -10785,6 +10785,7 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 		dm_request_for_irq_injection(vcpu) &&
 		kvm_cpu_accept_dm_intr(vcpu);
 	fastpath_t exit_fastpath;
+	u64 run_flags;
 
 	bool req_immediate_exit = false;
 
@@ -11029,8 +11030,11 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 		goto cancel_injection;
 	}
 
-	if (req_immediate_exit)
+	run_flags = 0;
+	if (req_immediate_exit) {
+		run_flags |= KVM_RUN_FORCE_IMMEDIATE_EXIT;
 		kvm_make_request(KVM_REQ_EVENT, vcpu);
+	}
 
 	fpregs_assert_state_consistent();
 	if (test_thread_flag(TIF_NEED_FPU_LOAD))
@@ -11067,8 +11071,7 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 		WARN_ON_ONCE((kvm_vcpu_apicv_activated(vcpu) != kvm_vcpu_apicv_active(vcpu)) &&
 			     (kvm_get_apic_mode(vcpu) != LAPIC_MODE_DISABLED));
 
-		exit_fastpath = kvm_x86_call(vcpu_run)(vcpu,
-						       req_immediate_exit);
+		exit_fastpath = kvm_x86_call(vcpu_run)(vcpu, run_flags);
 		if (likely(exit_fastpath != EXIT_FASTPATH_REENTER_GUEST))
 			break;
 
@@ -11079,6 +11082,8 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 			exit_fastpath = EXIT_FASTPATH_EXIT_HANDLED;
 			break;
 		}
+
+		run_flags = 0;
 
 		/* Note, VM-Exits that go down the "slow" path are accounted below. */
 		++vcpu->stat.exits;
