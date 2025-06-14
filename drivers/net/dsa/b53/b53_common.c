a62ee3d6bb61e5ca5ad80fc91917181c0c978adf
Álvaro Fernández Rojas <noltari@gmail.com>
2025-06-14 09:59:57 +0200

    net: dsa: b53: prevent GMII_PORT_OVERRIDE_CTRL access on BCM5325
    [ Upstream commit 37883bbc45a8555d6eca88d3a9730504d2dac86c ]
    
    BCM5325 doesn't implement GMII_PORT_OVERRIDE_CTRL register so we should
    avoid reading or writing it.
    PORT_OVERRIDE_RX_FLOW and PORT_OVERRIDE_TX_FLOW aren't defined on BCM5325
    and we should use PORT_OVERRIDE_LP_FLOW_25 instead.
    
    Reviewed-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>
    Link: https://patch.msgid.link/20250614080000.1884236-12-noltari@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: b6a8a5477fe9 ("net: dsa: b53: fix resetting speed and pause on forced link")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/dsa/b53/b53_common.c
+++ drivers/net/dsa/b53/b53_common.c
@@ -1026,6 +1026,8 @@ static void b53_force_link(struct b53_device *dev, int port, int link)
 	if (port == dev->cpu_port) {
 		off = B53_PORT_OVERRIDE_CTRL;
 		val = PORT_OVERRIDE_EN;
+	} else if (is5325(dev)) {
+		return;
 	} else {
 		off = B53_GMII_PORT_OVERRIDE_CTRL(port);
 		val = GMII_PO_EN;
@@ -1050,6 +1052,8 @@ static void b53_force_port_config(struct b53_device *dev, int port,
 	if (port == dev->cpu_port) {
 		off = B53_PORT_OVERRIDE_CTRL;
 		val = PORT_OVERRIDE_EN;
+	} else if (is5325(dev)) {
+		return;
 	} else {
 		off = B53_GMII_PORT_OVERRIDE_CTRL(port);
 		val = GMII_PO_EN;
@@ -1080,10 +1084,19 @@ static void b53_force_port_config(struct b53_device *dev, int port,
 		return;
 	}
 
-	if (rx_pause)
-		reg |= PORT_OVERRIDE_RX_FLOW;
-	if (tx_pause)
-		reg |= PORT_OVERRIDE_TX_FLOW;
+	if (rx_pause) {
+		if (is5325(dev))
+			reg |= PORT_OVERRIDE_LP_FLOW_25;
+		else
+			reg |= PORT_OVERRIDE_RX_FLOW;
+	}
+
+	if (tx_pause) {
+		if (is5325(dev))
+			reg |= PORT_OVERRIDE_LP_FLOW_25;
+		else
+			reg |= PORT_OVERRIDE_TX_FLOW;
+	}
 
 	b53_write8(dev, B53_CTRL_PAGE, off, reg);
 }
