c66239b7f0654256bda2020f94caa27542e232f1
Christian Brauner <brauner@kernel.org>
2025-06-18 22:53:35 +0200

    pidfs: raise SB_I_NODEV and SB_I_NOEXEC
    [ Upstream commit 1a1ad73aa1a66787f05f7f10f686b74bab77be72 ]
    
    Similar to commit 1ed95281c0c7 ("anon_inode: raise SB_I_NODEV and SB_I_NOEXEC"):
    it shouldn't be possible to execute pidfds via
    execveat(fd_anon_inode, "", NULL, NULL, AT_EMPTY_PATH)
    so raise SB_I_NOEXEC so that no one gets any creative ideas.
    
    Also raise SB_I_NODEV as we don't expect or support any devices on pidfs.
    
    Link: https://lore.kernel.org/20250618-work-pidfs-persistent-v2-1-98f3456fd552@kernel.org
    Reviewed-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/pidfs.c
+++ fs/pidfs.c
@@ -854,6 +854,8 @@ static int pidfs_init_fs_context(struct fs_context *fc)
 	if (!ctx)
 		return -ENOMEM;
 
+	fc->s_iflags |= SB_I_NOEXEC;
+	fc->s_iflags |= SB_I_NODEV;
 	ctx->ops = &pidfs_sops;
 	ctx->eops = &pidfs_export_operations;
 	ctx->dops = &pidfs_dentry_operations;
