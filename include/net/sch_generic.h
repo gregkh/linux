02ca042304e8dc9a56e28ae3f905cd038504a064
wenxu <wenxu@ucloud.cn>
2025-07-25 00:56:17 +0530

    net/sched: act_mirred: refactor the handle of xmit
    [ Upstream commit fa6d639930ee5cd3f932cc314f3407f07a06582d ]
    
    This one is prepare for the next patch.
    
    Signed-off-by: wenxu <wenxu@ucloud.cn>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    [ skulkarni: Adjusted patch for file 'sch_generic.h' wrt the mainline commit ]
    Stable-dep-of: ca22da2fbd69 ("act_mirred: use the backlog for nested calls to mirred ingress")
    Signed-off-by: Shubham Kulkarni <skulkarni@mvista.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/net/sch_generic.h
+++ include/net/sch_generic.h
@@ -1320,11 +1320,6 @@ void mini_qdisc_pair_swap(struct mini_Qdisc_pair *miniqp,
 void mini_qdisc_pair_init(struct mini_Qdisc_pair *miniqp, struct Qdisc *qdisc,
 			  struct mini_Qdisc __rcu **p_miniq);
 
-static inline int skb_tc_reinsert(struct sk_buff *skb, struct tcf_result *res)
-{
-	return res->ingress ? netif_receive_skb(skb) : dev_queue_xmit(skb);
-}
-
 /* Make sure qdisc is no longer in SCHED state. */
 static inline void qdisc_synchronize(const struct Qdisc *q)
 {
