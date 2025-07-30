1c296cba6568ee9f16e4e0e4ff9f129efae2a10b
Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
2025-07-29 18:53:32 -0700

    mm: reinstate ability to map write-sealed memfd mappings read-only
    [ Upstream commit 8ec396d05d1b737c87311fb7311f753b02c2a6b1 ]
    
    Patch series "mm: reinstate ability to map write-sealed memfd mappings
    read-only".
    
    In commit 158978945f31 ("mm: perform the mapping_map_writable() check
    after call_mmap()") (and preceding changes in the same series) it became
    possible to mmap() F_SEAL_WRITE sealed memfd mappings read-only.
    
    Commit 5de195060b2e ("mm: resolve faulty mmap_region() error path
    behaviour") unintentionally undid this logic by moving the
    mapping_map_writable() check before the shmem_mmap() hook is invoked,
    thereby regressing this change.
    
    This series reworks how we both permit write-sealed mappings being mapped
    read-only and disallow mprotect() from undoing the write-seal, fixing this
    regression.
    
    We also add a regression test to ensure that we do not accidentally
    regress this in future.
    
    Thanks to Julian Orth for reporting this regression.
    
    This patch (of 2):
    
    In commit 158978945f31 ("mm: perform the mapping_map_writable() check
    after call_mmap()") (and preceding changes in the same series) it became
    possible to mmap() F_SEAL_WRITE sealed memfd mappings read-only.
    
    This was previously unnecessarily disallowed, despite the man page
    documentation indicating that it would be, thereby limiting the usefulness
    of F_SEAL_WRITE logic.
    
    We fixed this by adapting logic that existed for the F_SEAL_FUTURE_WRITE
    seal (one which disallows future writes to the memfd) to also be used for
    F_SEAL_WRITE.
    
    For background - the F_SEAL_FUTURE_WRITE seal clears VM_MAYWRITE for a
    read-only mapping to disallow mprotect() from overriding the seal - an
    operation performed by seal_check_write(), invoked from shmem_mmap(), the
    f_op->mmap() hook used by shmem mappings.
    
    By extending this to F_SEAL_WRITE and critically - checking
    mapping_map_writable() to determine if we may map the memfd AFTER we
    invoke shmem_mmap() - the desired logic becomes possible.  This is because
    mapping_map_writable() explicitly checks for VM_MAYWRITE, which we will
    have cleared.
    
    Commit 5de195060b2e ("mm: resolve faulty mmap_region() error path
    behaviour") unintentionally undid this logic by moving the
    mapping_map_writable() check before the shmem_mmap() hook is invoked,
    thereby regressing this change.
    
    We reinstate this functionality by moving the check out of shmem_mmap()
    and instead performing it in do_mmap() at the point at which VMA flags are
    being determined, which seems in any case to be a more appropriate place
    in which to make this determination.
    
    In order to achieve this we rework memfd seal logic to allow us access to
    this information using existing logic and eliminate the clearing of
    VM_MAYWRITE from seal_check_write() which we are performing in do_mmap()
    instead.
    
    Link: https://lkml.kernel.org/r/99fc35d2c62bd2e05571cf60d9f8b843c56069e0.1732804776.git.lorenzo.stoakes@oracle.com
    Fixes: 5de195060b2e ("mm: resolve faulty mmap_region() error path behaviour")
    Signed-off-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Reported-by: Julian Orth <ju.orth@gmail.com>
    Closes: https://lore.kernel.org/all/CAHijbEUMhvJTN9Xw1GmbM266FXXv=U7s4L_Jem5x3AaPZxrYpQ@mail.gmail.com/
    Cc: Jann Horn <jannh@google.com>
    Cc: Liam R. Howlett <Liam.Howlett@Oracle.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/mmap.c
+++ mm/mmap.c
@@ -47,6 +47,7 @@
 #include <linux/pkeys.h>
 #include <linux/oom.h>
 #include <linux/sched/mm.h>
+#include <linux/memfd.h>
 
 #include <linux/uaccess.h>
 #include <asm/cacheflush.h>
@@ -1486,6 +1487,7 @@ unsigned long do_mmap(struct file *file, unsigned long addr,
 
 	if (file) {
 		struct inode *inode = file_inode(file);
+		unsigned int seals = memfd_file_seals(file);
 		unsigned long flags_mask;
 
 		if (!file_mmap_ok(file, inode, pgoff, len))
@@ -1524,6 +1526,8 @@ unsigned long do_mmap(struct file *file, unsigned long addr,
 			vm_flags |= VM_SHARED | VM_MAYSHARE;
 			if (!(file->f_mode & FMODE_WRITE))
 				vm_flags &= ~(VM_MAYWRITE | VM_SHARED);
+			else if (is_readonly_sealed(seals, vm_flags))
+				vm_flags &= ~VM_MAYWRITE;
 			fallthrough;
 		case MAP_PRIVATE:
 			if (!(file->f_mode & FMODE_READ))
