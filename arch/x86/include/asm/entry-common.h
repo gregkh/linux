ac60717f9a8d21c58617d0b34274babf24135835
Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
2025-08-14 10:20:42 -0700

    x86/vmscape: Add conditional IBPB mitigation
    commit 2f8f173413f1cbf52660d04df92d0069c4306d25 upstream.
    
    VMSCAPE is a vulnerability that exploits insufficient branch predictor
    isolation between a guest and a userspace hypervisor (like QEMU). Existing
    mitigations already protect kernel/KVM from a malicious guest. Userspace
    can additionally be protected by flushing the branch predictors after a
    VMexit.
    
    Since it is the userspace that consumes the poisoned branch predictors,
    conditionally issue an IBPB after a VMexit and before returning to
    userspace. Workloads that frequently switch between hypervisor and
    userspace will incur the most overhead from the new IBPB.
    
    This new IBPB is not integrated with the existing IBPB sites. For
    instance, a task can use the existing speculation control prctl() to
    get an IBPB at context switch time. With this implementation, the
    IBPB is doubled up: one at context switch and another before running
    userspace.
    
    The intent is to integrate and optimize these cases post-embargo.
    
    [ dhansen: elaborate on suboptimal IBPB solution ]
    
    Suggested-by: Dave Hansen <dave.hansen@linux.intel.com>
    Signed-off-by: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Borislav Petkov (AMD) <bp@alien8.de>
    Acked-by: Sean Christopherson <seanjc@google.com>
    [Amit: Use CPUID word 22 instead of 21 for the 5.10 backport]
    Signed-off-by: Amit Shah <amit.shah@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/include/asm/entry-common.h
+++ arch/x86/include/asm/entry-common.h
@@ -72,6 +72,13 @@ static inline void arch_exit_to_user_mode_prepare(struct pt_regs *regs,
 	 */
 	current_thread_info()->status &= ~(TS_COMPAT | TS_I386_REGS_POKED);
 #endif
+
+	/* Avoid unnecessary reads of 'x86_ibpb_exit_to_user' */
+	if (cpu_feature_enabled(X86_FEATURE_IBPB_EXIT_TO_USER) &&
+	    this_cpu_read(x86_ibpb_exit_to_user)) {
+		indirect_branch_prediction_barrier();
+		this_cpu_write(x86_ibpb_exit_to_user, false);
+	}
 }
 #define arch_exit_to_user_mode_prepare arch_exit_to_user_mode_prepare
 
