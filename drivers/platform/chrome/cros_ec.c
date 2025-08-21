3d3f5d3a2daeaf8406f0c71dc5b5b31887c55a90
Chen-Yu Tsai <wenst@chromium.org>
2025-08-21 12:14:35 -0400

    platform/chrome: cros_ec: Use per-device lockdep key
    [ Upstream commit 961a325becd9a142ae5c8b258e5c2f221f8bfac8 ]
    
    Lockdep reports a bogus possible deadlock on MT8192 Chromebooks due to
    the following lock sequences:
    
    1. lock(i2c_register_adapter) [1]; lock(&ec_dev->lock)
    2. lock(&ec_dev->lock); lock(prepare_lock);
    
    The actual dependency chains are much longer. The shortened version
    looks somewhat like:
    
    1. cros-ec-rpmsg on mtk-scp
       ec_dev->lock -> prepare_lock
    2. In rt5682_i2c_probe() on native I2C bus:
       prepare_lock -> regmap->lock -> (possibly) i2c_adapter->bus_lock
    3. In rt5682_i2c_probe() on native I2C bus:
       regmap->lock -> i2c_adapter->bus_lock
    4. In sbs_probe() on i2c-cros-ec-tunnel I2C bus attached on cros-ec:
       i2c_adapter->bus_lock -> ec_dev->lock
    
    While lockdep is correct that the shared lockdep classes have a circular
    dependency, it is bogus because
    
      a) 2+3 happen on a native I2C bus
      b) 4 happens on the actual EC on ChromeOS devices
      c) 1 happens on the SCP coprocessor on MediaTek Chromebooks that just
         happens to expose a cros-ec interface, but does not have an
         i2c-cros-ec-tunnel I2C bus
    
    In short, the "dependencies" are actually on different devices.
    
    Setup a per-device lockdep key for cros_ec devices so lockdep can tell
    the two instances apart. This helps with getting rid of the bogus
    lockdep warning. For ChromeOS devices that only have one cros-ec
    instance this doesn't change anything.
    
    Also add a missing mutex_destroy, just to make the teardown complete.
    
    [1] This is likely the per I2C bus lock with shared lockdep class
    
    Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>
    Signed-off-by: Tzung-Bi Shih <tzungbi@kernel.org>
    Link: https://lore.kernel.org/r/20230111074146.2624496-1-wenst@chromium.org
    Stable-dep-of: e23749534619 ("platform/chrome: cros_ec: Unregister notifier in cros_ec_unregister()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/platform/chrome/cros_ec.c
+++ drivers/platform/chrome/cros_ec.c
@@ -186,12 +186,14 @@ int cros_ec_register(struct cros_ec_device *ec_dev)
 	if (!ec_dev->dout)
 		return -ENOMEM;
 
+	lockdep_register_key(&ec_dev->lockdep_key);
 	mutex_init(&ec_dev->lock);
+	lockdep_set_class(&ec_dev->lock, &ec_dev->lockdep_key);
 
 	err = cros_ec_query_all(ec_dev);
 	if (err) {
 		dev_err(dev, "Cannot identify the EC: error %d\n", err);
-		return err;
+		goto destroy_mutex;
 	}
 
 	if (ec_dev->irq > 0) {
@@ -203,7 +205,7 @@ int cros_ec_register(struct cros_ec_device *ec_dev)
 		if (err) {
 			dev_err(dev, "Failed to request IRQ %d: %d",
 				ec_dev->irq, err);
-			return err;
+			goto destroy_mutex;
 		}
 	}
 
@@ -214,7 +216,8 @@ int cros_ec_register(struct cros_ec_device *ec_dev)
 	if (IS_ERR(ec_dev->ec)) {
 		dev_err(ec_dev->dev,
 			"Failed to create CrOS EC platform device\n");
-		return PTR_ERR(ec_dev->ec);
+		err = PTR_ERR(ec_dev->ec);
+		goto destroy_mutex;
 	}
 
 	if (ec_dev->max_passthru) {
@@ -273,6 +276,9 @@ int cros_ec_register(struct cros_ec_device *ec_dev)
 exit:
 	platform_device_unregister(ec_dev->ec);
 	platform_device_unregister(ec_dev->pd);
+destroy_mutex:
+	mutex_destroy(&ec_dev->lock);
+	lockdep_unregister_key(&ec_dev->lockdep_key);
 	return err;
 }
 EXPORT_SYMBOL(cros_ec_register);
@@ -290,6 +296,8 @@ void cros_ec_unregister(struct cros_ec_device *ec_dev)
 	if (ec_dev->pd)
 		platform_device_unregister(ec_dev->pd);
 	platform_device_unregister(ec_dev->ec);
+	mutex_destroy(&ec_dev->lock);
+	lockdep_unregister_key(&ec_dev->lockdep_key);
 }
 EXPORT_SYMBOL(cros_ec_unregister);
 
