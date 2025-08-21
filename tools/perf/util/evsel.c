2f3e5c090166cef96c64b15b5680fd6ad58359b2
Ian Rogers <irogers@google.com>
2025-08-21 09:38:17 -0700

    perf evsel: Avoid container_of on a NULL leader
    [ Upstream commit 2354479026d726954ff86ce82f4b649637319661 ]
    
    An evsel should typically have a leader of itself, however, in tests
    like 'Sample parsing' a NULL leader may occur and the container_of
    will return a corrupt pointer.
    
    Avoid this with an explicit NULL test.
    
    Fixes: fba7c86601e2e42d ("libperf: Move 'leader' from tools/perf to perf_evsel::leader")
    Reviewed-by: James Clark <james.clark@linaro.org>
    Signed-off-by: Ian Rogers <irogers@google.com>
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Athira Rajeev <atrajeev@linux.ibm.com>
    Cc: Blake Jones <blakejones@google.com>
    Cc: Chun-Tse Shao <ctshao@google.com>
    Cc: Collin Funk <collin.funk1@gmail.com>
    Cc: Howard Chu <howardchu95@gmail.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jan Polensky <japo@linux.ibm.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Kan Liang <kan.liang@linux.intel.com>
    Cc: Li Huafei <lihuafei1@huawei.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Nam Cao <namcao@linutronix.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Steinar H. Gunderson <sesse@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20250821163820.1132977-4-irogers@google.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -3497,6 +3497,8 @@ bool evsel__is_hybrid(const struct evsel *evsel)
 
 struct evsel *evsel__leader(const struct evsel *evsel)
 {
+	if (evsel->core.leader == NULL)
+		return NULL;
 	return container_of(evsel->core.leader, struct evsel, core);
 }
 
