41b838420457802f21918df66764b6fbf829d330
Pu Lehui <pulehui@huawei.com>
2025-08-24 09:58:43 -0400

    tracing: Limit access to parser->buffer when trace_get_user failed
    [ Upstream commit 6a909ea83f226803ea0e718f6e88613df9234d58 ]
    
    When the length of the string written to set_ftrace_filter exceeds
    FTRACE_BUFF_MAX, the following KASAN alarm will be triggered:
    
    BUG: KASAN: slab-out-of-bounds in strsep+0x18c/0x1b0
    Read of size 1 at addr ffff0000d00bd5ba by task ash/165
    
    CPU: 1 UID: 0 PID: 165 Comm: ash Not tainted 6.16.0-g6bcdbd62bd56-dirty
    Hardware name: linux,dummy-virt (DT)
    Call trace:
     show_stack+0x34/0x50 (C)
     dump_stack_lvl+0xa0/0x158
     print_address_description.constprop.0+0x88/0x398
     print_report+0xb0/0x280
     kasan_report+0xa4/0xf0
     __asan_report_load1_noabort+0x20/0x30
     strsep+0x18c/0x1b0
     ftrace_process_regex.isra.0+0x100/0x2d8
     ftrace_regex_release+0x484/0x618
     __fput+0x364/0xa58
     ____fput+0x28/0x40
     task_work_run+0x154/0x278
     do_notify_resume+0x1f0/0x220
     el0_svc+0xec/0xf0
     el0t_64_sync_handler+0xa0/0xe8
     el0t_64_sync+0x1ac/0x1b0
    
    The reason is that trace_get_user will fail when processing a string
    longer than FTRACE_BUFF_MAX, but not set the end of parser->buffer to 0.
    Then an OOB access will be triggered in ftrace_regex_release->
    ftrace_process_regex->strsep->strpbrk. We can solve this problem by
    limiting access to parser->buffer when trace_get_user failed.
    
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/20250813040232.1344527-1-pulehui@huaweicloud.com
    Fixes: 8c9af478c06b ("ftrace: Handle commands when closing set_ftrace_filter file")
    Signed-off-by: Pu Lehui <pulehui@huawei.com>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -1605,7 +1605,7 @@ int trace_get_user(struct trace_parser *parser, const char __user *ubuf,
 
 	ret = get_user(ch, ubuf++);
 	if (ret)
-		return ret;
+		goto fail;
 
 	read++;
 	cnt--;
@@ -1619,7 +1619,7 @@ int trace_get_user(struct trace_parser *parser, const char __user *ubuf,
 		while (cnt && isspace(ch)) {
 			ret = get_user(ch, ubuf++);
 			if (ret)
-				return ret;
+				goto fail;
 			read++;
 			cnt--;
 		}
@@ -1637,12 +1637,14 @@ int trace_get_user(struct trace_parser *parser, const char __user *ubuf,
 	while (cnt && !isspace(ch) && ch) {
 		if (parser->idx < parser->size - 1)
 			parser->buffer[parser->idx++] = ch;
-		else
-			return -EINVAL;
+		else {
+			ret = -EINVAL;
+			goto fail;
+		}
 
 		ret = get_user(ch, ubuf++);
 		if (ret)
-			return ret;
+			goto fail;
 		read++;
 		cnt--;
 	}
@@ -1657,11 +1659,15 @@ int trace_get_user(struct trace_parser *parser, const char __user *ubuf,
 		/* Make sure the parsed string always terminates with '\0'. */
 		parser->buffer[parser->idx] = 0;
 	} else {
-		return -EINVAL;
+		ret = -EINVAL;
+		goto fail;
 	}
 
 	*ppos += read;
 	return read;
+fail:
+	trace_parser_fail(parser);
+	return ret;
 }
 
 /* TODO add a seq_buf_to_buffer() */
