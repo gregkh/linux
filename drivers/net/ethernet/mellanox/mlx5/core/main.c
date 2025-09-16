c9b177d9cc24b254610178e177ff526aff6cd768
Cosmin Ratiu <cratiu@nvidia.com>
2025-09-16 17:11:38 +0300

    net/mlx5: Store the global doorbell in mlx5_priv
    [ Upstream commit aa4595d0ada65d5d44fa924a42a87c175d9d88e3 ]
    
    The global doorbell is used for more than just Ethernet resources, so
    move it out of mlx5e_hw_objs into a common place (mlx5_priv), to avoid
    non-Ethernet modules (e.g. HWS, ASO) depending on Ethernet structs.
    
    Use this opportunity to consolidate it with the 'uar' pointer already
    there, which was used as an RX doorbell. Underneath the 'uar' pointer is
    identical to 'bfreg->up', so store a single resource and use that
    instead.
    
    For CQ doorbells, care is taken to always use bfreg->up->index instead
    of bfreg->index, which may refer to a subsequent UAR page from the same
    ALLOC_UAR batch on some NICs.
    
    This paves the way for cleanly supporting multiple doorbells in the
    Ethernet driver.
    
    Signed-off-by: Cosmin Ratiu <cratiu@nvidia.com>
    Reviewed-by: Dragos Tatulea <dtatulea@nvidia.com>
    Signed-off-by: Tariq Toukan <tariqt@nvidia.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: e5eba42f0134 ("mlx5: Fix default values in create CQ")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/mellanox/mlx5/core/main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -1340,10 +1340,9 @@ static int mlx5_load(struct mlx5_core_dev *dev)
 {
 	int err;
 
-	dev->priv.uar = mlx5_get_uars_page(dev);
-	if (IS_ERR(dev->priv.uar)) {
-		mlx5_core_err(dev, "Failed allocating uar, aborting\n");
-		err = PTR_ERR(dev->priv.uar);
+	err = mlx5_alloc_bfreg(dev, &dev->priv.bfreg, false, false);
+	if (err) {
+		mlx5_core_err(dev, "Failed allocating bfreg, %d\n", err);
 		return err;
 	}
 
@@ -1454,7 +1453,7 @@ err_eq_table:
 err_irq_table:
 	mlx5_pagealloc_stop(dev);
 	mlx5_events_stop(dev);
-	mlx5_put_uars_page(dev, dev->priv.uar);
+	mlx5_free_bfreg(dev, &dev->priv.bfreg);
 	return err;
 }
 
@@ -1479,7 +1478,7 @@ static void mlx5_unload(struct mlx5_core_dev *dev)
 	mlx5_irq_table_destroy(dev);
 	mlx5_pagealloc_stop(dev);
 	mlx5_events_stop(dev);
-	mlx5_put_uars_page(dev, dev->priv.uar);
+	mlx5_free_bfreg(dev, &dev->priv.bfreg);
 }
 
 int mlx5_init_one_devl_locked(struct mlx5_core_dev *dev)
