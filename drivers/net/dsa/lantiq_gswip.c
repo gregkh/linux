37821b843e4e5b4986a6a733400ada59a8879056
Vladimir Oltean <vladimir.oltean@nxp.com>
2025-09-18 10:21:42 +0300

    net: dsa: lantiq_gswip: suppress -EINVAL errors for bridge FDB entries added to the CPU port
    [ Upstream commit 987afe147965ef7a8e7d144ffef0d70af14bb1d4 ]
    
    The blamed commit and others in that patch set started the trend
    of reusing existing DSA driver API for a new purpose: calling
    ds->ops->port_fdb_add() on the CPU port.
    
    The lantiq_gswip driver was not prepared to handle that, as can be seen
    from the many errors that Daniel presents in the logs:
    
    [  174.050000] gswip 1e108000.switch: port 2 failed to add fa:aa:72:f4:8b:1e vid 1 to fdb: -22
    [  174.060000] gswip 1e108000.switch lan2: entered promiscuous mode
    [  174.070000] gswip 1e108000.switch: port 2 failed to add 00:01:02:03:04:02 vid 0 to fdb: -22
    [  174.090000] gswip 1e108000.switch: port 2 failed to add 00:01:02:03:04:02 vid 1 to fdb: -22
    [  174.090000] gswip 1e108000.switch: port 2 failed to delete fa:aa:72:f4:8b:1e vid 1 from fdb: -2
    
    The errors are because gswip_port_fdb() wants to get a handle to the
    bridge that originated these FDB events, to associate it with a FID.
    Absolutely honourable purpose, however this only works for user ports.
    
    To get the bridge that generated an FDB entry for the CPU port, one
    would need to look at the db.bridge.dev argument. But this was
    introduced in commit c26933639b54 ("net: dsa: request drivers to perform
    FDB isolation"), first appeared in v5.18, and when the blamed commit was
    introduced in v5.14, no such API existed.
    
    So the core DSA feature was introduced way too soon for lantiq_gswip.
    Not acting on these host FDB entries and suppressing any errors has no
    other negative effect, and practically returns us to not supporting the
    host filtering feature at all - peacefully, this time.
    
    Fixes: 10fae4ac89ce ("net: dsa: include bridge addresses which are local in the host fdb list")
    Reported-by: Daniel Golle <daniel@makrotopia.org>
    Closes: https://lore.kernel.org/netdev/aJfNMLNoi1VOsPrN@pidgin.makrotopia.org/
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Link: https://patch.msgid.link/20250918072142.894692-3-vladimir.oltean@nxp.com
    Tested-by: Daniel Golle <daniel@makrotopia.org>
    Reviewed-by: Daniel Golle <daniel@makrotopia.org>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/dsa/lantiq_gswip.c
+++ drivers/net/dsa/lantiq_gswip.c
@@ -1337,8 +1337,9 @@ static int gswip_port_fdb(struct dsa_switch *ds, int port,
 	int i;
 	int err;
 
+	/* Operation not supported on the CPU port, don't throw errors */
 	if (!bridge)
-		return -EINVAL;
+		return 0;
 
 	for (i = cpu_port; i < ARRAY_SIZE(priv->vlans); i++) {
 		if (priv->vlans[i].bridge == bridge) {
