e4e99d69b8b8295c501b2eef89e13306b738b667
Charan Teja Kalla <charan.kalla@oss.qualcomm.com>
2025-09-24 23:41:38 +0530

    mm: swap: check for stable address space before operating on the VMA
    commit 1367da7eb875d01102d2ed18654b24d261ff5393 upstream.
    
    It is possible to hit a zero entry while traversing the vmas in unuse_mm()
    called from swapoff path and accessing it causes the OOPS:
    
    Unable to handle kernel NULL pointer dereference at virtual address
    0000000000000446--> Loading the memory from offset 0x40 on the
    XA_ZERO_ENTRY as address.
    Mem abort info:
      ESR = 0x0000000096000005
      EC = 0x25: DABT (current EL), IL = 32 bits
      SET = 0, FnV = 0
      EA = 0, S1PTW = 0
      FSC = 0x05: level 1 translation fault
    
    The issue is manifested from the below race between the fork() on a
    process and swapoff:
    fork(dup_mmap())			swapoff(unuse_mm)
    ---------------                         -----------------
    1) Identical mtree is built using
       __mt_dup().
    
    2) copy_pte_range()-->
    	copy_nonpresent_pte():
           The dst mm is added into the
        mmlist to be visible to the
        swapoff operation.
    
    3) Fatal signal is sent to the parent
    process(which is the current during the
    fork) thus skip the duplication of the
    vmas and mark the vma range with
    XA_ZERO_ENTRY as a marker for this process
    that helps during exit_mmap().
    
    				     4) swapoff is tried on the
    					'mm' added to the 'mmlist' as
    					part of the 2.
    
    				     5) unuse_mm(), that iterates
    					through the vma's of this 'mm'
    					will hit the non-NULL zero entry
    					and operating on this zero entry
    					as a vma is resulting into the
    					oops.
    
    The proper fix would be around not exposing this partially-valid tree to
    others when droping the mmap lock, which is being solved with [1].  A
    simpler solution would be checking for MMF_UNSTABLE, as it is set if
    mm_struct is not fully initialized in dup_mmap().
    
    Thanks to Liam/Lorenzo/David for all the suggestions in fixing this
    issue.
    
    Link: https://lkml.kernel.org/r/20250924181138.1762750-1-charan.kalla@oss.qualcomm.com
    Link: https://lore.kernel.org/all/20250815191031.3769540-1-Liam.Howlett@oracle.com/ [1]
    Fixes: d24062914837 ("fork: use __mt_dup() to duplicate maple tree in dup_mmap()")
    Signed-off-by: Charan Teja Kalla <charan.kalla@oss.qualcomm.com>
    Suggested-by: David Hildenbrand <david@redhat.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Barry Song <baohua@kernel.org>
    Cc: Chris Li <chrisl@kernel.org>
    Cc: Kairui Song <kasong@tencent.com>
    Cc: Kemeng Shi <shikemeng@huaweicloud.com>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Nhat Pham <nphamcs@gmail.com>
    Cc: Peng Zhang <zhangpeng.00@bytedance.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -2243,6 +2243,8 @@ static int unuse_mm(struct mm_struct *mm, unsigned int type)
 	VMA_ITERATOR(vmi, mm, 0);
 
 	mmap_read_lock(mm);
+	if (check_stable_address_space(mm))
+		goto unlock;
 	for_each_vma(vmi, vma) {
 		if (vma->anon_vma && !is_vm_hugetlb_page(vma)) {
 			ret = unuse_vma(vma, type);
@@ -2252,6 +2254,7 @@ static int unuse_mm(struct mm_struct *mm, unsigned int type)
 
 		cond_resched();
 	}
+unlock:
 	mmap_read_unlock(mm);
 	return ret;
 }
