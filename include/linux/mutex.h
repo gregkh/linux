f550466949e822afcd0b546a4fc35795930660bc
Peter Zijlstra <peterz@infradead.org>
2025-10-15 11:05:54 -0400

    locking: Introduce __cleanup() based infrastructure
    [ Upstream commit 54da6a0924311c7cf5015533991e44fb8eb12773 ]
    
    Use __attribute__((__cleanup__(func))) to build:
    
     - simple auto-release pointers using __free()
    
     - 'classes' with constructor and destructor semantics for
       scope-based resource management.
    
     - lock guards based on the above classes.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20230612093537.614161713%40infradead.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/mutex.h
+++ include/linux/mutex.h
@@ -19,6 +19,7 @@
 #include <asm/processor.h>
 #include <linux/osq_lock.h>
 #include <linux/debug_locks.h>
+#include <linux/cleanup.h>
 
 struct device;
 
@@ -245,5 +246,8 @@ extern int mutex_trylock(struct mutex *lock);
 extern void mutex_unlock(struct mutex *lock);
 
 extern int atomic_dec_and_mutex_lock(atomic_t *cnt, struct mutex *lock);
+
+DEFINE_GUARD(mutex, struct mutex *, mutex_lock(_T), mutex_unlock(_T))
+DEFINE_FREE(mutex, struct mutex *, if (_T) mutex_unlock(_T))
 
 #endif /* __LINUX_MUTEX_H */
