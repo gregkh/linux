5dacb83302ce8356f58becac2786523a8a506402
Christian Loehle <christian.loehle@arm.com>
2025-10-21 15:22:25 -0400

    PM: EM: Fix late boot with holes in CPU topology
    [ Upstream commit 1ebe8f7e782523e62cd1fa8237f7afba5d1dae83 ]
    
    Commit e3f1164fc9ee ("PM: EM: Support late CPUs booting and capacity
    adjustment") added a mechanism to handle CPUs that come up late by
    retrying when any of the `cpufreq_cpu_get()` call fails.
    
    However, if there are holes in the CPU topology (offline CPUs, e.g.
    nosmt), the first missing CPU causes the loop to break, preventing
    subsequent online CPUs from being updated.
    
    Instead of aborting on the first missing CPU policy, loop through all
    and retry if any were missing.
    
    Fixes: e3f1164fc9ee ("PM: EM: Support late CPUs booting and capacity adjustment")
    Suggested-by: Kenneth Crudup <kenneth.crudup@gmail.com>
    Reported-by: Kenneth Crudup <kenneth.crudup@gmail.com>
    Link: https://lore.kernel.org/linux-pm/40212796-734c-4140-8a85-854f72b8144d@panix.com/
    Cc: 6.9+ <stable@vger.kernel.org> # 6.9+
    Signed-off-by: Christian Loehle <christian.loehle@arm.com>
    Link: https://patch.msgid.link/20250831214357.2020076-1-christian.loehle@arm.com
    [ rjw: Drop the new pr_debug() message which is not very useful ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/power/energy_model.c
+++ kernel/power/energy_model.c
@@ -755,7 +755,7 @@ static void em_adjust_new_capacity(unsigned int cpu, struct device *dev,
 static void em_check_capacity_update(void)
 {
 	cpumask_var_t cpu_done_mask;
-	int cpu;
+	int cpu, failed_cpus = 0;
 
 	if (!zalloc_cpumask_var(&cpu_done_mask, GFP_KERNEL)) {
 		pr_warn("no free memory\n");
@@ -773,10 +773,8 @@ static void em_check_capacity_update(void)
 
 		policy = cpufreq_cpu_get(cpu);
 		if (!policy) {
-			pr_debug("Accessing cpu%d policy failed\n", cpu);
-			schedule_delayed_work(&em_update_work,
-					      msecs_to_jiffies(1000));
-			break;
+			failed_cpus++;
+			continue;
 		}
 		cpufreq_cpu_put(policy);
 
@@ -790,6 +788,9 @@ static void em_check_capacity_update(void)
 
 		em_adjust_new_capacity(cpu, dev, pd);
 	}
+
+	if (failed_cpus)
+		schedule_delayed_work(&em_update_work, msecs_to_jiffies(1000));
 
 	free_cpumask_var(cpu_done_mask);
 }
