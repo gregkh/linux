a35109f796db972cc6c7b087a9319a5918e2a5aa
Shuai Zhang <quic_shuaz@quicinc.com>
2025-11-09 17:24:37 +0800

    Bluetooth: btusb: add new custom firmwares
    [ Upstream commit a8b38d19857d42a1f2e90c9d9b0f74de2500acd7 ]
    
    The new platform uses the QCA2066 chip along with a new board ID, which
    requires a dedicated firmware file to ensure proper initialization.
    Without this entry, the driver cannot locate and load the correct
    firmware, resulting in Bluetooth bring-up failure.
    
    This patch adds a new entry to the firmware table for QCA2066 so that
    the driver can correctly identify the board ID and load the appropriate
    firmware from 'qca/QCA2066/' in the linux-firmware repository.
    
    Signed-off-by: Shuai Zhang <quic_shuaz@quicinc.com>
    Acked-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -3267,6 +3267,7 @@ static const struct qca_device_info qca_devices_table[] = {
 
 static const struct qca_custom_firmware qca_custom_btfws[] = {
 	{ 0x00130201, 0x030A, "QCA2066" },
+	{ 0x00130201, 0x030B, "QCA2066" },
 	{ },
 };
 
