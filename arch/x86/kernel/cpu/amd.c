f5bc3aef7df46eaaf423d7413ab8833f704ae576
Prathyushi Nangia <prathyushi.nangia@amd.com>
2025-12-09 10:01:33 -0600

    x86/CPU/AMD: Prevent improper isolation of shared resources in Zen2's op cache
    commit c21b90f77687075115d989e53a8ec5e2bb427ab1 upstream.
    
    Make sure resources are not improperly shared in the op cache and
    cause instruction corruption this way.
    
    Signed-off-by: Prathyushi Nangia <prathyushi.nangia@amd.com>
    Co-developed-by: Borislav Petkov (AMD) <bp@alien8.de>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Cc: stable@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -1170,6 +1170,9 @@ static void init_amd_zen2(struct cpuinfo_x86 *c)
 {
 	init_amd_zen_common();
 	init_spectral_chicken(c);
+
+	if (!cpu_has(c, X86_FEATURE_HYPERVISOR))
+		msr_set_bit(MSR_ZEN4_BP_CFG, MSR_ZEN2_BP_CFG_BUG_FIX_BIT);
 }
 
 static void init_amd_zen3(struct cpuinfo_x86 *c)
--- tools/arch/x86/include/asm/msr-index.h
+++ tools/arch/x86/include/asm/msr-index.h
@@ -530,6 +530,9 @@
 
 #define MSR_AMD64_VIRT_SPEC_CTRL	0xc001011f
 
+#define MSR_ZEN4_BP_CFG			0xc001102e
+#define MSR_ZEN2_BP_CFG_BUG_FIX_BIT	33
+
 /* Fam 17h MSRs */
 #define MSR_F17H_IRPERF			0xc00000e9
 
