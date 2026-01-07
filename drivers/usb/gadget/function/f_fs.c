7a8ac86f5133c7efdd372adfdc01e7863b15055d
Sam Day <me@samcday.com>
2026-01-08 08:30:20 +1000

    usb: gadget: f_fs: Fix ioctl error handling
    [ Upstream commit 8e4c1d06183c25022f6b0002a5cab84979ca6337 ]
    
    When ffs_epfile_ioctl handles FUNCTIONFS_DMABUF_* ioctls, it's currently
    falling through when copy_from_user fails.
    
    However, this fallthrough isn't being checked properly, so the handler
    continues executing further than it should. It then tries the secondary
    dispatch where it ultimately gives up and returns -ENOTTY.
    
    The end result is invalid ioctl invocations will yield a -ENOTTY rather
    than an -EFAULT.
    
    It's a common pattern elsewhere in the kernel code to directly return
    -EFAULT when copy_from_user fails. So we update ffs_epfile_ioctl to do
    the same and fix this issue.
    
    Signed-off-by: Sam Day <me@samcday.com>
    Link: https://patch.msgid.link/20260108-ffs-dmabuf-ioctl-fix-v1-1-e51633891a81@samcday.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -1724,10 +1724,8 @@ static long ffs_epfile_ioctl(struct file *file, unsigned code,
 	{
 		int fd;
 
-		if (copy_from_user(&fd, (void __user *)value, sizeof(fd))) {
-			ret = -EFAULT;
-			break;
-		}
+		if (copy_from_user(&fd, (void __user *)value, sizeof(fd)))
+			return -EFAULT;
 
 		return ffs_dmabuf_attach(file, fd);
 	}
@@ -1735,10 +1733,8 @@ static long ffs_epfile_ioctl(struct file *file, unsigned code,
 	{
 		int fd;
 
-		if (copy_from_user(&fd, (void __user *)value, sizeof(fd))) {
-			ret = -EFAULT;
-			break;
-		}
+		if (copy_from_user(&fd, (void __user *)value, sizeof(fd)))
+			return -EFAULT;
 
 		return ffs_dmabuf_detach(file, fd);
 	}
@@ -1746,10 +1742,8 @@ static long ffs_epfile_ioctl(struct file *file, unsigned code,
 	{
 		struct usb_ffs_dmabuf_transfer_req req;
 
-		if (copy_from_user(&req, (void __user *)value, sizeof(req))) {
-			ret = -EFAULT;
-			break;
-		}
+		if (copy_from_user(&req, (void __user *)value, sizeof(req)))
+			return -EFAULT;
 
 		return ffs_dmabuf_transfer(file, &req);
 	}
