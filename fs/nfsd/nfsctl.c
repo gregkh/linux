019debe5851d7355bea9ff0248cc317878924d8f
Kuniyuki Iwashima <kuniyu@google.com>
2026-01-24 04:18:41 +0000

    nfsd: Fix cred ref leak in nfsd_nl_listener_set_doit().
    commit 92978c83bb4eef55d02a6c990c01c423131eefa7 upstream.
    
    nfsd_nl_listener_set_doit() uses get_current_cred() without
    put_cred().
    
    As we can see from other callers, svc_xprt_create_from_sa()
    does not require the extra refcount.
    
    nfsd_nl_listener_set_doit() is always in the process context,
    sendmsg(), and current->cred does not go away.
    
    Let's use current_cred() in nfsd_nl_listener_set_doit().
    
    Fixes: 16a471177496 ("NFSD: add listener-{set,get} netlink command")
    Cc: stable@vger.kernel.org
    Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/nfsd/nfsctl.c
+++ fs/nfsd/nfsctl.c
@@ -1993,7 +1993,7 @@ int nfsd_nl_listener_set_doit(struct sk_buff *skb, struct genl_info *info)
 		}
 
 		ret = svc_xprt_create_from_sa(serv, xcl_name, net, sa, 0,
-					      get_current_cred());
+					      current_cred());
 		/* always save the latest error */
 		if (ret < 0)
 			err = ret;
