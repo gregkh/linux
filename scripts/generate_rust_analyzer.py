11cd1cf6a4c77ef8afb9ac382147e854e5e4804f
Tamir Duberstein <tamird@kernel.org>
2026-01-27 11:35:43 -0500

    scripts: generate_rust_analyzer.py: avoid FD leak
    commit 9b4744d8eda2824041064a5639ccbb079850914d upstream.
    
    Use `pathlib.Path.read_text()` to avoid leaking file descriptors.
    
    Fixes: 8c4555ccc55c ("scripts: add `generate_rust_analyzer.py`")
    Cc: stable@vger.kernel.org
    Reviewed-by: Daniel Almeida <daniel.almeida@collabora.com>
    Reviewed-by: Fiona Behrens <me@kloenk.dev>
    Reviewed-by: Trevor Gross <tmgross@umich.edu>
    Link: https://patch.msgid.link/20260127-rust-analyzer-fd-leak-v2-1-1bb55b9b6822@kernel.org
    Signed-off-by: Tamir Duberstein <tamird@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- scripts/generate_rust_analyzer.py
+++ scripts/generate_rust_analyzer.py
@@ -115,9 +115,10 @@ def generate_crates(srctree, objtree, sysroot_src, external_src, cfgs):
 
     def is_root_crate(build_file, target):
         try:
-            return f"{target}.o" in open(build_file).read()
+            contents = build_file.read_text()
         except FileNotFoundError:
             return False
+        return f"{target}.o" in contents
 
     # Then, the rest outside of `rust/`.
     #
