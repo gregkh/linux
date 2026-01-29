5c9d6c14d27025e4613f220907825aaea44809c0
Håkon Bugge <haakon.bugge@oracle.com>
2026-01-29 18:52:33 +0100

    PCI/ACPI: Restrict program_hpx_type2() to AER bits
    commit 9abf79c8d7b40db0e5a34aa8c744ea60ff9a3fcf upstream.
    
    Previously program_hpx_type2() applied PCIe settings unconditionally,
    which could incorrectly change bits like Extended Tag Field Enable and
    Enable Relaxed Ordering.
    
    When _HPX was added to ACPI r3.0, the intent of the PCIe Setting
    Record (Type 2) in sec 6.2.7.3 was to configure AER registers when the
    OS does not own the AER Capability:
    
      The PCI Express setting record contains ... [the AER] Uncorrectable
      Error Mask, Uncorrectable Error Severity, Correctable Error Mask
      ... to be used when configuring registers in the Advanced Error
      Reporting Extended Capability Structure ...
    
      OSPM [1] will only evaluate _HPX with Setting Record – Type 2 if
      OSPM is not controlling the PCI Express Advanced Error Reporting
      capability.
    
    ACPI r3.0b, sec 6.2.7.3, added more AER registers, including registers
    in the PCIe Capability with AER-related bits, and the restriction that
    the OS use this only when it owns PCIe native hotplug:
    
      ... when configuring PCI Express registers in the Advanced Error
      Reporting Extended Capability Structure *or PCI Express Capability
      Structure* ...
    
      An OS that has assumed ownership of native hot plug but does not
      ... have ownership of the AER register set must use ... the Type 2
      record to program the AER registers ...
    
      However, since the Type 2 record also includes register bits that
      have functions other than AER, the OS must ignore values ... that
      are not applicable.
    
    Restrict program_hpx_type2() to only the intended purpose:
    
      - Apply settings only when OS owns PCIe native hotplug but not AER,
    
      - Only touch the AER-related bits (Error Reporting Enables) in Device
        Control
    
      - Don't touch Link Control at all, since nothing there seems AER-related,
        but log _HPX settings for debugging purposes
    
    Note that Read Completion Boundary is now configured elsewhere, since it is
    unrelated to _HPX.
    
    [1] Operating System-directed configuration and Power Management
    
    Fixes: 40abb96c51bb ("[PATCH] pciehp: Fix programming hotplug parameters")
    Signed-off-by: Håkon Bugge <haakon.bugge@oracle.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Link: https://patch.msgid.link/20260129175237.727059-3-haakon.bugge@oracle.com
    [ Conflict in drivers/pci.h because the context has changed. ]
    Signed-off-by: Håkon Bugge <haakon.bugge@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -11,6 +11,9 @@
 
 #define PCI_VSEC_ID_INTEL_TBT	0x1234	/* Thunderbolt */
 
+#define PCI_EXP_AER_FLAGS	(PCI_EXP_DEVCTL_CERE | PCI_EXP_DEVCTL_NFERE | \
+				 PCI_EXP_DEVCTL_FERE | PCI_EXP_DEVCTL_URRE)
+
 extern const unsigned char pcie_link_speed[];
 extern bool pci_early_dump;
 
