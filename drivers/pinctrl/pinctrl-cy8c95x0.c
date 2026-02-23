03e71cc07cbab9d4f126bd92e52b36ef090839da
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2026-02-23 19:06:54 +0100

    pinctrl: cy8c95x0: Unify messages with help of dev_err_probe()
    [ Upstream commit 014884732095b982412d13d3220c3fe8483b9b3e ]
    
    Unify error messages that might appear during probe phase by
    switching to use dev_err_probe().
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Linus Walleij <linusw@kernel.org>
    Stable-dep-of: 5ad32c3607cf ("pinctrl: cy8c95x0: Avoid returning positive values to user space")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/pinctrl/pinctrl-cy8c95x0.c
+++ drivers/pinctrl/pinctrl-cy8c95x0.c
@@ -1262,10 +1262,8 @@ static int cy8c95x0_irq_setup(struct cy8c95x0_pinctrl *chip, int irq)
 
 	/* Read IRQ status register to clear all pending interrupts */
 	ret = cy8c95x0_irq_pending(chip, pending_irqs);
-	if (ret) {
-		dev_err(chip->dev, "failed to clear irq status register\n");
-		return ret;
-	}
+	if (ret)
+		return dev_err_probe(dev, ret, "failed to clear irq status register\n");
 
 	/* Mask all interrupts */
 	bitmap_fill(chip->irq_mask, MAX_LINE);
