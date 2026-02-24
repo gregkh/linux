efa365e0772f12e36ddb15268de88cfa020e8b04
Yosry Ahmed <yosry@kernel.org>
2026-02-24 22:50:17 +0000

    KVM: nSVM: Ensure AVIC is inhibited when restoring a vCPU to guest mode
    commit 24f7d36b824b65cf1a2db3db478059187b2a37b0 upstream.
    
    On nested VMRUN, KVM ensures AVIC is inhibited by requesting
    KVM_REQ_APICV_UPDATE, triggering a check of inhibit reasons, finding
    APICV_INHIBIT_REASON_NESTED, and disabling AVIC.
    
    However, when KVM_SET_NESTED_STATE is performed on a vCPU not in guest
    mode with AVIC enabled, KVM_REQ_APICV_UPDATE is not requested, and AVIC
    is not inhibited.
    
    Request KVM_REQ_APICV_UPDATE in the KVM_SET_NESTED_STATE path if AVIC is
    active, similar to the nested VMRUN path.
    
    Fixes: f44509f849fe ("KVM: x86: SVM: allow AVIC to co-exist with a nested guest running")
    Cc: stable@vger.kernel.org
    Signed-off-by: Yosry Ahmed <yosry@kernel.org>
    Link: https://patch.msgid.link/20260224225017.3303870-1-yosry@kernel.org
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kvm/svm/nested.c
+++ arch/x86/kvm/svm/nested.c
@@ -1252,6 +1252,9 @@ static int svm_set_nested_state(struct kvm_vcpu *vcpu,
 	load_nested_vmcb_control(svm, ctl);
 	nested_prepare_vmcb_control(svm);
 
+	if (kvm_vcpu_apicv_active(vcpu))
+		kvm_make_request(KVM_REQ_APICV_UPDATE, vcpu);
+
 	kvm_make_request(KVM_REQ_GET_NESTED_STATE_PAGES, vcpu);
 	ret = 0;
 out_free:
