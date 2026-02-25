5172adf9efb8298a52f4dcdc3f98d4d9d1e06a6d
Nikhil P. Rao <nikhil.rao@amd.com>
2026-02-25 00:00:26 +0000

    xsk: Fix fragment node deletion to prevent buffer leak
    [ Upstream commit 60abb0ac11dccd6b98fd9182bc5f85b621688861 ]
    
    After commit b692bf9a7543 ("xsk: Get rid of xdp_buff_xsk::xskb_list_node"),
    the list_node field is reused for both the xskb pool list and the buffer
    free list, this causes a buffer leak as described below.
    
    xp_free() checks if a buffer is already on the free list using
    list_empty(&xskb->list_node). When list_del() is used to remove a node
    from the xskb pool list, it doesn't reinitialize the node pointers.
    This means list_empty() will return false even after the node has been
    removed, causing xp_free() to incorrectly skip adding the buffer to the
    free list.
    
    Fix this by using list_del_init() instead of list_del() in all fragment
    handling paths, this ensures the list node is reinitialized after removal,
    allowing the list_empty() to work correctly.
    
    Fixes: b692bf9a7543 ("xsk: Get rid of xdp_buff_xsk::xskb_list_node")
    Acked-by: Maciej Fijalkowski <maciej.fijalkowski@intel.com>
    Signed-off-by: Nikhil P. Rao <nikhil.rao@amd.com>
    Link: https://patch.msgid.link/20260225000456.107806-2-nikhil.rao@amd.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: f7387d6579d6 ("xsk: Fix zero-copy AF_XDP fragment drop")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/net/xdp_sock_drv.h
+++ include/net/xdp_sock_drv.h
@@ -115,7 +115,7 @@ static inline void xsk_buff_free(struct xdp_buff *xdp)
 		goto out;
 
 	list_for_each_entry_safe(pos, tmp, xskb_list, list_node) {
-		list_del(&pos->list_node);
+		list_del_init(&pos->list_node);
 		xp_free(pos);
 	}
 
@@ -140,7 +140,7 @@ static inline struct xdp_buff *xsk_buff_get_frag(struct xdp_buff *first)
 	frag = list_first_entry_or_null(&xskb->pool->xskb_list,
 					struct xdp_buff_xsk, list_node);
 	if (frag) {
-		list_del(&frag->list_node);
+		list_del_init(&frag->list_node);
 		ret = &frag->xdp;
 	}
 
@@ -151,7 +151,7 @@ static inline void xsk_buff_del_tail(struct xdp_buff *tail)
 {
 	struct xdp_buff_xsk *xskb = container_of(tail, struct xdp_buff_xsk, xdp);
 
-	list_del(&xskb->list_node);
+	list_del_init(&xskb->list_node);
 }
 
 static inline struct xdp_buff *xsk_buff_get_tail(struct xdp_buff *first)
