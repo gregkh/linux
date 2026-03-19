0629a1a187e424373364d681b42b101894bdb548
Shyam Prasad N <sprasad@microsoft.com>
2026-03-18 20:35:52 -0400

    cifs: open files should not hold ref on superblock
    [ Upstream commit 340cea84f691c5206561bb2e0147158fe02070be ]
    
    Today whenever we deal with a file, in addition to holding
    a reference on the dentry, we also get a reference on the
    superblock. This happens in two cases:
    1. when a new cinode is allocated
    2. when an oplock break is being processed
    
    The reasoning for holding the superblock ref was to make sure
    that when umount happens, if there are users of inodes and
    dentries, it does not try to clean them up and wait for the
    last ref to superblock to be dropped by last of such users.
    
    But the side effect of doing that is that umount silently drops
    a ref on the superblock and we could have deferred closes and
    lease breaks still holding these refs.
    
    Ideally, we should ensure that all of these users of inodes and
    dentries are cleaned up at the time of umount, which is what this
    code is doing.
    
    This code change allows these code paths to use a ref on the
    dentry (and hence the inode). That way, umount is
    ensured to clean up SMB client resources when it's the last
    ref on the superblock (For ex: when same objects are shared).
    
    The code change also moves the call to close all the files in
    deferred close list to the umount code path. It also waits for
    oplock_break workers to be flushed before calling
    kill_anon_super (which eventually frees up those objects).
    
    Fixes: 24261fc23db9 ("cifs: delay super block destruction until all cifsFileInfo objects are gone")
    Fixes: 705c79101ccf ("smb: client: fix use-after-free in cifs_oplock_break")
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Shyam Prasad N <sprasad@microsoft.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    [ adapted kmalloc_obj() macro to kmalloc(sizeof()) ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/smb/client/file.c
+++ fs/smb/client/file.c
@@ -579,8 +579,6 @@ struct cifsFileInfo *cifs_new_fileinfo(struct cifs_fid *fid, struct file *file,
 	mutex_init(&cfile->fh_mutex);
 	spin_lock_init(&cfile->file_info_lock);
 
-	cifs_sb_active(inode->i_sb);
-
 	/*
 	 * If the server returned a read oplock and we have mandatory brlocks,
 	 * set oplock level to None.
@@ -635,7 +633,6 @@ static void cifsFileInfo_put_final(struct cifsFileInfo *cifs_file)
 	struct inode *inode = d_inode(cifs_file->dentry);
 	struct cifsInodeInfo *cifsi = CIFS_I(inode);
 	struct cifsLockInfo *li, *tmp;
-	struct super_block *sb = inode->i_sb;
 
 	/*
 	 * Delete any outstanding lock records. We'll lose them when the file
@@ -653,7 +650,6 @@ static void cifsFileInfo_put_final(struct cifsFileInfo *cifs_file)
 
 	cifs_put_tlink(cifs_file->tlink);
 	dput(cifs_file->dentry);
-	cifs_sb_deactive(sb);
 	kfree(cifs_file->symlink_target);
 	kfree(cifs_file);
 }
@@ -5154,12 +5150,6 @@ void cifs_oplock_break(struct work_struct *work)
 	__u64 persistent_fid, volatile_fid;
 	__u16 net_fid;
 
-	/*
-	 * Hold a reference to the superblock to prevent it and its inodes from
-	 * being freed while we are accessing cinode. Otherwise, _cifsFileInfo_put()
-	 * may release the last reference to the sb and trigger inode eviction.
-	 */
-	cifs_sb_active(sb);
 	wait_on_bit(&cinode->flags, CIFS_INODE_PENDING_WRITERS,
 			TASK_UNINTERRUPTIBLE);
 
@@ -5232,7 +5222,6 @@ oplock_break_ack:
 	cifs_put_tlink(tlink);
 out:
 	cifs_done_oplock_break(cinode);
-	cifs_sb_deactive(sb);
 }
 
 /*
