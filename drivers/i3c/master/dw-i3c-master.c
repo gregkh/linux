2fc602c3e5aa52eeed6d36d0933726bd92872b03
Felix Gu <ustc.gu@gmail.com>
2026-03-20 22:18:02 +0800

    i3c: master: dw-i3c: Fix missing reset assertion in remove() callback
    [ Upstream commit bef1eef667186cedb0bc6d152464acb3c97d5f72 ]
    
    The reset line acquired during probe is currently left deasserted when
    the driver is unbound.
    
    Switch to devm_reset_control_get_optional_exclusive_deasserted() to
    ensure the reset is automatically re-asserted by the devres core when
    the driver is removed.
    
    Fixes: 62fe9d06f570 ("i3c: dw: Add power management support")
    Reviewed-by: Philipp Zabel <p.zabel@pengutronix.de>
    Signed-off-by: Felix Gu <ustc.gu@gmail.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Link: https://patch.msgid.link/20260320-dw-i3c-v3-1-477040c2e3f5@gmail.com
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/i3c/master/dw-i3c-master.c
+++ drivers/i3c/master/dw-i3c-master.c
@@ -1566,12 +1566,10 @@ int dw_i3c_common_probe(struct dw_i3c_master *master,
 	if (IS_ERR(master->pclk))
 		return PTR_ERR(master->pclk);
 
-	master->core_rst = devm_reset_control_get_optional_exclusive(&pdev->dev,
-								    "core_rst");
+	master->core_rst = devm_reset_control_get_optional_exclusive_deasserted(&pdev->dev,
+										"core_rst");
 	if (IS_ERR(master->core_rst))
 		return PTR_ERR(master->core_rst);
-
-	reset_control_deassert(master->core_rst);
 
 	spin_lock_init(&master->xferqueue.lock);
 	INIT_LIST_HEAD(&master->xferqueue.list);
@@ -1584,7 +1582,7 @@ int dw_i3c_common_probe(struct dw_i3c_master *master,
 			       dw_i3c_master_irq_handler, 0,
 			       dev_name(&pdev->dev), master);
 	if (ret)
-		goto err_assert_rst;
+		return ret;
 
 	platform_set_drvdata(pdev, master);
 
@@ -1619,9 +1617,6 @@ err_disable_pm:
 	pm_runtime_disable(&pdev->dev);
 	pm_runtime_set_suspended(&pdev->dev);
 	pm_runtime_dont_use_autosuspend(&pdev->dev);
-
-err_assert_rst:
-	reset_control_assert(master->core_rst);
 
 	return ret;
 }
