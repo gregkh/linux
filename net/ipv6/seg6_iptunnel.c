956e9dc5e53d779184d448475d5d33672ca317da
Andrea Mayer <andrea.mayer@uniroma2.it>
2026-04-18 18:28:38 +0200

    seg6: fix seg6 lwtunnel output redirect for L2 reduced encap mode
    commit ade67d5f588832c7ba131aadd4215a94ce0a15c8 upstream.
    
    When SEG6_IPTUN_MODE_L2ENCAP_RED (L2ENCAP_RED) was introduced, the
    condition in seg6_build_state() that excludes L2 encap modes from
    setting LWTUNNEL_STATE_OUTPUT_REDIRECT was not updated to account for
    the new mode.
    As a consequence, L2ENCAP_RED routes incorrectly trigger seg6_output()
    on the output path, where the packet is silently dropped because
    skb_mac_header_was_set() fails on L3 packets.
    
    Extend the check to also exclude L2ENCAP_RED, consistent with L2ENCAP.
    
    Fixes: 13f0296be8ec ("seg6: add support for SRv6 H.L2Encaps.Red behavior")
    Cc: stable@vger.kernel.org
    Signed-off-by: Andrea Mayer <andrea.mayer@uniroma2.it>
    Reviewed-by: Justin Iurman <justin.iurman@gmail.com>
    Link: https://patch.msgid.link/20260418162838.31979-1-andrea.mayer@uniroma2.it
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/ipv6/seg6_iptunnel.c
+++ net/ipv6/seg6_iptunnel.c
@@ -715,7 +715,8 @@ static int seg6_build_state(struct net *net, struct nlattr *nla,
 	newts->type = LWTUNNEL_ENCAP_SEG6;
 	newts->flags |= LWTUNNEL_STATE_INPUT_REDIRECT;
 
-	if (tuninfo->mode != SEG6_IPTUN_MODE_L2ENCAP)
+	if (tuninfo->mode != SEG6_IPTUN_MODE_L2ENCAP &&
+	    tuninfo->mode != SEG6_IPTUN_MODE_L2ENCAP_RED)
 		newts->flags |= LWTUNNEL_STATE_OUTPUT_REDIRECT;
 
 	newts->headroom = seg6_lwt_headroom(tuninfo);
