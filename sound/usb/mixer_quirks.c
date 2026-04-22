addd89370397eff31afc18cc6560a0e483408f36
Cássio Gabriel <cassiogabrielcontato@gmail.com>
2026-04-21 21:53:52 -0300

    ALSA: usb-audio: Avoid false E-MU sample-rate notifications
    commit fca9c850042a7ab4828ce3a9caa8bc40ea09856a upstream.
    
    snd_emuusb_set_samplerate() unconditionally notifies the E-MU
    SampleRate Extension Unit control after issuing SET_CUR.
    
    If snd_usb_mixer_set_ctl_value() fails, the control value has not
    changed, yet snd_usb_mixer_notify_id() still invalidates the cache and
    emits a value-change event to userspace.
    
    Notify the control only after a successful write.
    
    Fixes: 7d2b451e65d2 ("ALSA: usb-audio - Added functionality for E-mu 0404USB/0202USB/TrackerPre")
    Cc: stable@vger.kernel.org
    Signed-off-by: Cássio Gabriel <cassiogabrielcontato@gmail.com>
    Link: https://patch.msgid.link/20260421-alsa-emuusb-samplerate-notify-v1-1-8b63bbc1d7f1@gmail.com
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- sound/usb/mixer_quirks.c
+++ sound/usb/mixer_quirks.c
@@ -1538,15 +1538,17 @@ void snd_emuusb_set_samplerate(struct snd_usb_audio *chip,
 {
 	struct usb_mixer_interface *mixer;
 	struct usb_mixer_elem_info *cval;
+	int err;
 	int unitid = 12; /* SampleRate ExtensionUnit ID */
 
 	list_for_each_entry(mixer, &chip->mixer_list, list) {
 		if (mixer->id_elems[unitid]) {
 			cval = mixer_elem_list_to_info(mixer->id_elems[unitid]);
-			snd_usb_mixer_set_ctl_value(cval, UAC_SET_CUR,
-						    cval->control << 8,
-						    samplerate_id);
-			snd_usb_mixer_notify_id(mixer, unitid);
+			err = snd_usb_mixer_set_ctl_value(cval, UAC_SET_CUR,
+							  cval->control << 8,
+							  samplerate_id);
+			if (!err)
+				snd_usb_mixer_notify_id(mixer, unitid);
 			break;
 		}
 	}
