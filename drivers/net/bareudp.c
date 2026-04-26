55193df8d6d33318435f19572bf5ea47a22eee28
Weiming Shi <bestswngs@gmail.com>
2026-04-26 09:53:51 -0700

    bareudp: fix NULL pointer dereference in bareudp_fill_metadata_dst()
    [ Upstream commit aa6c6d9ee064aabfede4402fd1283424e649ca19 ]
    
    bareudp_fill_metadata_dst() passes bareudp->sock to
    udp_tunnel6_dst_lookup() in the IPv6 path without a NULL check.
    The socket is only created in bareudp_open() and NULLed in
    bareudp_stop(), so calling this function while the device is down
    triggers a NULL dereference via sock->sk.
    
     BUG: kernel NULL pointer dereference, address: 0000000000000018
     RIP: 0010:udp_tunnel6_dst_lookup (net/ipv6/ip6_udp_tunnel.c:160)
     Call Trace:
      <TASK>
      bareudp_fill_metadata_dst (drivers/net/bareudp.c:532)
      do_execute_actions (net/openvswitch/actions.c:901)
      ovs_execute_actions (net/openvswitch/actions.c:1589)
      ovs_packet_cmd_execute (net/openvswitch/datapath.c:700)
      genl_family_rcv_msg_doit (net/netlink/genetlink.c:1114)
      genl_rcv_msg (net/netlink/genetlink.c:1209)
      netlink_rcv_skb (net/netlink/af_netlink.c:2550)
      </TASK>
    
    Add a NULL check returning -ESHUTDOWN, consistent with the xmit paths
    in the same driver.
    
    Fixes: 571912c69f0e ("net: UDP tunnel encapsulation module for tunnelling different protocols like MPLS, IP, NSH etc.")
    Reported-by: Xiang Mei <xmei5@asu.edu>
    Signed-off-by: Weiming Shi <bestswngs@gmail.com>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@google.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Link: https://patch.msgid.link/20260426165350.1663137-2-bestswngs@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/bareudp.c
+++ drivers/net/bareudp.c
@@ -513,6 +513,9 @@ static int bareudp_fill_metadata_dst(struct net_device *dev,
 		struct in6_addr saddr;
 		struct socket *sock = rcu_dereference(bareudp->sock);
 
+		if (!sock)
+			return -ESHUTDOWN;
+
 		dst = udp_tunnel6_dst_lookup(skb, dev, bareudp->net, sock,
 					     &saddr, info, IPPROTO_UDP,
 					     use_cache);
