710a946b1aa2c35dc56f86621f436938f31ba1a5
Benjamin Tissoires <bentiss@kernel.org>
2026-05-04 10:47:22 +0200

    HID: pass the buffer size to hid_report_raw_event
    [ Upstream commit 2c85c61d1332e1e16f020d76951baf167dcb6f7a ]
    
    commit 0a3fe972a7cb ("HID: core: Mitigate potential OOB by removing
    bogus memset()") enforced the provided data to be at least the size of
    the declared buffer in the report descriptor to prevent a buffer
    overflow. However, we can try to be smarter by providing both the buffer
    size and the data size, meaning that hid_report_raw_event() can make
    better decision whether we should plaining reject the buffer (buffer
    overflow attempt) or if we can safely memset it to 0 and pass it to the
    rest of the stack.
    
    Fixes: 0a3fe972a7cb ("HID: core: Mitigate potential OOB by removing bogus memset()")
    Cc: stable@vger.kernel.org
    Signed-off-by: Benjamin Tissoires <bentiss@kernel.org>
    Acked-by: Johan Hovold <johan@kernel.org>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Jiri Kosina <jkosina@suse.com>
    Stable-dep-of: 206342541fc8 ("HID: core: introduce hid_safe_input_report()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/hid.h
+++ include/linux/hid.h
@@ -1266,8 +1266,8 @@ static inline u32 hid_report_len(struct hid_report *report)
 	return DIV_ROUND_UP(report->size, 8) + (report->id > 0);
 }
 
-int hid_report_raw_event(struct hid_device *hid, enum hid_report_type type, u8 *data, u32 size,
-			 int interrupt);
+int hid_report_raw_event(struct hid_device *hid, enum hid_report_type type, u8 *data,
+			 size_t bufsize, u32 size, int interrupt);
 
 /* HID quirks API */
 unsigned long hid_lookup_quirk(const struct hid_device *hdev);
