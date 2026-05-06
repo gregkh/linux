73f120029d780ca09fd4f40bf27efe805c140a97
Matt Vollrath <tactii@gmail.com>
2026-05-06 14:48:11 -0700

    i40e: Cleanup PTP pins on probe failure
    commit 678b713ece1e853f11e670a84cb887c35e1381b7 upstream.
    
    PTP pin structs are allocated early in probe, but never cleaned up.
    
    Fix this by calling i40e_ptp_free_pins in the error path.
    
    To support this, i40e_ptp_free_pins is added to the header and
    pin_config is correctly nullified after being freed.
    
    This has been an issue since i40e_ptp_alloc_pins was introduced.
    
    Fixes: 1050713026a08 ("i40e: add support for PTP external synchronization clock")
    Reported-by: Kohei Enju <kohei@enjuk.jp>
    Cc: stable@vger.kernel.org
    Signed-off-by: Matt Vollrath <tactii@gmail.com>
    Reviewed-by: Paul Menzel <pmenzel@molgen.mpg.de>
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Reviewed-by: Kohei Enju <kohei@enjuk.jp>
    Tested-by: Sunitha Mekala <sunithax.d.mekala@intel.com>
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Link: https://patch.msgid.link/20260506-jk-iwl-net-2026-05-04-v2-2-a5ea4dc837a9@intel.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -16227,6 +16227,7 @@ err_vsis:
 	i40e_clear_interrupt_scheme(pf);
 	kfree(pf->vsi);
 err_switch_setup:
+	i40e_ptp_free_pins(pf);
 	i40e_reset_interrupt_capability(pf);
 	del_timer_sync(&pf->service_timer);
 err_mac_addr:
