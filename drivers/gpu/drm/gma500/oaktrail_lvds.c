da3461d5d34eeb4a69a06d8b0996e5c941e14016
Johan Hovold <johan@kernel.org>
2026-05-08 16:44:46 +0200

    drm/gma500/oaktrail_lvds: fix i2c adapter leaks on init
    commit 84d1c9b416d54afe760ca4c378bd95c89261254c upstream.
    
    The LVDS init code looks up an I2C adapter using i2c_get_adapter() and
    tries to read the EDID before falling back to allocating and registering
    its own adapter.
    
    Make sure to drop the references taken by i2c_get_adapter() when falling
    back to allocating an adapter as well as on late errors to allow the
    looked up adapter to be deregistered.
    
    Fixes: 1b082ccf5901 ("gma500: Add Oaktrail support")
    Cc: stable@vger.kernel.org	# 3.3
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
    Link: https://patch.msgid.link/20260508144446.59722-4-johan@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/gma500/oaktrail_lvds.c
+++ drivers/gpu/drm/gma500/oaktrail_lvds.c
@@ -366,6 +366,8 @@ void oaktrail_lvds_init(struct drm_device *dev,
 	if (edid == NULL && dev_priv->lpc_gpio_base) {
 		ddc_bus = oaktrail_lvds_i2c_init(dev);
 		if (!IS_ERR(ddc_bus)) {
+			if (i2c_adap)
+				i2c_put_adapter(i2c_adap);
 			i2c_adap = &ddc_bus->base;
 			edid = drm_get_edid(connector, i2c_adap);
 		}
@@ -422,6 +424,8 @@ err_unlock:
 	mutex_unlock(&dev->mode_config.mutex);
 	if (!IS_ERR_OR_NULL(ddc_bus))
 		gma_i2c_destroy(ddc_bus);
+	else if (i2c_adap)
+		i2c_put_adapter(i2c_adap);
 	drm_encoder_cleanup(encoder);
 err_connector_cleanup:
 	drm_connector_cleanup(connector);
