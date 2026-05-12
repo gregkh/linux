76410790f1491c8e06a451045ae223a61c652455
Sungwoo Kim <iam@sung-woo.kim>
2026-05-12 01:09:29 -0400

    block: bio-integrity: Fix null-ptr-deref in bio_integrity_map_user()
    [ Upstream commit 8582792cf23b3d94674d4d838f7cde9a28d0fcaf ]
    
    pin_user_pages_fast() can partially succeed and return the number of
    pages that were actually pinned. However, the bio_integrity_map_user()
    does not handle this partial pinning. This leads to a general protection
    fault since bvec_from_pages() dereferences an unpinned page address,
    which is 0.
    
    To fix this, add a check to verify that all requested memory is pinned.
    If partial pinning occurs, unpin the memory and return -EFAULT.
    
    Kernel Oops:
    
    Oops: general protection fault, probably for non-canonical address 0xdffffc0000000001: 0000 [#1] SMP KASAN NOPTI
    KASAN: null-ptr-deref in range [0x0000000000000008-0x000000000000000f]
    CPU: 0 UID: 0 PID: 1061 Comm: nvme-passthroug Not tainted 7.0.0-11783-g90957f9314e8-dirty #16 PREEMPT(lazy)
    Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS rel-1.17.0-0-gb52ca86e094d-prebuilt.qemu.org 04/01/2014
    RIP: 0010:bio_integrity_map_user.cold+0x1b0/0x9d6
    
    Fixes: 492c5d455969 ("block: bio-integrity: directly map user buffers")
    Acked-by: Chao Shi <cshi008@fiu.edu>
    Acked-by: Weidong Zhu <weizhu@fiu.edu>
    Acked-by: Dave Tian <daveti@purdue.edu>
    Signed-off-by: Sungwoo Kim <iam@sung-woo.kim>
    Tested-by: Shin'ichiro Kawasaki <shinichiro.kawasaki@wdc.com>
    Link: https://github.com/linux-blktests/blktests/pull/244
    Link: https://patch.msgid.link/20260512050929.541397-2-iam@sung-woo.kim
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- block/bio-integrity.c
+++ block/bio-integrity.c
@@ -299,6 +299,24 @@ int bio_integrity_map_user(struct bio *bio, struct iov_iter *iter)
 	if (unlikely(ret < 0))
 		goto free_bvec;
 
+	/*
+	 * Handle partial pinning. This can happen when pin_user_pages_fast()
+	 * returns fewer pages than requested.
+	 */
+	if (user_backed_iter(iter) && unlikely(ret != bytes)) {
+		if (ret > 0) {
+			int npinned = DIV_ROUND_UP(offset + ret, PAGE_SIZE);
+			int i;
+
+			for (i = 0; i < npinned; i++)
+				unpin_user_page(pages[i]);
+		}
+		if (pages != stack_pages)
+			kvfree(pages);
+		ret = -EFAULT;
+		goto free_bvec;
+	}
+
 	nr_bvecs = bvec_from_pages(bvec, pages, nr_vecs, bytes, offset,
 				   &is_p2p);
 	if (pages != stack_pages)
