222944afe4f91189d39d383c32cd72567a6083a1
Peter Chen <peter.chen@cixtech.com>
2026-05-13 16:53:09 +0800

    usb: cdns3: plat: fix leaked usb2_phy initialization on usb3_phy acquisition failure
    commit e6970cda63fd4b4546aeed9d0e2f53a7c95cd09c upstream.
    
    Move usb2_phy initialization after usb3_phy acquisition.
    
    Fixes: f738957277ba ("usb: cdns3: Split core.c into cdns3-plat and core.c file")
    Cc: stable <stable@kernel.org>
    Reported-by: sashiko-bot <sashiko-bot@kernel.org>
    Closes: https://lore.kernel.org/linux-devicetree/agKaEePSFknhDBg2@nchen-desktop/T/#m21e1d9c1574eb127ce03c0c2a1a49002ce435b52
    Signed-off-by: Peter Chen <peter.chen@cixtech.com>
    Link: https://patch.msgid.link/20260513085310.2217547-2-peter.chen@cixtech.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/cdns3/cdns3-plat.c
+++ drivers/usb/cdns3/cdns3-plat.c
@@ -126,14 +126,14 @@ static int cdns3_plat_probe(struct platform_device *pdev)
 		return dev_err_probe(dev, PTR_ERR(cdns->usb2_phy),
 				     "Failed to get cdn3,usb2-phy\n");
 
-	ret = phy_init(cdns->usb2_phy);
-	if (ret)
-		return ret;
-
 	cdns->usb3_phy = devm_phy_optional_get(dev, "cdns3,usb3-phy");
 	if (IS_ERR(cdns->usb3_phy))
 		return dev_err_probe(dev, PTR_ERR(cdns->usb3_phy),
 				     "Failed to get cdn3,usb3-phy\n");
+
+	ret = phy_init(cdns->usb2_phy);
+	if (ret)
+		return ret;
 
 	ret = phy_init(cdns->usb3_phy);
 	if (ret)
