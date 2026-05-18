cb4634cb537b2444c7995b2d2a7b652b5fc0c30e
Sasha Levin <sashal@kernel.org>
2026-05-18 10:51:32 -0400

    Revert "crypto: nx - fix context leak in nx842_crypto_free_ctx"
    This reverts commit b94588f5a69718be2c942f4a851125f655e4e819.
    
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/crypto/nx/nx-842.c
+++ drivers/crypto/nx/nx-842.c
@@ -115,7 +115,10 @@ void *nx842_crypto_alloc_ctx(struct nx842_driver *driver)
 	ctx->sbounce = (u8 *)__get_free_pages(GFP_KERNEL, BOUNCE_BUFFER_ORDER);
 	ctx->dbounce = (u8 *)__get_free_pages(GFP_KERNEL, BOUNCE_BUFFER_ORDER);
 	if (!ctx->wmem || !ctx->sbounce || !ctx->dbounce) {
-		nx842_crypto_free_ctx(ctx);
+		kfree(ctx->wmem);
+		free_pages((unsigned long)ctx->sbounce, BOUNCE_BUFFER_ORDER);
+		free_pages((unsigned long)ctx->dbounce, BOUNCE_BUFFER_ORDER);
+		kfree(ctx);
 		return ERR_PTR(-ENOMEM);
 	}
 
@@ -130,7 +133,6 @@ void nx842_crypto_free_ctx(void *p)
 	kfree(ctx->wmem);
 	free_pages((unsigned long)ctx->sbounce, BOUNCE_BUFFER_ORDER);
 	free_pages((unsigned long)ctx->dbounce, BOUNCE_BUFFER_ORDER);
-	kfree(ctx);
 }
 EXPORT_SYMBOL_GPL(nx842_crypto_free_ctx);
 
