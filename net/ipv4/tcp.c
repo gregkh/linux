4affe063fa56c880cbea8d0bfded0bb80751579d
Eric Dumazet <edumazet@google.com>
2026-05-19 08:46:11 +0000

    tcp: fix stale per-CPU tcp_tw_isn leak enabling ISN prediction
    [ Upstream commit 1bbf0ced1d9db73ac7893c2187f3459288603e0d ]
    
    Blamed commit moved the TIME_WAIT-derived ISN from the skb control
    block to a per-CPU variable, assuming the value would always be consumed
    by tcp_conn_request() for the same packet that wrote it. That assumption
    is violated by multiple drop paths between the producer
    (__this_cpu_write(tcp_tw_isn, isn) in tcp_v{4,6}_rcv()) and the consumer
    (tcp_conn_request()):
    
     - min_ttl / min_hopcount check
     - xfrm policy check
     - tcp_inbound_hash() MD5/AO mismatch
     - tcp_filter() eBPF/SO_ATTACH_FILTER drop
     - th->syn && th->fin discard in tcp_rcv_state_process() TCP_LISTEN
     - psp_sk_rx_policy_check() in tcp_v{4,6}_do_rcv()
     - tcp_checksum_complete() in tcp_v{4,6}_do_rcv()
     - tcp_v{4,6}_cookie_check() returning NULL
    
    When a packet is dropped on any of these paths, tcp_tw_isn is left set.
    
    The next SYN processed on the same CPU then consumes the non zero value in
    tcp_conn_request(), receiving a potentially predictable ISN.
    
    This patch moves back tcp_tw_isn to skb->cb[], getting rid of the per-cpu
    variable.
    
    Note that tcp_v{4,6}_fill_cb() do not set it.
    
    Very litle impact on overall code size/complexity:
    
    $ scripts/bloat-o-meter -t vmlinux.old vmlinux.new
    add/remove: 0/0 grow/shrink: 2/1 up/down: 8/-15 (-7)
    Function                                     old     new   delta
    tcp_v6_rcv                                  3038    3042      +4
    tcp_v4_rcv                                  3035    3039      +4
    tcp_conn_request                            2938    2923     -15
    Total: Before=24436060, After=24436053, chg -0.00%
    
    Fixes: 41eecbd712b7 ("tcp: replace TCP_SKB_CB(skb)->tcp_tw_isn with a per-cpu field")
    Reported-by: Chris Mason <clm@meta.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@google.com>
    Link: https://patch.msgid.link/20260519084611.2485277-1-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -299,9 +299,6 @@ enum {
 DEFINE_PER_CPU(unsigned int, tcp_orphan_count);
 EXPORT_PER_CPU_SYMBOL_GPL(tcp_orphan_count);
 
-DEFINE_PER_CPU(u32, tcp_tw_isn);
-EXPORT_PER_CPU_SYMBOL_GPL(tcp_tw_isn);
-
 long sysctl_tcp_mem[3] __read_mostly;
 EXPORT_IPV6_MOD(sysctl_tcp_mem);
 
--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -1818,6 +1818,7 @@ lookup:
 		}
 	}
 
+	isn = 0;
 process:
 	if (static_branch_unlikely(&ip6_min_hopcount)) {
 		/* min_hopcount can be changed concurrently from do_ipv6_setsockopt() */
@@ -1846,6 +1847,7 @@ process:
 	th = (const struct tcphdr *)skb->data;
 	hdr = ipv6_hdr(skb);
 	tcp_v6_fill_cb(skb, hdr, th);
+	TCP_SKB_CB(skb)->tcp_tw_isn = isn;
 
 	skb->dev = NULL;
 
@@ -1933,7 +1935,6 @@ do_time_wait:
 			sk = sk2;
 			tcp_v6_restore_cb(skb);
 			refcounted = false;
-			__this_cpu_write(tcp_tw_isn, isn);
 			goto process;
 		}
 
