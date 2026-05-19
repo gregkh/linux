55fb9581986141659002264fdc75cef307811eb8
Jian Zhang <zhangjian496@huawei.com>
2026-05-19 18:44:22 +0800

    cifs: remove all cifs files before kill super
    [ Upstream commit 6d9a4aaaa8b2612b5ef9d581e2f286a458b71ee1 ]
    
    Cifs files may be put into fileinfo_put_wq during umounting cifs.
    After umount done, cifsFileInfo_put_final is called, which cause
    following BUG:
    
    BUG: kernel NULL pointer dereference, address: 0000000000000000
    ...
    [  134.222152]  list_lru_add+0x64/0x1a0
    [  134.222399]  ? cifs_put_tcon+0x171/0x340 [cifs]
    [  134.222772]  d_lru_add+0x44/0x60
    [  134.222997]  dput+0x1fc/0x210
    [  134.223213]  cifsFileInfo_put_final+0x11a/0x140 [cifs]
    [  134.223576]  process_one_work+0x17c/0x320
    [  134.223843]  worker_thread+0x188/0x280
    [  134.224084]  ? __pfx_worker_thread+0x10/0x10
    [  134.224366]  kthread+0xcc/0x100
    [  134.224576]  ? __pfx_kthread+0x10/0x10
    [  134.224827]  ret_from_fork+0x30/0x50
    [  134.225063]  ? __pfx_kthread+0x10/0x10
    [  134.225328]  ret_from_fork_asm+0x1b/0x30
    
    This can be reproduce by following:
    unshare -n bash -c "
    mkdir -p ${CIFS_MNT}
    ip netns attach root 1
    ip link add eth0 type veth peer veth0 netns root
    ip link set eth0 up
    ip -n root link set veth0 up
    ip addr add 192.168.0.2/24 dev eth0
    ip -n root addr add 192.168.0.1/24 dev veth0
    ip route add default via 192.168.0.1 dev eth0
    ip netns exec root sysctl net.ipv4.ip_forward=1
    ip netns exec root iptables -t nat -A POSTROUTING -s 192.168.0.2 -o
    ${DEV} -j MASQUERADE
    mount -t cifs ${CIFS_PATH} ${CIFS_MNT} -o
    vers=3.0,sec=ntlmssp,credentials=${CIFS_CRED},rsize=65536,wsize=65536,cache=none,echo_interval=1
    touch ${CIFS_MNT}/a.txt
    ip netns exec root iptables -t nat -D POSTROUTING -s 192.168.0.2 -o
    ${DEV} -j MASQUERADE
    "
    umount ${CIFS_MNT}
    
    Fixes: 340cea84f691 ("cifs: open files should not hold ref on superblock")
    
    Signed-off-by: Jian Zhang <zhangjian496@huawei.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/smb/client/connect.c
+++ fs/smb/client/connect.c
@@ -4187,6 +4187,9 @@ cifs_umount(struct cifs_sb_info *cifs_sb)
 	}
 	spin_unlock(&cifs_sb->tlink_tree_lock);
 
+	flush_workqueue(serverclose_wq);
+	flush_workqueue(fileinfo_put_wq);
+
 	kfree(cifs_sb->prepath);
 #ifdef CONFIG_CIFS_DFS_UPCALL
 	dfs_cache_put_refsrv_sessions(&cifs_sb->dfs_mount_id);
