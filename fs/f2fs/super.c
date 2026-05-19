fac9cfad2f90fa21d81327028c7125889442c005
Chao Yu <chao@kernel.org>
2026-05-19 07:53:09 -0400

    f2fs: fix false alarm of lockdep on cp_global_sem lock
    [ Upstream commit 6a5e3de9c2bb0b691d16789a5d19e9276a09b308 ]
    
    lockdep reported a potential deadlock:
    
    a) TCMU device removal context:
     - call del_gendisk() to get q->q_usage_counter
     - call start_flush_work() to get work_completion of wb->dwork
    b) f2fs writeback context:
     - in wb_workfn(), which holds work_completion of wb->dwork
     - call f2fs_balance_fs() to get sbi->gc_lock
    c) f2fs vfs_write context:
     - call f2fs_gc() to get sbi->gc_lock
     - call f2fs_write_checkpoint() to get sbi->cp_global_sem
    d) f2fs mount context:
     - call recover_fsync_data() to get sbi->cp_global_sem
     - call f2fs_check_and_fix_write_pointer() to call blkdev_report_zones()
       that goes down to blk_mq_alloc_request and get q->q_usage_counter
    
    Original callstack is in Closes tag.
    
    However, I think this is a false alarm due to before mount returns
    successfully (context d), we can not access file therein via vfs_write
    (context c).
    
    Let's introduce per-sb cp_global_sem_key, and assign the key for
    cp_global_sem, so that lockdep can recognize cp_global_sem from
    different super block correctly.
    
    A lot of work are done by Shin'ichiro Kawasaki, thanks a lot for
    the work.
    
    Fixes: c426d99127b1 ("f2fs: Check write pointer consistency of open zones")
    Cc: stable@kernel.org
    Reported-and-tested-by: Shin'ichiro Kawasaki <shinichiro.kawasaki@wdc.com>
    Closes: https://lore.kernel.org/linux-f2fs-devel/20260218125237.3340441-1-shinichiro.kawasaki@wdc.com
    Signed-off-by: Shin'ichiro Kawasaki <shinichiro.kawasaki@wdc.com>
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    [ adapted context to use plain `init_f2fs_rwsem` instead of mainline's `init_f2fs_rwsem_trace` macro ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -4889,6 +4889,11 @@ try_onemore:
 	init_f2fs_rwsem(&sbi->gc_lock);
 	mutex_init(&sbi->writepages);
 	init_f2fs_rwsem(&sbi->cp_global_sem);
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
+	lockdep_register_key(&sbi->cp_global_sem_key);
+	lockdep_set_class(&sbi->cp_global_sem.internal_rwsem,
+					&sbi->cp_global_sem_key);
+#endif
 	init_f2fs_rwsem(&sbi->node_write);
 	init_f2fs_rwsem(&sbi->node_change);
 	spin_lock_init(&sbi->stat_lock);
@@ -5360,6 +5365,9 @@ free_options:
 free_sb_buf:
 	kfree(raw_super);
 free_sbi:
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
+	lockdep_unregister_key(&sbi->cp_global_sem_key);
+#endif
 	kfree(sbi);
 	sb->s_fs_info = NULL;
 
@@ -5441,6 +5449,9 @@ static void kill_f2fs_super(struct super_block *sb)
 	/* Release block devices last, after fscrypt_destroy_keyring(). */
 	if (sbi) {
 		destroy_device_list(sbi);
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
+		lockdep_unregister_key(&sbi->cp_global_sem_key);
+#endif
 		kfree(sbi);
 		sb->s_fs_info = NULL;
 	}
