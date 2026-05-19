4e3d1b2c48ca6c55f1e9ca7f8dccc76f120f276c
Jann Horn <jannh@google.com>
2026-05-19 16:40:34 +0200

    fuse: limit FUSE_NOTIFY_RETRIEVE to uptodate folios
    FUSE_NOTIFY_RETRIEVE must be limited to uptodate folios; !uptodate folios
    can contain uninitialized data.
    Since FUSE_NOTIFY_RETRIEVE is intended to only return data that is already
    in the page cache and not wait for data from the FUSE daemon, treat
    !uptodate folios as if they weren't present.
    
    This only has security impact on systems that don't enable automatic
    zero-initialization of all page allocations via
    CONFIG_INIT_ON_ALLOC_DEFAULT_ON or init_on_alloc=1.
    
    Cc: stable@kernel.org
    Fixes: 2d45ba381a74 ("fuse: add retrieve request")
    Signed-off-by: Jann Horn <jannh@google.com>
    Link: https://patch.msgid.link/20260519-fuse-retrieve-uptodate-v1-1-a7a1912a37f9@google.com
    Acked-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -1912,6 +1912,10 @@ static int fuse_retrieve(struct fuse_mount *fm, struct inode *inode,
 		folio = filemap_get_folio(mapping, index);
 		if (IS_ERR(folio))
 			break;
+		if (!folio_test_uptodate(folio)) {
+			folio_put(folio);
+			break;
+		}
 
 		folio_offset = offset_in_folio(folio, pos);
 		nr_bytes = min(folio_size(folio) - folio_offset, num);
