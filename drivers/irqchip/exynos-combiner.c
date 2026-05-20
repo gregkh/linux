96031b31a4b3b6ec836b9fe7be8f6e6ebcfe8d67
Marek Szyprowski <m.szyprowski@samsung.com>
2026-05-21 00:04:22 +0200

    irqchip/exynos-combiner: Switch to raw_spinlock
    The exynos-combiner driver uses a regular spinlock to protect access to
    the combiner interrupt status register in combiner_handle_cascade_irq(),
    which is invoked in hard interrupt context as a chained interrupt handler.
    
    When PREEMPT_RT is enabled on ARM, regular spinlock is converted to a
    sleeping lock (mutex-based), which must not be used in atomic context
    such as hard interrupt handlers.
    
    Switch the irq_controller_lock to raw_spinlock, which remains a true
    non-sleeping spinlock even under PREEMPT_RT.
    
    Fixes: a900e5d99718 ("ARM: exynos: move exynos4210-combiner to drivers/irqchip")
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>

--- drivers/irqchip/exynos-combiner.c
+++ drivers/irqchip/exynos-combiner.c
@@ -24,7 +24,7 @@
 
 #define IRQ_IN_COMBINER		8
 
-static DEFINE_SPINLOCK(irq_controller_lock);
+static DEFINE_RAW_SPINLOCK(irq_controller_lock);
 
 struct combiner_chip_data {
 	unsigned int hwirq_offset;
@@ -72,9 +72,9 @@ static void combiner_handle_cascade_irq(struct irq_desc *desc)
 
 	chained_irq_enter(chip, desc);
 
-	spin_lock(&irq_controller_lock);
+	raw_spin_lock(&irq_controller_lock);
 	status = readl_relaxed(chip_data->base + COMBINER_INT_STATUS);
-	spin_unlock(&irq_controller_lock);
+	raw_spin_unlock(&irq_controller_lock);
 	status &= chip_data->irq_mask;
 
 	if (status == 0)
