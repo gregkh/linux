ddab66c9a90d6c9d0fe65b1bce96b33392b7dc8a
Bart Van Assche <bvanassche@acm.org>
2026-05-21 10:33:29 -0700

    ata: libata: Fix ata_exec_internal()
    [ Upstream commit aa0ae1c35f7b3e9afed2324bed5f5c87ad55b92c ]
    
    Some but not all ata_exec_internal() calls happen from the context of
    the ATA error handler. Commit c0c362b60e25 ("libata: implement cross-port
    EH exclusion") added ata_eh_release() and ata_eh_acquire() calls in
    ata_exec_internal(). Calling these functions is necessary if the caller
    holds the eh_mutex but is not allowed if the caller doesn't hold that
    mutex. Fix this by only calling ata_eh_release() and ata_eh_acquire() if
    the caller holds the eh_mutex. An example of an indirect caller of
    ata_exec_internal() that does not hold the eh_mutex is
    ata_host_register().
    
    Fixes: c0c362b60e25 ("libata: implement cross-port EH exclusion")
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Reviewed-by: Niklas Cassel <cassel@kernel.org>
    Reviewed-by: Damien Le Moal <dlemoal@kernel.org>
    Reviewed-by: Hannes Reinecke <hare@kernel.org>
    Signed-off-by: Niklas Cassel <cassel@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -1508,6 +1508,7 @@ static unsigned ata_exec_internal_sg(struct ata_device *dev,
 {
 	struct ata_link *link = dev->link;
 	struct ata_port *ap = link->ap;
+	const bool owns_eh_mutex = ap->host->eh_owner == current;
 	u8 command = tf->command;
 	int auto_timeout = 0;
 	struct ata_queued_cmd *qc;
@@ -1586,11 +1587,25 @@ static unsigned ata_exec_internal_sg(struct ata_device *dev,
 		}
 	}
 
-	ata_eh_release(ap);
+	if (owns_eh_mutex) {
+		/*
+		 * To prevent that the compiler complains about the
+		 * ata_eh_release() call below.
+		 */
+		__acquire(&ap->host->eh_mutex);
+		ata_eh_release(ap);
+	}
 
 	rc = wait_for_completion_timeout(&wait, msecs_to_jiffies(timeout));
 
-	ata_eh_acquire(ap);
+	if (owns_eh_mutex) {
+		ata_eh_acquire(ap);
+		/*
+		 * To prevent that the compiler complains about the above
+		 * ata_eh_acquire() call.
+		 */
+		__release(&ap->host->eh_mutex);
+	}
 
 	ata_sff_flush_pio_task(ap);
 
