cf396941901858b0de426cdcd3974eea6a02c98c
Tejun Heo <tj@kernel.org>
2026-05-21 10:52:11 -0400

    sched_ext: Avoid UAF in scx_root_enable_workfn() init failure path
    [ Upstream commit 9a415cc53711f2238e0f0ca8a6bcc796c003b127 ]
    
    In scx_root_enable_workfn(), put_task_struct(p) is called before scx_error()
    dereferences p->comm and p->pid. If the iterator's reference is the last
    drop, the task is freed synchronously and the deref becomes a UAF.
    
    Move put_task_struct() past scx_error().
    
    Reported-by: Sashiko <sashiko-bot@kernel.org>
    Closes: https://lore.kernel.org/all/20260511214031.AF5E9C2BCB0@smtp.kernel.org/
    Fixes: f0e1a0643a59 ("sched_ext: Implement BPF extensible scheduler class")
    Cc: stable@vger.kernel.org # v6.12+
    Signed-off-by: Tejun Heo <tj@kernel.org>
    [ adapted fix to pre-refactor scx_ops_enable_workfn() with scx_task_iter_relock() instead of upstream scx_root_enable_workfn() ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/sched/ext.c
+++ kernel/sched/ext.c
@@ -5406,11 +5406,11 @@ static void scx_ops_enable_workfn(struct kthread_work *work)
 
 		ret = scx_ops_init_task(p, task_group(p), false);
 		if (ret) {
-			put_task_struct(p);
 			scx_task_iter_relock(&sti);
 			scx_task_iter_stop(&sti);
 			scx_ops_error("ops.init_task() failed (%d) for %s[%d]",
 				      ret, p->comm, p->pid);
+			put_task_struct(p);
 			goto err_disable_unlock_all;
 		}
 
