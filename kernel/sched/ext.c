4e17caa0e2a366303cb104a406709f554119d15b
Samuele Mariotti <smariotti@disroot.org>
2026-05-21 08:57:52 -0400

    sched_ext: Fix missing warning in scx_set_task_state() default case
    [ Upstream commit b905ee77d5f557a83a485b4146210f54f13365fc ]
    
    In scx_set_task_state(), the default case was setting the
    warn flag, but then returning immediately. This is problematic
    because the only purpose of the warn flag is to trigger
    WARN_ONCE, but the early return prevented it from ever firing,
    leaving invalid task states undetected and untraced.
    
    To fix this, a WARN_ONCE call is now added directly in the
    default case.
    
    The fix addresses two aspects:
    
     - Guarantees the invalid task states are properly logged
       and traced.
    
     - Provides a distinct warning message
       ("sched_ext: Invalid task state") specifically for
       states outside the defined scx_task_state enum values,
       making it easier to distinguish from other transition
       warnings.
    
    This ensures proper detection and reporting of invalid states.
    
    Signed-off-by: Samuele Mariotti <smariotti@disroot.org>
    Signed-off-by: Paolo Valente <paolo.valente@unimore.it>
    Reviewed-by: Andrea Righi <arighi@nvidia.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Stable-dep-of: 9a415cc53711 ("sched_ext: Avoid UAF in scx_root_enable_workfn() init failure path")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/sched/ext.c
+++ kernel/sched/ext.c
@@ -2936,7 +2936,8 @@ static void scx_set_task_state(struct task_struct *p, enum scx_task_state state)
 		warn = prev_state != SCX_TASK_READY;
 		break;
 	default:
-		warn = true;
+		WARN_ONCE(1, "sched_ext: Invalid task state %d -> %d for %s[%d]",
+			  prev_state, state, p->comm, p->pid);
 		return;
 	}
 
