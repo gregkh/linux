39c01c405063a8a8697f156dcbde8fcf011e98cd
Jakub Kicinski <kuba@kernel.org>
2026-05-22 16:06:44 -0700

    ethtool: rss: fix falsely ignoring indir table updates
    [ Upstream commit 8d60141a32875248ef71d49c9920fa5e2aa40b29 ]
    
    rss_set_prep_indir() compares the new indirection table against the
    current one to determine whether any update is needed. The memcmp
    call passes data->indir_size as the length argument, but indir_size
    is the number of u32 entries, not the byte count.
    
    Fixes: c0ae03588bbb ("ethtool: rss: initial RSS_SET (indirection table handling)")
    Link: https://patch.msgid.link/20260522230647.1705600-4-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/ethtool/rss.c
+++ net/ethtool/rss.c
@@ -684,7 +684,7 @@ rss_set_prep_indir(struct net_device *dev, struct genl_info *info,
 				ethtool_rxfh_indir_default(i, num_rx_rings);
 	}
 
-	*mod |= memcmp(rxfh->indir, data->indir_table, data->indir_size);
+	*mod |= memcmp(rxfh->indir, data->indir_table, alloc_size);
 
 	return 0;
 
