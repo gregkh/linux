3106f326f67c03dd9da4ca64663d11e40138cf40
Kuniyuki Iwashima <kuniyu@google.com>
2026-05-26 13:47:44 +0800

    af_unix: Give up GC if MSG_PEEK intervened.
    [ Upstream commit e5b31d988a41549037b8d8721a3c3cae893d8670 ]
    
    Igor Ushakov reported that GC purged the receive queue of
    an alive socket due to a race with MSG_PEEK with a nice repro.
    
    This is the exact same issue previously fixed by commit
    cbcf01128d0a ("af_unix: fix garbage collect vs MSG_PEEK").
    
    After GC was replaced with the current algorithm, the cited
    commit removed the locking dance in unix_peek_fds() and
    reintroduced the same issue.
    
    The problem is that MSG_PEEK bumps a file refcount without
    interacting with GC.
    
    Consider an SCC containing sk-A and sk-B, where sk-A is
    close()d but can be recv()ed via sk-B.
    
    The bad thing happens if sk-A is recv()ed with MSG_PEEK from
    sk-B and sk-B is close()d while GC is checking unix_vertex_dead()
    for sk-A and sk-B.
    
      GC thread                    User thread
      ---------                    -----------
      unix_vertex_dead(sk-A)
      -> true   <------.
                        \
                         `------   recv(sk-B, MSG_PEEK)
                  invalidate !!    -> sk-A's file refcount : 1 -> 2
    
                                   close(sk-B)
                                   -> sk-B's file refcount : 2 -> 1
      unix_vertex_dead(sk-B)
      -> true
    
    Initially, sk-A's file refcount is 1 by the inflight fd in sk-B
    recvq.  GC thinks sk-A is dead because the file refcount is the
    same as the number of its inflight fds.
    
    However, sk-A's file refcount is bumped silently by MSG_PEEK,
    which invalidates the previous evaluation.
    
    At this moment, sk-B's file refcount is 2; one by the open fd,
    and one by the inflight fd in sk-A.  The subsequent close()
    releases one refcount by the former.
    
    Finally, GC incorrectly concludes that both sk-A and sk-B are dead.
    
    One option is to restore the locking dance in unix_peek_fds(),
    but we can resolve this more elegantly thanks to the new algorithm.
    
    The point is that the issue does not occur without the subsequent
    close() and we actually do not need to synchronise MSG_PEEK with
    the dead SCC detection.
    
    When the issue occurs, close() and GC touch the same file refcount.
    If GC sees the refcount being decremented by close(), it can just
    give up garbage-collecting the SCC.
    
    Therefore, we only need to signal the race during MSG_PEEK with
    a proper memory barrier to make it visible to the GC.
    
    Let's use seqcount_t to notify GC when MSG_PEEK occurs and let
    it defer the SCC to the next run.
    
    This way no locking is needed on the MSG_PEEK side, and we can
    avoid imposing a penalty on every MSG_PEEK unnecessarily.
    
    Note that we can retry within unix_scc_dead() if MSG_PEEK is
    detected, but we do not do so to avoid hung task splat from
    abusive MSG_PEEK calls.
    
    Fixes: 118f457da9ed ("af_unix: Remove lock dance in unix_peek_fds().")
    Reported-by: Igor Ushakov <sysroot314@gmail.com>
    Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>
    Link: https://patch.msgid.link/20260311054043.1231316-1-kuniyu@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    [ Using include/net/af_unix.h instead of net/unix/af_unix.h on 6.6 ]
    Signed-off-by: Leon Chen <leonchen.oss@139.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -1848,6 +1848,8 @@ static void unix_detach_fds(struct scm_cookie *scm, struct sk_buff *skb)
 static void unix_peek_fds(struct scm_cookie *scm, struct sk_buff *skb)
 {
 	scm->fp = scm_fp_dup(UNIXCB(skb).fp);
+
+	unix_peek_fpl(scm->fp);
 }
 
 static void unix_destruct_scm(struct sk_buff *skb)
