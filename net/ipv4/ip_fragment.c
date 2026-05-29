543555954b1ee8d1903a7020324efb41b0c97428
Jakub Kicinski <kuba@kernel.org>
2026-05-29 17:29:52 +0800

    inet: frags: flush pending skbs in fqdir_pre_exit()
    [ Upstream commit 006a5035b495dec008805df249f92c22c89c3d2e ]
    
    We have been seeing occasional deadlocks on pernet_ops_rwsem since
    September in NIPA. The stuck task was usually modprobe (often loading
    a driver like ipvlan), trying to take the lock as a Writer.
    lockdep does not track readers for rwsems so the read wasn't obvious
    from the reports.
    
    On closer inspection the Reader holding the lock was conntrack looping
    forever in nf_conntrack_cleanup_net_list(). Based on past experience
    with occasional NIPA crashes I looked thru the tests which run before
    the crash and noticed that the crash follows ip_defrag.sh. An immediate
    red flag. Scouring thru (de)fragmentation queues reveals skbs sitting
    around, holding conntrack references.
    
    The problem is that since conntrack depends on nf_defrag_ipv6,
    nf_defrag_ipv6 will load first. Since nf_defrag_ipv6 loads first its
    netns exit hooks run _after_ conntrack's netns exit hook.
    
    Flush all fragment queue SKBs during fqdir_pre_exit() to release
    conntrack references before conntrack cleanup runs. Also flush
    the queues in timer expiry handlers when they discover fqdir->dead
    is set, in case packet sneaks in while we're running the pre_exit
    flush.
    
    The commit under Fixes is not exactly the culprit, but I think
    previously the timer firing would eventually unblock the spinning
    conntrack.
    
    Fixes: d5dd88794a13 ("inet: fix various use-after-free in defrags units")
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Link: https://patch.msgid.link/20251207010942.1672972-4-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Rajani Kantha <681739313@139.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/ipv4/ip_fragment.c
+++ net/ipv4/ip_fragment.c
@@ -148,11 +148,6 @@ static void ip_expire(struct timer_list *t)
 	net = qp->q.fqdir->net;
 
 	rcu_read_lock();
-
-	/* Paired with WRITE_ONCE() in fqdir_pre_exit(). */
-	if (READ_ONCE(qp->q.fqdir->dead))
-		goto out_rcu_unlock;
-
 	spin_lock(&qp->q.lock);
 
 	if (qp->q.flags & INET_FRAG_COMPLETE)
@@ -160,6 +155,13 @@ static void ip_expire(struct timer_list *t)
 
 	qp->q.flags |= INET_FRAG_DROP;
 	ipq_kill(qp);
+
+	/* Paired with WRITE_ONCE() in fqdir_pre_exit(). */
+	if (READ_ONCE(qp->q.fqdir->dead)) {
+		inet_frag_queue_flush(&qp->q, 0);
+		goto out;
+	}
+
 	__IP_INC_STATS(net, IPSTATS_MIB_REASMFAILS);
 	__IP_INC_STATS(net, IPSTATS_MIB_REASMTIMEOUT);
 
