03bf148af6954bd7cf64043ee258d46fd27da936
Bjorn Andersson <bjorn.andersson@oss.qualcomm.com>
2026-05-30 21:44:16 +0100

    slimbus: qcom-ngd-ctrl: Fix probe error path ordering
    [ Upstream commit 2c22ff152d380ec3d3af099fa05d0ac5ca9b4c1e ]
    
    qcom_slim_ngd_ctrl_probe() first registers the SSR callback then
    allocates the PDR context, as such the error path needs to come in
    opposite order to allow us to unroll each step.
    
    Fixes: 16f14551d0df ("slimbus: qcom-ngd: cleanup in probe error path")
    Cc: stable@vger.kernel.org
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Reviewed-by: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@oss.qualcomm.com>
    Signed-off-by: Srinivas Kandagatla <srini@kernel.org>
    Link: https://patch.msgid.link/20260530204421.116824-4-srini@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/slimbus/qcom-ngd-ctrl.c
+++ drivers/slimbus/qcom-ngd-ctrl.c
@@ -1577,23 +1577,22 @@ static int qcom_slim_ngd_ctrl_probe(struct platform_device *pdev)
 	if (IS_ERR(ctrl->pdr)) {
 		dev_err(dev, "Failed to init PDR handle\n");
 		ret = PTR_ERR(ctrl->pdr);
-		goto err_pdr_alloc;
+		goto err_unregister_ssr;
 	}
 
 	pds = pdr_add_lookup(ctrl->pdr, "avs/audio", "msm/adsp/audio_pd");
 	if (IS_ERR(pds) && PTR_ERR(pds) != -EALREADY) {
 		ret = PTR_ERR(pds);
 		dev_err(dev, "pdr add lookup failed: %d\n", ret);
-		goto err_pdr_lookup;
+		goto err_pdr_release;
 	}
 
 	return of_qcom_slim_ngd_register(dev, ctrl);
 
-err_pdr_alloc:
-	qcom_unregister_ssr_notifier(ctrl->notifier, &ctrl->nb);
-
-err_pdr_lookup:
+err_pdr_release:
 	pdr_handle_release(ctrl->pdr);
+err_unregister_ssr:
+	qcom_unregister_ssr_notifier(ctrl->notifier, &ctrl->nb);
 
 	return ret;
 }
