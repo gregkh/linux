1f96f8c0a6ed4f6d01d3dd29ad0cbf08dde96082
Carlos Llamas <cmllamas@google.com>
2026-06-19 18:52:30 +0000

    binder: fix UAF in binder_thread_release()
    commit 114a116aaa5f0295376cdf12da743c5bce3b20ce upstream.
    
    When a thread exits, binder_thread_release() walks its transaction stack
    to clear the t->from and t->to_proc that correspond with the exiting
    thread. However, a process dying in parallel might attempt to kfree some
    of these transactions. And if one of them has no associated t->to_proc,
    the t->to_proc->inner_lock will not be acquired.
    
    This means that transaction accesses in binder_thread_release() after
    t->to_proc has been cleared might race with binder_free_transaction()
    and cause a use-after-free error as reported by KASAN:
    
      ==================================================================
      BUG: KASAN: slab-use-after-free in binder_thread_release+0x5d0/0x798
      Write of size 8 at addr ffff000016627500 by task X/715
    
      CPU: 17 UID: 0 PID: 715 Comm: X Not tainted 7.1.0-rc5-00149-g8fde5d1d47f6 #30 PREEMPT
      Hardware name: linux,dummy-virt (DT)
      Call trace:
       binder_thread_release+0x5d0/0x798
       binder_ioctl+0x12c0/0x299c
       [...]
    
      Allocated by task 717 on cpu 18 at 67.267803s:
       __kasan_kmalloc+0xa0/0xbc
       __kmalloc_cache_noprof+0x174/0x444
       binder_transaction+0x554/0x8150
       binder_thread_write+0xa30/0x4354
       binder_ioctl+0x20f0/0x299c
       [...]
    
      Freed by task 202 on cpu 18 at 90.416221s:
       __kasan_slab_free+0x58/0x80
       kfree+0x1a0/0x4a4
       binder_free_transaction+0x150/0x294
       binder_send_failed_reply+0x398/0x6d8
       binder_release_work+0x3e4/0x4ec
       binder_deferred_func+0xbd8/0x104c
       [...]
      ==================================================================
    
    In order to avoid this, make sure that binder_free_transaction() reads
    the t->to_proc under the transaction lock. This will serialize the
    transaction release with the accesses in binder_thread_release(). Plus,
    it matches the documented locking rules for @to_proc.
    
    Cc: stable <stable@kernel.org>
    Fixes: 7a4408c6bd3e ("binder: make sure accesses to proc/thread are safe")
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    Link: https://patch.msgid.link/20260619185233.2194678-1-cmllamas@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -1909,7 +1909,11 @@ static void binder_free_txn_fixups(struct binder_transaction *t)
 
 static void binder_free_transaction(struct binder_transaction *t)
 {
-	struct binder_proc *target_proc = t->to_proc;
+	struct binder_proc *target_proc;
+
+	spin_lock(&t->lock);
+	target_proc = t->to_proc;
+	spin_unlock(&t->lock);
 
 	if (target_proc) {
 		binder_inner_proc_lock(target_proc);
