41c5b269af8b1f0bffcab7766a793f294ae6764e
Paul Moore <paul@paul-moore.com>
2026-06-27 14:57:19 +0800

    lsm: add backing_file LSM hooks
    [ Upstream commit 6af36aeb147a06dea47c49859cd6ca5659aeb987 ]
    
    Stacked filesystems such as overlayfs do not currently provide the
    necessary mechanisms for LSMs to properly enforce access controls on the
    mmap() and mprotect() operations.  In order to resolve this gap, a LSM
    security blob is being added to the backing_file struct and the following
    new LSM hooks are being created:
    
     security_backing_file_alloc()
     security_backing_file_free()
     security_mmap_backing_file()
    
    The first two hooks are to manage the lifecycle of the LSM security blob
    in the backing_file struct, while the third provides a new mmap() access
    control point for the underlying backing file.  It is also expected that
    LSMs will likely want to update their security_file_mprotect() callback
    to address issues with their mprotect() controls, but that does not
    require a change to the security_file_mprotect() LSM hook.
    
    There are a three other small changes to support these new LSM hooks:
    * Pass the user file associated with a backing file down to
    alloc_empty_backing_file() so it can be included in the
    security_backing_file_alloc() hook.
    * Add getter and setter functions for the backing_file struct LSM blob
    as the backing_file struct remains private to fs/file_table.c.
    * Constify the file struct field in the LSM common_audit_data struct to
    better support LSMs that need to pass a const file struct pointer into
    the common LSM audit code.
    
    Thanks to Arnd Bergmann for identifying the missing EXPORT_SYMBOL_GPL()
    and supplying a fixup.
    
    Cc: stable@vger.kernel.org
    Cc: linux-fsdevel@vger.kernel.org
    Cc: linux-unionfs@vger.kernel.org
    Cc: linux-erofs@lists.ozlabs.org
    Reviewed-by: Amir Goldstein <amir73il@gmail.com>
    Reviewed-by: Serge Hallyn <serge@hallyn.com>
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    [1. Mainline uses call_int_hook(FUNC, ...) with the default IRC baked
    into the macro. Linux 6.6.y uses call_int_hook(FUNC, IRC, ...) requiring
    an explicit default return value.
    
    2. fs/backing-file.c does not exist in LTS
    Linux 6.6.y places backing_file_open() in fs/open.c and lacks a
    dedicated fs/backing-file.c.  The backing_file_mmap() function and
    scoped_with_creds() do not exist in 6.6.y.  Therefore the LTS patch calls
    security_mmap_backing_file() directly in ovl_mmap() in
    fs/overlayfs/file.c rather than modifying backing_file_mmap().
    
    3. Missing filesystems/modules
    Linux 6.6.y does not have backing_tmpfile_open(), fs/fuse/passthrough.c,
    or the erofs ishare mmap path that the mainline patch touches.  These hunks
    are dropped in the 6.6 LTS backport.
    
    4. Use macro backing_file to replace inline function to eliminate the
    const warning.]
    Signed-off-by: Cai Xinchen <caixinchen1@huawei.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/file_table.c
+++ fs/file_table.c
@@ -48,12 +48,12 @@ static struct percpu_counter nr_files __cacheline_aligned_in_smp;
 struct backing_file {
 	struct file file;
 	struct path real_path;
+#ifdef CONFIG_SECURITY
+	void *security;
+#endif
 };
 
-static inline struct backing_file *backing_file(struct file *f)
-{
-	return container_of(f, struct backing_file, file);
-}
+#define backing_file(f) container_of(f, struct backing_file, file)
 
 struct path *backing_file_real_path(struct file *f)
 {
@@ -72,8 +72,21 @@ static void file_free_rcu(struct rcu_head *head)
 		kmem_cache_free(filp_cachep, f);
 }
 
+#ifdef CONFIG_SECURITY
+void *backing_file_security(const struct file *f)
+{
+	return backing_file(f)->security;
+}
+
+void backing_file_set_security(struct file *f, void *security)
+{
+	backing_file(f)->security = security;
+}
+#endif /* CONFIG_SECURITY */
+
 static inline void backing_file_free(struct backing_file *ff)
 {
+	security_backing_file_free(&ff->file);
 	path_put(&ff->real_path);
 }
 
@@ -257,10 +270,12 @@ struct file *alloc_empty_file_noaccount(int flags, const struct cred *cred)
 	return f;
 }
 
-static int init_backing_file(struct backing_file *ff)
+static int init_backing_file(struct backing_file *ff,
+			     const struct file *user_file)
 {
 	memset(&ff->real_path, 0, sizeof(ff->real_path));
-	return 0;
+	backing_file_set_security(&ff->file, NULL);
+	return security_backing_file_alloc(&ff->file, user_file);
 }
 
 /*
@@ -270,7 +285,8 @@ static int init_backing_file(struct backing_file *ff)
  * This is only for kernel internal use, and the allocate file must not be
  * installed into file tables or such.
  */
-struct file *alloc_empty_backing_file(int flags, const struct cred *cred)
+struct file *alloc_empty_backing_file(int flags, const struct cred *cred,
+				      const struct file *user_file)
 {
 	struct backing_file *ff;
 	int error;
@@ -287,7 +303,7 @@ struct file *alloc_empty_backing_file(int flags, const struct cred *cred)
 
 	/* The f_mode flags must be set before fput(). */
 	ff->file.f_mode |= FMODE_BACKING | FMODE_NOACCOUNT;
-	error = init_backing_file(ff);
+	error = init_backing_file(ff, user_file);
 	if (unlikely(error)) {
 		fput(&ff->file);
 		return ERR_PTR(error);
