50cc5db9f9d29696c04031eadaf633e23b051d5a
Longjun Tang <tanglongjun@kylinos.cn>
2026-06-29 10:42:30 +0800

    virtio_net: disable cb when NAPI is busy-polled
    [ Upstream commit 1eb8fc67ca41db71c90866ff76c990d85247daef ]
    
    When busy-poll is active, napi_schedule_prep() returns false in
    virtqueue_napi_schedule(), so virtqueue_disable_cb() is skipped.
    The device may keep firing irqs until reaches virtqueue_napi_complete().
    Under load (received == budget), it will lead to a large number
    of spurious interrupts.
    
    Fix it by disabling the callback at the virtnet_poll() entry.
    This keeps the callback off while we poll and it is re-enabled by
    virtqueue_napi_complete() when going idle.
    
    Fixes: ceef438d613f ("virtio_net: remove custom busy_poll")
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Longjun Tang <tanglongjun@kylinos.cn>
    Link: https://patch.msgid.link/20260629024230.37325-1-lange_tang@163.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -1612,6 +1612,9 @@ static int virtnet_poll(struct napi_struct *napi, int budget)
 	unsigned int received;
 	unsigned int xdp_xmit = 0;
 
+	if (budget)
+		virtqueue_disable_cb(rq->vq);
+
 	virtnet_poll_cleantx(rq, budget);
 
 	received = virtnet_receive(rq, budget, &xdp_xmit);
