9641d09dfbe0349022e0436e53286175acfe82b2
Wei Wang <wei.w.wang@hotmail.com>
2026-07-02 22:40:18 +0800

    iommu/amd: Prevent SB IOAPIC from overriding IVRS validation errors
    [ Upstream commit 854056480f9217568e3ab5edd81a9347a173ea79 ]
    
    The check_ioapic_information() function validates IOAPICs against the
    IVRS table to safely disable Interrupt Remapping (IR) if the BIOS provides
    a broken topology.
    
    Currently, the validation loop contains a bug: If an unmapped secondary
    IOAPIC is encountered, 'ret' is set to false. But if the Southbridge (SB)
    IOAPIC is enumerated after it in the MADT, the loop overwrites 'ret' to
    true.
    
    This bypasses the validation failure and leaves IR enabled. When devices
    attached to the unmapped secondary IOAPIC fire interrupts, the IOMMU drops
    them due to the missing Requestor ID, leading to localized device hangs.
    
    Fix this by initializing 'ret' to true and only toggling it to false
    upon encountering a validation error, ensuring failures are never erased.
    
    Fixes: c2ff5cf5294b ("iommu/amd: Work around wrong IOAPIC device-id in IVRS table")
    Signed-off-by: Wei Wang <wei.w.wang@hotmail.com>
    Tested-by: Yongwei Xu <xuyongwei@open-hieco.net>
    Reviewed-by: Vasant Hegde <vasant.hegde@amd.com>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/iommu/amd/init.c
+++ drivers/iommu/amd/init.c
@@ -2669,7 +2669,7 @@ static bool __init check_ioapic_information(void)
 	int idx;
 
 	has_sb_ioapic = false;
-	ret           = false;
+	ret           = true;
 
 	/*
 	 * If we have map overrides on the kernel command line the
@@ -2689,7 +2689,6 @@ static bool __init check_ioapic_information(void)
 			ret = false;
 		} else if (devid == IOAPIC_SB_DEVID) {
 			has_sb_ioapic = true;
-			ret           = true;
 		}
 	}
 
@@ -2703,6 +2702,7 @@ static bool __init check_ioapic_information(void)
 		 * device id for the IOAPIC in the system.
 		 */
 		pr_err("%s: No southbridge IOAPIC found\n", fw_bug);
+		ret = false;
 	}
 
 	if (!ret)
