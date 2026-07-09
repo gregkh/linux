6e5714bcc98f3959794954287f5c3e16d2eebaaf
Ahmed Genidi <ahmed.genidi@arm.com>
2026-07-09 22:36:02 +0000

    KVM: arm64: Initialize SCTLR_EL1 in __kvm_hyp_init_cpu()
    [ Upstream commit 3855a7b91d42ebf3513b7ccffc44807274978b3d ]
    
    When KVM is in protected mode, host calls to PSCI are proxied via EL2,
    and cold entries from CPU_ON, CPU_SUSPEND, and SYSTEM_SUSPEND bounce
    through __kvm_hyp_init_cpu() at EL2 before entering the host kernel's
    entry point at EL1. While __kvm_hyp_init_cpu() initializes SPSR_EL2 for
    the exception return to EL1, it does not initialize SCTLR_EL1.
    
    Due to this, it's possible to enter EL1 with SCTLR_EL1 in an UNKNOWN
    state. In practice this has been seen to result in kernel crashes after
    CPU_ON as a result of SCTLR_EL1.M being 1 in violation of the initial
    core configuration specified by PSCI.
    
    Fix this by initializing SCTLR_EL1 for cold entry to the host kernel.
    As it's necessary to write to SCTLR_EL12 in VHE mode, this
    initialization is moved into __kvm_host_psci_cpu_entry() where we can
    use write_sysreg_el1().
    
    The remnants of the '__init_el2_nvhe_prepare_eret' macro are folded into
    its only caller, as this is clearer than having the macro.
    
    Fixes: cdf367192766ad11 ("KVM: arm64: Intercept host's CPU_ON SMCs")
    Reported-by: Leo Yan <leo.yan@arm.com>
    Signed-off-by: Ahmed Genidi <ahmed.genidi@arm.com>
    [ Mark: clarify commit message, handle E2H, move to C, remove macro ]
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Ahmed Genidi <ahmed.genidi@arm.com>
    Cc: Ben Horgan <ben.horgan@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Leo Yan <leo.yan@arm.com>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Oliver Upton <oliver.upton@linux.dev>
    Cc: Will Deacon <will@kernel.org>
    Reviewed-by: Leo Yan <leo.yan@arm.com>
    Link: https://lore.kernel.org/r/20250227180526.1204723-3-mark.rutland@arm.com
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    [ Backport: Resolved context conflicts when removing the
      __init_el2_nvhe_prepare_eret macro and invocation:
      - arch/arm64/include/asm/el2_setup.h: conflicted because 6.6.y lacks later
        GCS/MPAM macros (__init_el2_gcs / __init_el2_mpam) surrounding the definition.
      - arch/arm64/kvm/hyp/nvhe/hyp-init.S: conflicted because __kvm_init_el2_state
        does not exist in 6.6.y (EL2 state is initialized inline). ]
    Signed-off-by: Colton Lewis <coltonlewis@google.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/arm64/include/asm/el2_setup.h
+++ arch/arm64/include/asm/el2_setup.h
@@ -229,10 +229,6 @@
 .Lskip_fgt_\@:
 .endm
 
-.macro __init_el2_nvhe_prepare_eret
-	mov	x0, #INIT_PSTATE_EL1
-	msr	spsr_el2, x0
-.endm
 
 /**
  * Initialize EL2 registers to sane values. This should be called early on all
