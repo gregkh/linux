8ee84f9b64fd0617def6d0cda3558da66b9bdc0f
Sean Christopherson <seanjc@google.com>
2026-07-09 13:49:34 -0700

    KVM: SEV: Wire up kvm_x86_ops.gmem_xxx() if and only if CONFIG_KVM_AMD_SEV=y
    commit 01a96ff30dde5127c37497f1e098e639e7ae152f upstream.
    
    Wire up the SEV-SNP guest_memfd kvm_x86_ops hooks if and only if SEV is
    actually enabled, and drop the now-unnecessary stubs.  Leaving the hooks
    NULL allows the static call infrastructure to elide the CALL+RET, and more
    importantly, referencing the hooks if and only if SEV support is enabled
    will allow conditionally definining the hooks using their corresponding
    HAVE_KVM_ARCH_GMEM_XXX Kconfig.
    
    No functional change intended.
    
    Cc: stable@vger.kernel.org # 6.12.x
    Reviewed-by: Ackerley Tng <ackerleytng@google.com>
    Link: https://patch.msgid.link/20260709204948.1988414-5-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -993,16 +993,6 @@ static inline int sev_cpu_init(struct svm_cpu_data *sd) { return 0; }
 static inline int sev_dev_get_attr(u32 group, u64 attr, u64 *val) { return -ENXIO; }
 #define max_sev_asid 0
 static inline void sev_handle_rmp_fault(struct kvm_vcpu *vcpu, gpa_t gpa, u64 error_code) {}
-static inline int sev_gmem_prepare(struct kvm *kvm, kvm_pfn_t pfn, gfn_t gfn, int max_order)
-{
-	return 0;
-}
-static inline void sev_gmem_invalidate(kvm_pfn_t start, kvm_pfn_t end) {}
-static inline int sev_gmem_max_mapping_level(struct kvm *kvm, kvm_pfn_t pfn, bool is_private)
-{
-	return 0;
-}
-
 static inline struct vmcb_save_area *sev_decrypt_vmsa(struct kvm_vcpu *vcpu)
 {
 	return NULL;
