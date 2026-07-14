1edd524de5cc8143ece9c42c466346983dc5b5ed
Johan Hovold <johan@kernel.org>
2026-07-14 09:26:50 -0400

    Bluetooth: btusb: fix use-after-free on marvell probe failure
    [ Upstream commit c5b600a3c05b1a7a110d558df935a8fc8a471c79 ]
    
    Make sure to stop any TX URBs submitted during Marvell OOB wakeup
    configuration on later probe failures to avoid use-after-free in the
    completion callback.
    
    This issue was reported by Sashiko while reviewing a fix for a wakeup
    source leak in the btusb probe errors paths.
    
    Link: https://sashiko.dev/#/patchset/20260402092704.2346710-1-johan%40kernel.org
    Fixes: a4ccc9e33d2f ("Bluetooth: btusb: Configure Marvell to use one of the pins for oob wakeup")
    Cc: stable@vger.kernel.org	# 4.11
    Cc: Rajat Jain <rajatja@google.com>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -4352,7 +4352,7 @@ static int btusb_probe(struct usb_interface *intf,
 		err = usb_set_interface(data->udev, 0, 0);
 		if (err < 0) {
 			BT_ERR("failed to set interface 0, alt 0 %d", err);
-			goto out_free_dev;
+			goto err_kill_tx_urbs;
 		}
 	}
 
@@ -4360,7 +4360,7 @@ static int btusb_probe(struct usb_interface *intf,
 		err = usb_driver_claim_interface(&btusb_driver,
 						 data->isoc, data);
 		if (err < 0)
-			goto out_free_dev;
+			goto err_kill_tx_urbs;
 	}
 
 	if (IS_ENABLED(CONFIG_BT_HCIBTUSB_BCM) && data->diag) {
@@ -4391,6 +4391,8 @@ err_release_siblings:
 		usb_set_intfdata(data->isoc, NULL);
 		usb_driver_release_interface(&btusb_driver, data->isoc);
 	}
+err_kill_tx_urbs:
+	usb_kill_anchored_urbs(&data->tx_anchor);
 out_free_dev:
 	if (data->reset_gpio)
 		gpiod_put(data->reset_gpio);
