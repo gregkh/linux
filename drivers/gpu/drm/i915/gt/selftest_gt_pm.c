03db3a2d01006efc6e551a02ff21e5ef4d47672f
Emre Cecanpunar <emreleno@gmail.com>
2026-07-15 01:04:30 +0300

    drm/i915/selftests: Fix GT PM sort comparators
    [ Upstream commit 612978b83f45bf7018815209db5395d759db6f26 ]
    
    Compare the sampled clock values instead of their addresses. Comparing
    addresses leaves the samples unsorted, preventing the code from discarding
    the minimum and maximum samples.
    
    Fixes: 1a5392479207 ("drm/i915/selftests: Measure CS_TIMESTAMP")
    Signed-off-by: Emre Cecanpunar <emreleno@gmail.com>
    Signed-off-by: Tvrtko Ursulin <tursulin@ursulin.net>
    Link: https://lore.kernel.org/r/20260714220430.238433-1-emreleno@gmail.com
    (cherry picked from commit 682ea2d28d18bb06f9fc663cb5ab7e80dc0e606a)
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/i915/gt/selftest_gt_pm.c
+++ drivers/gpu/drm/i915/gt/selftest_gt_pm.c
@@ -15,9 +15,9 @@ static int cmp_u64(const void *A, const void *B)
 {
 	const u64 *a = A, *b = B;
 
-	if (a < b)
+	if (*a < *b)
 		return -1;
-	else if (a > b)
+	else if (*a > *b)
 		return 1;
 	else
 		return 0;
@@ -27,9 +27,9 @@ static int cmp_u32(const void *A, const void *B)
 {
 	const u32 *a = A, *b = B;
 
-	if (a < b)
+	if (*a < *b)
 		return -1;
-	else if (a > b)
+	else if (*a > *b)
 		return 1;
 	else
 		return 0;
