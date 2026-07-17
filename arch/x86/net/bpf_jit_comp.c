3b3b23c80202cf1ceeedc789df22e2f665f486a6
Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
2026-07-17 10:06:16 -0700

    bpf: Restrict JIT predictor flush to cBPF
    commit 0bb99f2cfaae6822d734d69722de30af823efdf3 upstream.
    
    Currently predictor flush on memory reuse is done for all BPF JIT
    allocations, but only cBPF programs can be loaded by an unprivileged user.
    eBPF is privileged by default, and flushing predictors for all CPUs on
    every eBPF reuse penalizes the common case for no security benefit.
    
    eBPF allocations can be frequent on busy systems, only flush predictors
    for cBPF programs. Trampoline and dispatcher allocations also skip the
    flush as they are eBPF-only.
    
      [pawan: backport dropped "was_classic" hunk for arches that do not
              support pack allocator]
    
    Signed-off-by: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/x86/net/bpf_jit_comp.c
+++ arch/x86/net/bpf_jit_comp.c
@@ -2927,7 +2927,8 @@ out_image:
 			/* allocate module memory for x86 insns and extable */
 			header = bpf_jit_binary_pack_alloc(roundup(proglen, align) + extable_size,
 							   &image, align, &rw_header, &rw_image,
-							   jit_fill_hole);
+							   jit_fill_hole,
+							   bpf_prog_was_classic(prog));
 			if (!header) {
 				prog = orig_prog;
 				goto out_addrs;
