4cc1bb9569930b2af7fd93ff1036e13b2de3c942
Iván Ezequiel Rodriguez <ivanrwcm25@gmail.com>
2026-07-23 15:12:59 -0500

    ntsync: reject wait ioctls with zero owner
    commit 61611481f7b599e0526a3782c626b1a9deeb48bd upstream.
    
    setup_wait() already validates pad and flags but not owner, while
    Documentation/userspace-api/ntsync.rst requires EINVAL when owner is
    zero. Reject early before queueing waiters.
    
    Signed-off-by: Iván Ezequiel Rodriguez <ivanrwcm25@gmail.com>
    Signed-off-by: Griffin Kroah-Hartman <griffin@kroah.com>
    Reviewed-by: Elizabeth Figura <zfigura@codeweavers.com>
    Signed-off-by: Elizabeth Figura <zfigura@codeweavers.com>
    Link: https://patch.msgid.link/20260723201301.11826-4-zfigura@codeweavers.com
    Cc: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/ntsync.c
+++ drivers/misc/ntsync.c
@@ -884,6 +884,9 @@ static int setup_wait(struct ntsync_device *dev,
 	if (args->pad || (args->flags & ~NTSYNC_WAIT_REALTIME))
 		return -EINVAL;
 
+	if (!args->owner)
+		return -EINVAL;
+
 	if (size >= sizeof(fds))
 		return -EINVAL;
 
