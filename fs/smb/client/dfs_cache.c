14f60e959b5ac813715ff72e127beb3584ddabf6
Fredric Cover <fredric.cover.lkernel@gmail.com>
2026-07-24 15:01:46 -0700

    smb: client: clear ce->tgthint in free_tgts()
    commit b1b741cf8e7ce1b91d937e23decd3d3358748700 upstream.
    
    When free_tgts() frees all structures in ce->tlist, ce->tgthint
    is left pointing to one of the freed cache_dfs_tgt structures.
    
    If ce->tgthint is not reset before it is used later, it results
    in a use-after-free.
    
    Set ce->tgthint to NULL in free_tgts() after the elements are
    freed to reflect that no elements remain.
    
    Fixes: 54be1f6c1c37 ("cifs: Add DFS cache routines")
    Cc: stable@vger.kernel.org # depends on: smb: client: harden DFS cache against invalid target hints
    Signed-off-by: Fredric Cover <fredric.cover.lkernel@gmail.com>
    Reviewed-by: ChenXiaoSong <chenxiaosong@kylinos.cn>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Paulo Alcantara <pc@manguebit.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/smb/client/dfs_cache.c
+++ fs/smb/client/dfs_cache.c
@@ -234,6 +234,8 @@ static inline void free_tgts(struct cache_entry *ce)
 		kfree(t->name);
 		kfree(t);
 	}
+
+	WRITE_ONCE(ce->tgthint, NULL);
 }
 
 static inline void flush_cache_ent(struct cache_entry *ce)
