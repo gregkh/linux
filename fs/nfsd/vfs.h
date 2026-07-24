43ffd230b9cc1bdf9be4c2c7242d55b7df37e8ae
Mike Snitzer <snitzer@kernel.org>
2026-07-23 21:25:11 -0400

    NFSD: pass nfsd_file to nfsd_iter_read()
    [ Upstream commit 803bc849f0039291f546ba0e2237faebeb5c073e ]
    
    Prepare for nfsd_iter_read() to use the DIO alignment stored in
    nfsd_file by passing the nfsd_file to nfsd_iter_read() rather than
    just the file which is associaed with the nfsd_file.
    
    This means nfsd4_encode_readv() now also needs the nfsd_file rather
    than the file.  Instead of changing the file arg to be the nfsd_file,
    we discard the file arg as the nfsd_file (and indeed the file) is
    already available via the "read" argument.
    
    Signed-off-by: Mike Snitzer <snitzer@kernel.org>
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Reviewed-by: NeilBrown <neil@brown.name>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Stable-dep-of: 18c1cc698861 ("SUNRPC: Return an error from xdr_buf_to_bvec() on overflow")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/nfsd/vfs.h
+++ fs/nfsd/vfs.h
@@ -120,7 +120,7 @@ __be32		nfsd_splice_read(struct svc_rqst *rqstp, struct svc_fh *fhp,
 				unsigned long *count,
 				u32 *eof);
 __be32		nfsd_iter_read(struct svc_rqst *rqstp, struct svc_fh *fhp,
-				struct file *file, loff_t offset,
+				struct nfsd_file *nf, loff_t offset,
 				unsigned long *count, unsigned int base,
 				u32 *eof);
 __be32		nfsd_read(struct svc_rqst *rqstp, struct svc_fh *fhp,
