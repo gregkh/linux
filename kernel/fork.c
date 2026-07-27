7df67a4799067a59e6a2d53f8059a6be6e73e678
Jann Horn <jannh@google.com>
2026-07-27 12:02:22 -0400

    bpf,fork: wipe ->bpf_storage before bailouts that access it
    [ Upstream commit 9b51a6155d14389876916726430da30eabb1d4ed ]
    
    Currently, copy_process() can bail out to free_task() before p->bpf_storage
    has been initialized, with this call graph (shown here for the
    !CONFIG_MEMCG case):
    
    copy_process
      dup_task_struct
        arch_dup_task_struct
          [copies the entire task_struct, including ->bpf_storage member]
      [RLIMIT_NPROC check fails]
      delayed_free_task
        free_task
          bpf_task_storage_free
            rcu_dereference(task->bpf_storage)
            bpf_local_storage_destroy
    
    In this case, the nascent task's ->bpf_storage member that
    bpf_local_storage_destroy() operates on is a plain copy of the parent's
    ->bpf_storage pointer, not a real initialized pointer.
    This leads to badness (kernel hangs, UAF).
    
    This is reachable as long as the process calling fork() has been inserted
    into a task storage map.
    
    Cc: stable@kernel.org
    Fixes: a10787e6d58c ("bpf: Enable task local storage for tracing programs")
    Signed-off-by: Jann Horn <jannh@google.com>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -983,6 +983,11 @@ static struct task_struct *dup_task_struct(struct task_struct *orig, int node)
 #ifdef CONFIG_MEMCG
 	tsk->active_memcg = NULL;
 #endif
+
+#ifdef CONFIG_BPF_SYSCALL
+	RCU_INIT_POINTER(tsk->bpf_storage, NULL);
+	tsk->bpf_ctx = NULL;
+#endif
 	return tsk;
 
 free_stack:
@@ -2251,10 +2256,6 @@ static __latent_entropy struct task_struct *copy_process(
 #ifdef CONFIG_BCACHE
 	p->sequential_io	= 0;
 	p->sequential_io_avg	= 0;
-#endif
-#ifdef CONFIG_BPF_SYSCALL
-	RCU_INIT_POINTER(p->bpf_storage, NULL);
-	p->bpf_ctx = NULL;
 #endif
 
 	/* Perform scheduler related setup. Assign this task to a CPU. */
