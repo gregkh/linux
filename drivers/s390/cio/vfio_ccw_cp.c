15fb4559a7fdf0b8725e433a71cfd03a1313a48b
Eric Farman <farman@linux.ibm.com>
2026-07-28 05:30:14 +0200

    s390/vfio_ccw: Limit the number of channel program segments
    commit 5405c90d6a47b3014e74ee0618a162449abbbc93 upstream.
    
    The processing of channel programs, and the CCWs within them, is done
    recursively. As such, there is an arbitrary (but not architectural)
    limit to the number of CCWs that can exist in a single channel program.
    
    The vfio-ccw logic breaks these channel programs into segments whenever
    it encounters a Transfer-In-Channel (TIC) CCW, and the combined number
    of segments count towards the global limit. Impose an equivalent limit
    to the number of segments until such logic can be made non-recursive.
    
    Fixes: 0a19e61e6d4c ("vfio: ccw: introduce channel program interfaces")
    Cc: stable@vger.kernel.org
    Reviewed-by: Matthew Rosato <mjrosato@linux.ibm.com>
    Signed-off-by: Eric Farman <farman@linux.ibm.com>
    Signed-off-by: Christian Borntraeger <borntraeger@linux.ibm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/s390/cio/vfio_ccw_cp.c
+++ drivers/s390/cio/vfio_ccw_cp.c
@@ -331,6 +331,7 @@ static struct ccwchain *ccwchain_alloc(struct channel_program *cp, int len)
 		goto out_err;
 
 	list_add_tail(&chain->next, &cp->ccwchain_list);
+	cp->ccwchain_count++;
 
 	return chain;
 
@@ -437,6 +438,10 @@ static int ccwchain_handle_ccw(u32 cda, struct channel_program *cp)
 	len = ccwchain_calc_length(cda, cp);
 	if (len < 0)
 		return len;
+
+	/* Limit number of chains in a single channel program */
+	if (cp->ccwchain_count >= CCWCHAIN_COUNT_MAX)
+		return -EINVAL;
 
 	/* Need alloc a new chain for this one. */
 	chain = ccwchain_alloc(cp, len);
@@ -725,6 +730,7 @@ int cp_init(struct channel_program *cp, union orb *orb)
 			vdev->dev,
 			"Prefetching channel program even though prefetch not specified in ORB");
 
+	cp->ccwchain_count = 0;
 	INIT_LIST_HEAD(&cp->ccwchain_list);
 	memcpy(&cp->orb, orb, sizeof(*orb));
 
