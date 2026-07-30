c0a83f29a24c7e7f8516630848ef6db4c174deed
Cheng Xu <chengyou@linux.alibaba.com>
2026-07-30 20:43:53 +0800

    RDMA/erdma: Hold CQ references when processing EQ events
    [ Upstream commit 98df2aee1459ee1c62c70cbe9b370d2a532aea36 ]
    
    EQ handlers look up CQs from dev->cq_xa and invoke CQ completion or
    error callbacks outside the xarray lock. erdma_destroy_cq() can erase the
    CQ from the xarray and free its queue buffer and doorbell record while a
    previously scheduled EQ handler is still using the CQ.
    
    Add a CQ refcount and take a reference under the xarray lock with
    refcount_inc_not_zero(). Remove the CQ from the xarray before dropping
    the destroy-path reference, then wait for in-flight EQ users before
    releasing CQ resources.
    
    Fixes: 155055771704 ("RDMA/erdma: Add verbs implementation")
    Signed-off-by: Cheng Xu <chengyou@linux.alibaba.com>
    Link: https://patch.msgid.link/20260730124357.12976-1-chengyou@linux.alibaba.com
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/infiniband/hw/erdma/erdma_verbs.c
+++ drivers/infiniband/hw/erdma/erdma_verbs.c
@@ -1228,6 +1228,7 @@ int erdma_destroy_cq(struct ib_cq *ibcq, struct ib_udata *udata)
 	struct erdma_dev *dev = to_edev(ibcq->device);
 	struct erdma_ucontext *ctx = rdma_udata_to_drv_context(
 		udata, struct erdma_ucontext, ibucontext);
+	unsigned long flags;
 	int err;
 	struct erdma_cmdq_destroy_cq_req req;
 
@@ -1239,6 +1240,13 @@ int erdma_destroy_cq(struct ib_cq *ibcq, struct ib_udata *udata)
 	if (err)
 		return err;
 
+	xa_lock_irqsave(&dev->cq_xa, flags);
+	__xa_erase(&dev->cq_xa, cq->cqn);
+	xa_unlock_irqrestore(&dev->cq_xa, flags);
+
+	erdma_cq_put(cq);
+	wait_for_completion(&cq->free);
+
 	if (rdma_is_kernel_res(&cq->ibcq.res)) {
 		dma_free_coherent(&dev->pdev->dev,
 				  WARPPED_BUFSIZE(cq->depth << CQE_SHIFT),
@@ -1247,8 +1255,6 @@ int erdma_destroy_cq(struct ib_cq *ibcq, struct ib_udata *udata)
 		erdma_unmap_user_dbrecords(ctx, &cq->user_cq.user_dbr_page);
 		put_mtt_entries(dev, &cq->user_cq.qbuf_mem);
 	}
-
-	xa_erase(&dev->cq_xa, cq->cqn);
 
 	return 0;
 }
@@ -1658,6 +1664,8 @@ int erdma_create_cq(struct ib_cq *ibcq, const struct ib_cq_init_attr *attr,
 	cq->ibcq.cqe = depth;
 	cq->depth = depth;
 	cq->assoc_eqn = attr->comp_vector + 1;
+	refcount_set(&cq->refcount, 1);
+	init_completion(&cq->free);
 
 	ret = xa_alloc_cyclic(&dev->cq_xa, &cq->cqn, cq,
 			      XA_LIMIT(1, dev->attrs.max_cq - 1),
