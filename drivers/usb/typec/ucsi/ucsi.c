da08061930ef1849cab9760bbff18d5f725505ef
Sergey Senozhatsky <senozhatsky@chromium.org>
2026-07-31 07:10:45 -0400

    usb: typec: ucsi: split connector lock classes
    [ Upstream commit 8c22256bbafad3dc5fdbe9f684d045b67ff06a68 ]
    
    Lockdep detects a possible recursive locking scenario during
    ucsi init:
    
    [    5.418616] ============================================
    [    5.418634] WARNING: possible recursive locking detected
    [    5.418706] --------------------------------------------
    [    5.418725] kworker/4:1/82 is trying to acquire lock:
    [    5.418759] ffff888119a34648 (&con->lock){+.+.}-{3:3}, at: ucsi_init_work+0x1a78/0x2eb0 [typec_ucsi]
    [    5.418801]
                   but task is already holding lock:
    [    5.418835] ffff888119a34080 (&con->lock){+.+.}-{3:3}, at: ucsi_init_work+0x1a78/0x2eb0 [typec_ucsi]
    [    5.418884]
                   other info that might help us debug this:
    [    5.418904]  Possible unsafe locking scenario:
    
    [    5.418937]        CPU0
    [    5.418956]        ----
    [    5.418991]   lock(&con->lock);
    [    5.419013]   lock(&con->lock);
    [    5.419033]
                    *** DEADLOCK ***
    
    [    5.419387] Call Trace:
    [    5.419406]  <TASK>
    [    5.419425]  dump_stack_lvl+0x61/0xa0
    [    5.419448]  print_deadlock_bug+0x4a6/0x650
    [    5.419483]  __lock_acquire+0x62b6/0x7f50
    [    5.419507]  lock_acquire+0x11b/0x390
    [    5.419654]  __mutex_lock+0xbc/0xcd0
    [    5.419741]  ucsi_init_work+0x1a78/0x2eb0
    [    5.419785]  ? worker_thread+0xf53/0x2bc0
    [    5.419819]  worker_thread+0xff4/0x2bc0
    [    5.419842]  kthread+0x2a7/0x330
    [    5.419863]  ? __pfx_worker_thread+0x10/0x10
    [    5.419896]  ? __pfx_kthread+0x10/0x10
    [    5.419916]  ret_from_fork+0x38/0x70
    [    5.419936]  ? __pfx_kthread+0x10/0x10
    [    5.419969]  ret_from_fork_asm+0x1b/0x30
    [    5.419991]  </TASK>
    [    5.420009] ---[ end trace 0000000000000000 ]---
    
    The problem is that all connector locks belong to the same
    lockdep lock class, so the following loop:
    
    	for (i = 0; i < ucsi->cap.num_connectors; i++)
    		ucsi_register_port(connector[i])
    			mutex_lock(&connector[i]->lock)
    
    looks like a recursive acquire of the same mutex.  Put each connector
    lock into a dedicated lock class so that lockdep doesn't see it as a
    possible recursion.
    
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Reviewed-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Link: https://patch.msgid.link/20260515060042.136083-1-senozhatsky@chromium.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Stable-dep-of: 7aa7d4bf9d3f ("usb: typec: ucsi: Fix race condition and ordering in port unregistration")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/typec/ucsi/ucsi.c
+++ drivers/usb/typec/ucsi/ucsi.c
@@ -1120,6 +1120,7 @@ static int ucsi_register_port(struct ucsi *ucsi, struct ucsi_connector *con)
 	INIT_WORK(&con->work, ucsi_handle_connector_change);
 	init_completion(&con->complete);
 	mutex_init(&con->lock);
+	lockdep_set_class(&con->lock, &con->lock_key);
 	INIT_LIST_HEAD(&con->partner_tasks);
 	con->ucsi = ucsi;
 
@@ -1327,6 +1328,9 @@ static int ucsi_init(struct ucsi *ucsi)
 		goto err_reset;
 	}
 
+	for (i = 0; i < ucsi->cap.num_connectors; i++)
+		lockdep_register_key(&connector[i].lock_key);
+
 	/* Register all connectors */
 	for (i = 0; i < ucsi->cap.num_connectors; i++) {
 		connector[i].num = i + 1;
@@ -1356,6 +1360,9 @@ static int ucsi_init(struct ucsi *ucsi)
 	return 0;
 
 err_unregister:
+	for (i = 0; i < ucsi->cap.num_connectors; i++)
+		lockdep_unregister_key(&connector[i].lock_key);
+
 	for (con = connector; con->port; con++) {
 		if (con->wq)
 			destroy_workqueue(con->wq);
@@ -1597,6 +1604,7 @@ void ucsi_unregister(struct ucsi *ucsi)
 					 UCSI_RECIPIENT_CON);
 		ucsi_unregister_port_psy(&ucsi->connector[i]);
 		typec_unregister_port(ucsi->connector[i].port);
+		lockdep_unregister_key(&ucsi->connector[i].lock_key);
 	}
 
 	kfree(ucsi->connector);
