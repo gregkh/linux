c5a2468d170c533697e098906dffb3dff864671b
Gokul Sivakumar <gokulkumar.sivakumar@infineon.com>
2026-08-06 14:42:08 -0400

    wifi: brcmfmac: fix 43752 SDIO FWVID incorrectly labelled as Cypress (CYW)
    [ Upstream commit 74e2ef72bd4b25ce21c8f309d4f5b91b5df9ff5b ]
    
    Cypress(Infineon) is not the vendor for this 43752 SDIO WLAN chip, and so
    has not officially released any firmware binary for it. It is incorrect to
    maintain this WLAN chip with firmware vendor ID as "CYW". So relabel the
    chip's firmware Vendor ID as "WCC" as suggested by the maintainer.
    
    Fixes: d2587c57ffd8 ("brcmfmac: add 43752 SDIO ids and initialization")
    Fixes: f74f1ec22dc2 ("wifi: brcmfmac: add support for Cypress firmware api")
    Signed-off-by: Gokul Sivakumar <gokulkumar.sivakumar@infineon.com>
    Acked-by: Arend van Spriel <arend.vanspriel@broadcom.com>
    Link: https://patch.msgid.link/20250724101136.6691-1-gokulkumar.sivakumar@infineon.com
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Stable-dep-of: 29ab31f3f271 ("wifi: brcmfmac: set F2 blocksize to 256 for BCM43752")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -4270,8 +4270,8 @@ static void brcmf_sdio_firmware_callback(struct device *dev, int err,
 				   bus->hostintmask, NULL);
 
 		switch (sdiod->func1->device) {
+		case SDIO_DEVICE_ID_BROADCOM_43752:
 		case SDIO_DEVICE_ID_BROADCOM_CYPRESS_4373:
-		case SDIO_DEVICE_ID_BROADCOM_CYPRESS_43752:
 			brcmf_dbg(INFO, "set F2 watermark to 0x%x*4 bytes\n",
 				  CY_4373_F2_WATERMARK);
 			brcmf_sdiod_writeb(sdiod, SBSDIO_WATERMARK,
