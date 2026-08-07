770ba3e4f0e02c955265e5828a31b179c76162fd
Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
2026-08-06 23:34:45 -0400

    serial: sc16is7xx: Fill in rs485_supported
    [ Upstream commit 267913ecf73745ca3e8fc8282671b0b4f24df5fe ]
    
    Add information on supported serial_rs485 features.
    
    This driver does not support delay_rts_after_send but the pre-existing
    behavior is to return -EINVAL if delay_rts_after_send is non-zero. In
    contrast, other drivers that do not support delay_rts_after_send either
    zero delay_rts_after_send or do not care (leave the inaccurate value).
    As changing this would cause userspace visible impact, the change is
    not attempted here. But perhaps it should be still tried (maybe nobody
    finds that kind of API oddity significant)?
    
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Link: https://lore.kernel.org/r/20220606100433.13793-21-ilpo.jarvinen@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Stable-dep-of: af071d9e07e5 ("serial: sc16is7xx: implement gpio get_direction() callback")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/sc16is7xx.c
+++ drivers/tty/serial/sc16is7xx.c
@@ -1239,6 +1239,12 @@ static int sc16is7xx_gpio_direction_output(struct gpio_chip *chip,
 }
 #endif
 
+static const struct serial_rs485 sc16is7xx_rs485_supported = {
+	.flags = SER_RS485_ENABLED | SER_RS485_RTS_AFTER_SEND,
+	.delay_rts_before_send = 1,
+	.delay_rts_after_send = 1,	/* Not supported but keep returning -EINVAL */
+};
+
 static int sc16is7xx_probe(struct device *dev,
 			   const struct sc16is7xx_devtype *devtype,
 			   struct regmap *regmap, int irq)
@@ -1326,6 +1332,7 @@ static int sc16is7xx_probe(struct device *dev,
 		s->p[i].port.iotype	= UPIO_PORT;
 		s->p[i].port.uartclk	= freq;
 		s->p[i].port.rs485_config = sc16is7xx_config_rs485;
+		s->p[i].port.rs485_supported = &sc16is7xx_rs485_supported;
 		s->p[i].port.ops	= &sc16is7xx_ops;
 		s->p[i].port.line	= sc16is7xx_alloc_line();
 		if (s->p[i].port.line >= SC16IS7XX_MAX_DEVS) {
