02bd02c585293634b213b142cba63cbf77891f6b
Shivank Garg <shivankg@amd.com>
2026-08-22 19:22:05 +0000

    dmaengine: fix use-after-free in dma_chan_put() and dma_release_channel()
    [ Upstream commit e873c74132f0c5f1452816cd9bb26208f0bba1e1 ]
    
    When dma_device_put() drops the last reference on chan->device->ref,
    dma_device_release() runs and may free the dma_device along with its
    channels.
    
    dma_chan_put() then still reads chan->device->owner via
    dma_chan_to_owner() for the trailing module_put(). KASAN catches it:
    
    	slab-use-after-free in dma_chan_put+0x3e6/0x4c0
    	Read of size 8 by task insmod/6319
    	Freed by task 6319:
    	  kfree+0x225/0x470
    	  dma_chan_put+0x395/0x4c0
    	  dmaengine_put+0xf8/0x160
    
    Cache the module owner in dma_chan_put() before the put so the trailing
    module_put() does not need chan->device.
    
    Fixes: 8ad342a86359 ("dmaengine: Add reference counting to dma_device struct")
    Suggested-by: Sashiko <sashiko-bot@kernel.org>
    Link: https://sashiko.dev/#/patchset/20260518-dmaengine-kref-fix-v1-1-4d6125048fb7@amd.com
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Reviewed-by: Logan Gunthorpe <logang@deltatee.com>
    Signed-off-by: Shivank Garg <shivankg@amd.com>
    Link: https://patch.msgid.link/20260822-dmaengine-kref-fix-v5-3-d4a4ee47d927@amd.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/dma/dmaengine.c
+++ drivers/dma/dmaengine.c
@@ -495,10 +495,13 @@ module_put_out:
  */
 static void dma_chan_put(struct dma_chan *chan)
 {
+	struct module *owner;
+
 	/* This channel is not in use, bail out */
 	if (!chan->client_count)
 		return;
 
+	owner = dma_chan_to_owner(chan);
 	chan->client_count--;
 
 	/* This channel is not in use anymore, free it */
@@ -518,7 +521,7 @@ static void dma_chan_put(struct dma_chan *chan)
 	/* This channel is not in use anymore, drop the device ref */
 	if (!chan->client_count)
 		dma_device_put(chan->device);
-	module_put(dma_chan_to_owner(chan));
+	module_put(owner);
 }
 
 enum dma_status dma_sync_wait(struct dma_chan *chan, dma_cookie_t cookie)
