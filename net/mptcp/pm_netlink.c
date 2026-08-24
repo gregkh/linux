81d8142148164176385c279c7c1e1d581867423d
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2026-08-24 19:50:38 -0400

    mptcp: pm: ADD_ADDR rtx: always decrease sk refcount
    [ Upstream commit 9634cb35af17019baec21ca648516ce376fa10e6 ]
    
    When an ADD_ADDR is retransmitted, the sk is held in sk_reset_timer().
    It should then be released in all cases at the end.
    
    Some (unlikely) checks were returning directly instead of calling
    sock_put() to decrease the refcount. Jump to a new 'exit' label to call
    __sock_put() (which will become sock_put() in the next commit) to fix
    this potential leak.
    
    While at it, drop the '!msk' check which cannot happen because it is
    never reset, and explicitly mark the remaining one as "unlikely".
    
    Fixes: 00cfd77b9063 ("mptcp: retransmit ADD_ADDR when timeout")
    Cc: stable@vger.kernel.org
    Reviewed-by: Mat Martineau <martineau@kernel.org>
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Link: https://patch.msgid.link/20260505-net-mptcp-pm-fixes-7-1-rc3-v1-4-fca8091060a4@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: a7aad5b69d3b ("mptcp: pm: fix data race in add_addr timer callback")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/mptcp/pm_netlink.c
+++ net/mptcp/pm_netlink.c
@@ -309,11 +309,8 @@ static void mptcp_pm_add_timer(struct timer_list *timer)
 
 	pr_debug("msk=%p\n", msk);
 
-	if (!msk)
-		return;
-
-	if (inet_sk_state_load(sk) == TCP_CLOSE)
-		return;
+	if (unlikely(inet_sk_state_load(sk) == TCP_CLOSE))
+		goto exit;
 
 	bh_lock_sock(sk);
 	if (sock_owned_by_user(sk)) {
@@ -357,6 +354,7 @@ static void mptcp_pm_add_timer(struct timer_list *timer)
 
 out:
 	bh_unlock_sock(sk);
+exit:
 	__sock_put(sk);
 }
 
