8709e7e1dd7d7de0651e61700edf24f5784f9243
Jinjie Ruan <ruanjinjie@huawei.com>
2026-09-04 07:38:09 +0200

    entry: Fix seccomp bypass after ptrace with TSYNC
    commit 4a3591287fb7f808e209b4974ed337f609a2006b upstream.
    
    Sashiko review pointed out the following issue.
    
    If a thread is stopped in syscall_trace_enter() for ptrace, another
    thread can install a seccomp filter with SECCOMP_FILTER_FLAG_TSYNC
    (e.g., via seccomp_attach_filter()). This will successfully set
    SYSCALL_WORK_SECCOMP on the stopped thread, but syscall_trace_enter()
    evaluates a cached 'work' variable sampled on entry. Consequently,
    the subsequent check for SYSCALL_WORK_SECCOMP misses the newly
    assigned flag, and the filter is silently bypassed.
    
    This race condition could allow an unprivileged process to execute
    a prohibited system call (e.g., execve) that the newly installed filter
    was intended to block, especially since the tracer might have modified
    the system call number during the ptrace stop.
    
    Fix this by re-reading the syscall_work flags after ptrace handling,
    so that any new SYSCALL_WORK_SECCOMP flag set by another thread via
    TSYNC during the ptrace stop is observed before the subsequent
    seccomp check.
    
    Fixes: 142781e108b1 ("entry: Provide generic syscall entry functionality")
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/all/20260629132914.1135C1F000E9@smtp.kernel.org/
    Link: https://patch.msgid.link/20260713025712.416366-1-ruanjinjie@huawei.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/entry/common.c
+++ kernel/entry/common.c
@@ -61,6 +61,9 @@ static long syscall_trace_enter(struct pt_regs *regs, long syscall,
 		ret = arch_syscall_enter_tracehook(regs);
 		if (ret || (work & SYSCALL_WORK_SYSCALL_EMU))
 			return -1L;
+
+		/* ptrace might have changed work flags */
+		work = READ_ONCE(current_thread_info()->syscall_work);
 	}
 
 	/* Do seccomp after ptrace, to catch any tracer changes. */
