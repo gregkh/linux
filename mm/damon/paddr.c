072d538e9d8c5159a55d319d2d0674a84b2a9506
SJ Park <sj@kernel.org>
2026-09-08 22:45:45 -0700

    mm/damon/paddr: drop last same folio access check reuse optimization
    [ Upstream commit f23f0aa62b2f32c2b12f95959fc4603ef81678b2 ]
    
    It can race when multiple kdamonds are being used.  The problem from the
    race is doubtful, but the gain from the optimization is also doubtful.
    Simply drop the optimization in favor of code simplicity.
    
    The user impact is doubtfully trivial.  After all, this kind of
    interference can happen only by intentional user setup.  Even if it
    happens, it will be rare, and the consequence is degradation of the
    best-effort monitoring results.  No critical consequences like kernel
    panic or memory corruption happen.
    
    The race was discovered [1] by Sashiko.
    
    Link: https://lore.kernel.org/20260715031002.108504-5-sj@kernel.org
    Link: https://lore.kernel.org/20260621204050.10993-1-sj@kernel.org [1]
    Fixes: a28397beb55b ("mm/damon: implement primitives for physical address space monitoring")
    Signed-off-by: SJ Park <sj@kernel.org>
    Cc: <stable@vger.kernel.org> # 5.16.x
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: SJ Park <sj@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- mm/damon/paddr.c
+++ mm/damon/paddr.c
@@ -111,7 +111,7 @@ static bool __damon_pa_young(struct folio *folio, struct vm_area_struct *vma,
 	return *accessed == false;
 }
 
-static bool damon_pa_young(unsigned long paddr, unsigned long *folio_sz)
+static bool damon_pa_young(unsigned long paddr)
 {
 	struct folio *folio = damon_get_folio(PHYS_PFN(paddr));
 	bool accessed = false;
@@ -143,30 +143,17 @@ static bool damon_pa_young(unsigned long paddr, unsigned long *folio_sz)
 		folio_unlock(folio);
 
 out:
-	*folio_sz = folio_size(folio);
 	folio_put(folio);
 	return accessed;
 }
 
 static void __damon_pa_check_access(struct damon_region *r)
 {
-	static unsigned long last_addr;
-	static unsigned long last_folio_sz = PAGE_SIZE;
-	static bool last_accessed;
+	bool accessed;
 
-	/* If the region is in the last checked page, reuse the result */
-	if (ALIGN_DOWN(last_addr, last_folio_sz) ==
-				ALIGN_DOWN(r->sampling_addr, last_folio_sz)) {
-		if (last_accessed)
-			r->nr_accesses++;
-		return;
-	}
-
-	last_accessed = damon_pa_young(r->sampling_addr, &last_folio_sz);
-	if (last_accessed)
+	accessed = damon_pa_young(r->sampling_addr);
+	if (accessed)
 		r->nr_accesses++;
-
-	last_addr = r->sampling_addr;
 }
 
 static unsigned int damon_pa_check_accesses(struct damon_ctx *ctx)
