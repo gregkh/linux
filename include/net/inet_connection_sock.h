5d5389b2c37ca00da61d1407618ebe15abae0c8b
Sechang Lim <rhkrqnwk98@gmail.com>
2026-09-09 10:56:45 +0200

    tcp: clear sock_ops cb flags before force-closing a child socket
    [ Upstream commit 990348e5bb457697c2f1f7f7b65154a3334d9d2b ]
    
    A child socket inherits the listener's bpf_sock_ops_cb_flags via
    sk_clone_lock(). If its setup fails in tcp_v4_syn_recv_sock() /
    tcp_v6_syn_recv_sock(), the child is freed through put_and_exit, where
    inet_csk_prepare_forced_close() drops the socket lock and tcp_done() runs
    without it.
    
    If BPF_SOCK_OPS_STATE_CB_FLAG was inherited, tcp_done() -> tcp_set_state()
    calls tcp_call_bpf(), which expects the lock and trips sock_owned_by_me():
    
      WARNING: include/net/sock.h:1799 at tcp_set_state+0x433/0x550
      RIP: 0010:tcp_set_state+0x433/0x550 include/net/sock.h:1799
      Call Trace:
       <IRQ>
       tcp_done+0xba/0x250 net/ipv4/tcp.c:5095
       tcp_v4_syn_recv_sock+0x850/0xa50 net/ipv4/tcp_ipv4.c:1787
       tcp_check_req+0xf30/0x1360 net/ipv4/tcp_minisocks.c:926
       tcp_v4_rcv+0x1047/0x1b50 net/ipv4/tcp_ipv4.c:2164
       </IRQ>
    
    The child is freed before it is ever established, so it should run no
    sock_ops callback. Clear its cb flags in inet_csk_prepare_for_destroy_sock(),
    the common point for the IPv4, IPv6 and chtls forced-close paths and for the
    MPTCP ->syn_recv_sock() failure path (dispose_child), which reaches tcp_done()
    on a child that was never established too.
    
    Suggested-by: Jiayuan Chen <jiayuan.chen@linux.dev>
    Fixes: d44874910a26 ("bpf: Add BPF_SOCK_OPS_STATE_CB")
    Signed-off-by: Sechang Lim <rhkrqnwk98@gmail.com>
    Reviewed-by: Jiayuan Chen <jiayuan.chen@linux.dev>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@google.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Link: https://patch.msgid.link/20260611092923.1895982-1-rhkrqnwk98@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    
    [ Moved inet_csk_prepare_for_destroy_sock() to inet_connection_sock.c due to header dependency,
      Added a guard arount the call to tcp_clear_sock_ops_cb_flags to avoid
      calling it for DDCP sockets.]
    
    Signed-off-by: Miguel Gazquez (Schneider Electric) <miguel.gazquez@bootlin.com>

--- include/net/inet_connection_sock.h
+++ include/net/inet_connection_sock.h
@@ -290,14 +290,9 @@ static inline int inet_csk_reqsk_queue_is_full(const struct sock *sk)
 bool inet_csk_reqsk_queue_drop(struct sock *sk, struct request_sock *req);
 void inet_csk_reqsk_queue_drop_and_put(struct sock *sk, struct request_sock *req);
 
-static inline void inet_csk_prepare_for_destroy_sock(struct sock *sk)
-{
-	/* The below has to be done to allow calling inet_csk_destroy_sock */
-	sock_set_flag(sk, SOCK_DEAD);
-	this_cpu_inc(*sk->sk_prot->orphan_count);
-}
 
 void inet_csk_destroy_sock(struct sock *sk);
+void inet_csk_prepare_for_destroy_sock(struct sock *sk);
 void inet_csk_prepare_forced_close(struct sock *sk);
 
 /*
