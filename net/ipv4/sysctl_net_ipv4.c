7898bda1ebc198f5559b301a96dd5398edd4d4d3
Eric Dumazet <edumazet@google.com>
2026-09-12 14:48:48 +0000

    tcp: do not let tcp_rmem be set below 4096
    [ Upstream commit 83a945a529d6e002dd7339c532288a931f463dba ]
    
    We can hit a division by zero crash in tcp_rcvbuf_grow()
    and tcp_rcv_space_adjust():
    
    divide error: 0000 [#1] PREEMPT SMP
    RIP: 0010:tcp_rcvbuf_grow+0x187/0x450 net/ipv4/tcp_input.c:939
    ...
    grow = div_u64(((u64)rcvwin << 1) * (newval - oldval), oldval);
    
    The division uses oldval = tp->rcvq_space.space as divisor.
    When tp->rcvq_space.space is zero, this leads to a divide-by-zero
    exception.
    
    tp->rcvq_space.space is initialized in tcp_init_buffer_space():
        tp->rcvq_space.space = min3(tp->rcv_ssthresh, tp->rcv_wnd,
                                    (u32)TCP_INIT_CWND * tp->advmss);
    
    If tcp_rmem[1] is configured to very small values (such as 1),
    sk->sk_rcvbuf is initialized to 1. Then tcp_full_space(sk), which
    computes (sk->sk_rcvbuf * scaling_ratio) >> 8, truncates to 0.
    This sets tp->window_clamp = 0, tp->rcv_ssthresh = 0, and
    tp->rcvq_space.space = 0. Later, when data arrives and DRS is invoked,
    tcp_rcvbuf_grow() divides by oldval == 0.
    
    Back in 2015, commit b1cb59cf2efe ("net: sysctl_net_core: check SNDBUF
    and RCVBUF for min length") ensured that net.core.rmem_default and
    net.core.rmem_max cannot be set below SOCK_MIN_RCVBUF. Similarly,
    SO_RCVBUF setsockopt enforces max_t(int, val * 2, SOCK_MIN_RCVBUF).
    
    However, net.ipv4.tcp_rmem still had .extra1 = SYSCTL_ONE, allowing
    arbitrarily small values.
    
    Because SOCK_MIN_RCVBUF depends on sizeof(struct sk_buff) and cacheline
    alignment, its value varies across architectures and configuration options.
    Using a fixed constant of 4096 ensures a predictable, architecture-
    independent lower bound that is safely above SOCK_MIN_RCVBUF everywhere
    and matches the documented 4K default.
    
    Fix this by setting tcp_rmem.extra1 to 4096 and updating the documentation.
    
    Fixes: 1da177e4c3f4 ("Linux-2.6.12-rc2")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/20260912144848.3448026-1-edumazet@google.com
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -49,6 +49,8 @@ static int tcp_plb_max_cong_thresh = 256;
 static unsigned int tcp_tw_reuse_delay_max = TCP_PAWS_MSL * MSEC_PER_SEC;
 static int tcp_ecn_mode_max = 2;
 
+static int tcp_min_rcvbuf = 4096;
+
 /* obsolete */
 static int sysctl_tcp_low_latency __read_mostly;
 
@@ -1435,7 +1437,7 @@ static struct ctl_table ipv4_net_table[] = {
 		.maxlen		= sizeof(init_net.ipv4.sysctl_tcp_rmem),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= SYSCTL_ONE,
+		.extra1		= &tcp_min_rcvbuf,
 	},
 	{
 		.procname	= "tcp_comp_sack_delay_ns",
