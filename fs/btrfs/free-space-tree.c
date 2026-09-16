a32bedab8ece9d1f90a1476a5b486a1804dc78ba
Guanghui Yang <3497809730@qq.com>
2026-09-16 05:16:38 +0000

    btrfs: clear free space tree creation state on rebuild failure
    commit 3565893cc72cdf6b795cf6a33e7ff9605322334d upstream.
    
    btrfs_rebuild_free_space_tree() sets BTRFS_FS_CREATING_FREE_SPACE_TREE
    before rebuilding the free space tree.  Several error paths return
    without clearing this flag.
    
    The transaction restart failure path can leave the flag set on a live
    filesystem, causing delayed reference processing to be skipped. Clear it
    on all free space tree rebuild failure paths. Keep
    BTRFS_FS_FREE_SPACE_TREE_UNTRUSTED set, since a failed rebuild leaves
    the free space tree untrusted. Callers must fall back to extent-tree
    caching.
    
    Fixes: 882af9f13e83 ("btrfs: handle free space tree rebuild in multiple transactions")
    CC: stable@vger.kernel.org # 6.14+
    Assisted-by: LLM
    Reviewed-by: Boris Burkov <boris@bur.io>
    Reviewed-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: Guanghui Yang <3497809730@qq.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/btrfs/free-space-tree.c
+++ fs/btrfs/free-space-tree.c
@@ -1346,7 +1346,7 @@ int btrfs_rebuild_free_space_tree(struct btrfs_fs_info *fs_info)
 	if (unlikely(ret)) {
 		btrfs_abort_transaction(trans, ret);
 		btrfs_end_transaction(trans);
-		return ret;
+		goto out_clear;
 	}
 
 	node = rb_first_cached(&fs_info->block_group_cache_tree);
@@ -1364,14 +1364,16 @@ int btrfs_rebuild_free_space_tree(struct btrfs_fs_info *fs_info)
 		if (unlikely(ret)) {
 			btrfs_abort_transaction(trans, ret);
 			btrfs_end_transaction(trans);
-			return ret;
+			goto out_clear;
 		}
 next:
 		if (btrfs_should_end_transaction(trans)) {
 			btrfs_end_transaction(trans);
 			trans = btrfs_start_transaction(free_space_root, 1);
-			if (IS_ERR(trans))
-				return PTR_ERR(trans);
+			if (IS_ERR(trans)) {
+				ret = PTR_ERR(trans);
+				goto out_clear;
+			}
 		}
 		node = rb_next(node);
 	}
@@ -1382,6 +1384,10 @@ next:
 
 	ret = btrfs_commit_transaction(trans);
 	clear_bit(BTRFS_FS_FREE_SPACE_TREE_UNTRUSTED, &fs_info->flags);
+	return ret;
+
+out_clear:
+	clear_bit(BTRFS_FS_CREATING_FREE_SPACE_TREE, &fs_info->flags);
 	return ret;
 }
 
