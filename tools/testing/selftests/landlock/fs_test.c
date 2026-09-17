74bdf27404c24143503f962e8e72ba483d383270
Günther Noack <gnoack@google.com>
2026-09-17 17:41:28 +0200

    selftests/landlock: Use an actual chardev for MAKE_CHAR audit test
    [ Upstream commit 173b1bd8730825e1f6862dbd07856e7d68447a41 ]
    
    By passing a (0, 0) device number, the audit test for
    LANDLOCK_ACCESS_FS_MAKE_CHAR was accidentally creating a whiteout object
    rather than a char device.  In preparation to treating whiteout objects
    differently, use an actual character device instead.
    
    Signed-off-by: Günther Noack <gnoack@google.com>
    Link: https://patch.msgid.link/20260813093157.1436894-2-gnoack@google.com
    Signed-off-by: Mickaël Salaün <mic@digikod.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/testing/selftests/landlock/fs_test.c
+++ tools/testing/selftests/landlock/fs_test.c
@@ -7315,7 +7315,7 @@ TEST_F(audit_layout1, make_char)
 					      .handled_access_fs = access_fs_16,
 				      });
 
-	EXPECT_EQ(-1, mknod(file1_s1d3, S_IFCHR | 0644, 0));
+	EXPECT_EQ(-1, mknod(file1_s1d3, S_IFCHR | 0644, makedev(7, 0)));
 	EXPECT_EQ(EACCES, errno);
 	EXPECT_EQ(0, matches_log_fs(_metadata, self->audit_fd, "fs\\.make_char",
 				    dir_s1d3));
