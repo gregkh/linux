e29d6ab39ea3eea47d67279bc84abb39f630e97f
Jianbo Liu <jianbol@nvidia.com>
2026-09-22 11:35:52 -0400

    xfrm: Refactor xfrm_input lock to reduce contention with RSS
    [ Upstream commit 10a11861943902fda74f37f456b45183b2bca270 ]
    
    With newer NICs like mlx5 supporting RSS for IPsec crypto offload,
    packets for a single Security Association (SA) are scattered across
    multiple CPU cores for parallel processing. The xfrm_state spinlock
    (x->lock) is held for each packet during xfrm processing.
    
    When multiple connections or flows share the same SA, this parallelism
    causes high lock contention on x->lock, creating a performance
    bottleneck and limiting scalability.
    
    The original xfrm_input() function exacerbated this issue by releasing
    and immediately re-acquiring x->lock. For hardware crypto offload
    paths, this unlock/relock sequence is unnecessary and introduces
    significant overhead. This patch refactors the function to relocate
    the type_offload->input_tail call for the offload path, performing all
    necessary work while continuously holding the lock. This reordering is
    safe, since packets which don't pass the checks below will still fail
    them with the new code.
    
    Performance testing with iperf using multiple parallel streams over a
    single IPsec SA shows significant improvement in throughput as the
    number of queues (and thus CPU cores) increases:
    
    +-----------+---------------+--------------+-----------------+
    | RX queues | Before (Gbps) | After (Gbps) | Improvement (%) |
    +-----------+---------------+--------------+-----------------+
    |         2 |          32.3 |         34.4 |             6.5 |
    |         4 |          34.4 |         40.0 |            16.3 |
    |         6 |          24.5 |         38.3 |            56.3 |
    |         8 |          23.1 |         38.3 |            65.8 |
    |        12 |          18.1 |         29.9 |            65.2 |
    |        16 |          16.0 |         25.2 |            57.5 |
    +-----------+---------------+--------------+-----------------+
    
    Signed-off-by: Jianbo Liu <jianbol@nvidia.com>
    Reviewed-by: Cosmin Ratiu <cratiu@nvidia.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Stable-dep-of: 3cf5cdecd99c ("xfrm: save input state data before secpath resets")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/xfrm/xfrm_input.c
+++ net/xfrm/xfrm_input.c
@@ -504,6 +504,7 @@ int xfrm_input(struct sk_buff *skb, int nexthdr, __be32 spi, int encap_type)
 		if (encap_type == -1) {
 			async = 1;
 			seq = XFRM_SKB_CB(skb)->seq.input.low;
+			spin_lock(&x->lock);
 			goto resume;
 		}
 		/* GRO call */
@@ -540,6 +541,8 @@ int xfrm_input(struct sk_buff *skb, int nexthdr, __be32 spi, int encap_type)
 				XFRM_INC_STATS(net, LINUX_MIB_XFRMINHDRERROR);
 				goto drop;
 			}
+
+			nexthdr = x->type_offload->input_tail(x, skb);
 		}
 
 		goto lock;
@@ -637,11 +640,9 @@ lock:
 			goto drop_unlock;
 		}
 
-		spin_unlock(&x->lock);
-
 		if (xfrm_tunnel_check(skb, x, family)) {
 			XFRM_INC_STATS(net, LINUX_MIB_XFRMINSTATEMODEERROR);
-			goto drop;
+			goto drop_unlock;
 		}
 
 		seq_hi = htonl(xfrm_replay_seqhi(x, seq));
@@ -649,9 +650,8 @@ lock:
 		XFRM_SKB_CB(skb)->seq.input.low = seq;
 		XFRM_SKB_CB(skb)->seq.input.hi = seq_hi;
 
-		if (crypto_done) {
-			nexthdr = x->type_offload->input_tail(x, skb);
-		} else {
+		if (!crypto_done) {
+			spin_unlock(&x->lock);
 			dev_hold(skb->dev);
 
 			nexthdr = x->type->input(x, skb);
@@ -662,9 +662,9 @@ lock:
 			}
 
 			dev_put(skb->dev);
+			spin_lock(&x->lock);
 		}
 resume:
-		spin_lock(&x->lock);
 		if (nexthdr < 0) {
 			if (nexthdr == -EBADMSG) {
 				xfrm_audit_state_icvfail(x, skb,
